1307bc46987cafce7ae7e64343a783c44a272f22
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-02-13 00:19:29 +0100

    Split asynchronous CUDA API wrappers into multiple header files.

--- cuda_wrapper/event.hpp
+++ cuda_wrapper/event.hpp
@@ -1,4 +1,4 @@
-/* cuda_wrapper/async.hpp
+/* cuda_wrapper/event.hpp
  *
  * Copyright (C) 2007  Peter Colberg
  *
@@ -16,85 +16,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Asynchronous CUDA runtime API wrapper classes
- */
-
-#ifndef CUDA_ASYNC_HPP
-#define CUDA_ASYNC_HPP
-
-/* requires CUDA runtime version >= 1.1 */
-#if (CUDART_VERSION >= 1010)
-#define CUDA_ASYNC_API
-#endif
-
-#ifdef CUDA_ASYNC_API
+#ifndef CUDA_EVENT_HPP
+#define CUDA_EVENT_HPP
 
 #include <cuda_runtime.h>
 #include <cuda_wrapper/error.hpp>
+#include <cuda_wrapper/stream.hpp>
 
 namespace cuda
 {
 
-/*
- * CUDA stream wrapper class
- */
-class stream
-{
-    friend class event;
+#ifdef CUDA_WRAPPER_ASYNC_API
 
-protected:
-    cudaStream_t _stream;
-
-public:
-    /*
-     * creates a stream
-     */
-    stream()
-    {
-	CUDA_CALL(cudaStreamCreate(&_stream));
-    }
-
-    /*
-     * destroys the stream
-     */
-    ~stream()
-    {
-	CUDA_CALL(cudaStreamDestroy(_stream));
-    }
-
-    /*
-     * blocks until the device has completed all operations in the stream
-     */
-    void synchronize()
-    {
-	CUDA_CALL(cudaStreamSynchronize(_stream));
-    }
-
-    /*
-     * checks if the device has completed all operations in the stream
-     *
-     * WARNING: this function will not detect kernel launch failures
-     */
-    bool query()
-    {
-	cudaError_t err = cudaStreamQuery(_stream);
-	if (cudaSuccess == err)
-	    return true;
-	else if (cudaErrorNotReady == err)
-	    return false;
-	CUDA_ERROR(err);
-    }
-
-private:
-    // disable default copy constructor
-    stream(const stream&);
-    // disable default assignment operator
-    stream& operator=(const stream&);
-};
-
-
-/*
+/**
  * CUDA event wrapper class
  */
 class event
@@ -103,7 +37,7 @@ protected:
     cudaEvent_t _event;
 
 public:
-    /*
+    /**
      * creates an event
      */
     event()
@@ -111,7 +45,7 @@ public:
 	CUDA_CALL(cudaEventCreate(&_event));
     }
 
-    /*
+    /**
      * destroys the event
      */
     ~event()
@@ -119,7 +53,7 @@ public:
 	CUDA_CALL(cudaEventDestroy(_event));
     }
 
-    /*
+    /**
      * records an event
      *
      * after all preceding operations in the CUDA context have been completed
@@ -129,7 +63,7 @@ public:
 	CUDA_CALL(cudaEventRecord(_event, 0));
     }
 
-    /*
+    /**
      * records an event
      *
      * after all preceding operations in the stream have been completed
@@ -139,7 +73,7 @@ public:
 	CUDA_CALL(cudaEventRecord(_event, stream._stream));
     }
 
-    /*
+    /**
      * blocks until the event has actually been recorded
      */
     void synchronize()
@@ -147,7 +81,7 @@ public:
 	CUDA_CALL(cudaEventSynchronize(_event));
     }
 
-    /*
+    /**
      * checks if the event has actually been recorded
      *
      * WARNING: this function will not detect kernel launch failures
@@ -162,7 +96,7 @@ public:
 	CUDA_ERROR(err);
     }
 
-    /*
+    /**
      * computes the elapsed time between two events
      *
      * (in milliseconds with a resolution of around 0.5 microseconds)
@@ -181,8 +115,8 @@ private:
     event& operator=(const event&);
 };
 
+#endif /* CUDA_WRAPPER_ASYNC_API */
+
 }
 
-#endif /* CUDA_ASYNC_API */
-
-#endif /* ! CUDA_ASYNC_HPP */
+#endif /* ! CUDA_EVENT_HPP */
