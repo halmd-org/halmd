b406d26d637e2ba81c4ec71a7594223757e6befb
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-04-09 00:44:53 +0200

    Use iterators for iterating over fluid particles.

--- src/ljfluid.hpp
+++ src/ljfluid.hpp
@@ -48,6 +48,10 @@ struct particle
 template <typename V>
 class ljfluid_base
 {
+public:
+    typedef typename std::vector<particle<V> >::iterator iterator;
+    typedef typename std::vector<particle<V> >::const_iterator const_iterator;
+
 protected:
     /** number of particles in periodic box */
     size_t npart;
@@ -136,10 +140,13 @@ public:
     /**
      * write particle coordinates and velocities to output stream
      */
-    void trajectories(std::ostream& os)
+    void trajectories(std::ostream& os) const
     {
-	for (size_t i = 0; i < npart; i++) {
-	    os << i + 1 << "\t" << part[i].pos << "\t" << part[i].vel << std::endl;
+	const_iterator it;
+	size_t i;
+
+	for (it = part.begin(), i = 1; it != part.end(); ++it, ++i) {
+	    os << i << "\t" << it->pos << "\t" << it->vel << std::endl;
 	}
 	os << std::endl << std::endl;
     }
@@ -150,13 +157,13 @@ private:
      */
     void leapfrog_half()
     {
-	for (size_t i = 0; i < npart; i++) {
+	for (iterator it = part.begin(); it != part.end(); ++it) {
 	    // half step velocity
-	    part[i].vel += part[i].force * (timestep_ / 2.);
+	    it->vel += it->force * (timestep_ / 2.);
 	    // full step coordinates
-	    part[i].pos += part[i].vel * timestep_;
+	    it->pos += it->vel * timestep_;
 	    // enforce periodic boundary conditions
-	    part[i].pos -= floor(part[i].pos / box) * box;
+	    it->pos -= floor(it->pos / box) * box;
 	}
     }
 
@@ -170,13 +177,13 @@ private:
 	// squared velocities sum
 	vel2_sum = 0.;
 
-	for (size_t i = 0; i < npart; i++) {
+	for (iterator it = part.begin(); it != part.end(); ++it) {
 	    // full step velocity
-	    part[i].vel = part[i].vel + (timestep_ / 2) * part[i].force;
+	    it->vel = it->vel + (timestep_ / 2) * it->force;
 	    // velocity center of mass
-	    vel_cm += part[i].vel;
+	    vel_cm += it->vel;
 	    // total kinetic energy
-	    vel2_sum += part[i].vel * part[i].vel;
+	    vel2_sum += it->vel * it->vel;
 	}
 
 	vel_cm /= npart;
@@ -193,14 +200,14 @@ private:
 	// virial equation sum
 	virial = 0.;
 
-	for (size_t i = 0; i < npart; i++) {
-	    part[i].force = 0.;
+	for (iterator it = part.begin(); it != part.end(); ++it) {
+	    it->force = 0.;
 	}
 
-	for (size_t i = 0; i < npart - 1; i++) {
-	    for (size_t j = i + 1; j < npart; j++) {
+	for (iterator it = part.begin(); it != part.end(); ++it) {
+	    for (iterator it2 = it; ++it2 != part.end(); ) {
 		// particle distance vector
-		V r = part[i].pos - part[j].pos;
+		V r = it->pos - it2->pos;
 		// enforce periodic boundary conditions
 		r -= round(r / box) * box;
 		// squared particle distance
@@ -214,8 +221,8 @@ private:
 		double r6i = rri * rri * rri;
 		double fval = 48. * rri * r6i * (r6i - 0.5);
 
-		part[i].force += r * fval;
-		part[j].force -= r * fval;
+		it->force += r * fval;
+		it2->force -= r * fval;
 
 		en_pot += 4. * r6i * (r6i - 1.) - en_cut;
 		virial += rr * fval;
@@ -237,16 +244,16 @@ private:
 	// center of mass velocity
 	V vel_cm = 0.;
 
-	for (size_t i = 0; i < npart; i++) {
-	    rng.unit_vector(part[i].vel);
-	    part[i].vel *= vel_mag;
-	    vel_cm += part[i].vel;
+	for (iterator it = part.begin(); it != part.end(); ++it) {
+	    rng.unit_vector(it->vel);
+	    it->vel *= vel_mag;
+	    vel_cm += it->vel;
 	}
 
 	// set center of mass velocity to zero
 	vel_cm /= npart;
-	for (size_t i = 0; i < npart; i++) {
-	    part[i].vel -= vel_cm;
+	for (iterator it = part.begin(); it != part.end(); ++it) {
+	    it->vel -= vel_cm;
 	}
     }
 
@@ -255,8 +262,8 @@ private:
      */
     void init_forces()
     {
-	for (size_t i = 0; i < npart; i++) {
-	    part[i].force = 0.;
+	for (iterator it = part.begin(); it != part.end(); ++it) {
+	    it->force = 0.;
 	}
     }
 };
@@ -296,7 +303,7 @@ public:
 	// particle density
 	this->density_ = density_;
 	// periodic box length
-	this->box = sqrt(this->npart / density_);
+	this->box = sqrt(npart / density_);
 
 	init_lattice();
     }
@@ -307,13 +314,16 @@ private:
      */
     void init_lattice()
     {
-	// number of particles along one lattice dimension
-	size_t n = (size_t) ceil(sqrt(this->npart));
-	// lattice distance
-	double a = this->box / n;
+	iterator it;
+	size_t i;
 
-	for (size_t i = 0; i < this->npart; i++) {
-	    this->part[i].pos = V(i % n + 0.5, i / n + 0.5) * a;
+	// number of particles along one lattice dimension
+	size_t n = (size_t) ceil(sqrt(npart));
+	// lattice distance
+	double a = box / n;
+
+	for (it = part.begin(), i = 0; it != part.end(); ++it, ++i) {
+	    it->pos = V(i % n + 0.5, i / n + 0.5) * a;
 	}
     }
 };
@@ -349,7 +359,7 @@ public:
 	// particle density
 	this->density_ = density_;
 	// periodic box length
-	this->box = cbrt(this->npart / density_);
+	this->box = cbrt(npart / density_);
 
 	init_lattice();
     }
@@ -360,13 +370,16 @@ private:
      */
     void init_lattice()
     {
-	// number of particles along one lattice dimension
-	size_t n = (size_t) ceil(cbrt(this->npart));
-	// lattice distance
-	double a = this->box / n;
+	iterator it;
+	size_t i;
 
-	for (size_t i = 0; i < this->npart; i++) {
-	    this->part[i].pos = V(i % n + 0.5, i / n % n + 0.5, i / n / n + 0.5) * a;
+	// number of particles along one lattice dimension
+	size_t n = (size_t) ceil(cbrt(npart));
+	// lattice distance
+	double a = box / n;
+
+	for (it = part.begin(), i = 0; it != part.end(); ++it, ++i) {
+	    it->pos = V(i % n + 0.5, i / n % n + 0.5, i / n / n + 0.5) * a;
 	}
     }
 };
