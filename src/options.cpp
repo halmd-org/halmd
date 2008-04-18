d993a56546b49ea7628743bd4d19acf6670e8bc0
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-04-18 17:04:33 +0200

    Add program options parser class.

---
+++ src/options.cpp
@@ -0,0 +1,222 @@
+/* Molecular Dynamics simulation program options
+ *
+ * Copyright (C) 2008  Peter Colberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "options.hpp"
+#include "version.h"
+#include <iostream>
+#include <fstream>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+using namespace std;
+
+
+namespace boost { namespace program_options
+{
+
+/**
+ * program option value validation for double precision floating-point values
+ */
+template <>
+void validate(boost::any& value_store, vector<string> const& values, double*, long)
+{
+    string const& s = validators::get_single_string(values);
+    double value;
+
+    try {
+	value = lexical_cast<double>(s);
+    }
+    catch (bad_lexical_cast const& e)
+    {
+	throw invalid_option_value(s);
+    }
+
+    // require positive value
+    if (value > 0.) {
+	value_store = boost::any(value);
+    }
+    else {
+	throw invalid_option_value(s);
+    }
+}
+
+/**
+ * program option value validation for unsigned integer values
+ */
+template <>
+void validate(boost::any& value_store, vector<string> const& values, unsigned int*, long)
+{
+    string const& s = validators::get_single_string(values);
+    unsigned int value;
+
+    try {
+	value = lexical_cast<unsigned int>(s);
+    }
+    catch (bad_lexical_cast const& e)
+    {
+	throw invalid_option_value(s);
+    }
+
+    // require positive value
+    if (value > 0) {
+	value_store = boost::any(value);
+    }
+    else {
+	throw invalid_option_value(s);
+    }
+}
+
+/**
+ * program option value validation for 64-bit unsigned integer values
+ */
+template <>
+void validate(boost::any& value_store, vector<string> const& values, uint64_t*, long)
+{
+    string const& s = validators::get_single_string(values);
+    uint64_t value;
+
+    try {
+	value = lexical_cast<uint64_t>(s);
+    }
+    catch (bad_lexical_cast const& e)
+    {
+	throw invalid_option_value(s);
+    }
+
+    // require positive value
+    if (value > 0) {
+	value_store = boost::any(value);
+    }
+    else {
+	throw invalid_option_value(s);
+    }
+}
+
+/**
+ * Function used to check that 'opt1' and 'opt2' are not specified at the same time.
+ */
+void conflicting_options(const variables_map& vm, const char* opt1, const char* opt2)
+{
+    if (vm.count(opt1) && !vm[opt1].defaulted() && vm.count(opt2) && !vm[opt2].defaulted()) {
+	throw logic_error(string("conflicting options '") + opt1 + "' and '" + opt2 + "'");
+    }
+}
+
+}} // namespace boost::program_options
+
+
+namespace mdsim {
+
+/**
+ * set default parameters
+ */
+options::options()
+{
+    npart_ = 10000;
+    density_ = 0.05;
+    timestep_ = 0.005;
+    temp_ = 1.;
+    steps_ = 1000;
+    avgsteps_ = 100;
+
+    device_ = 0;
+    blocks_ = 16;
+    threads_ = 512;
+
+    rngseed_ = 123;
+}
+
+/**
+ * parse program option values
+ */
+void options::parse(int argc, char** argv)
+{
+    po::options_description mdsim_opts("MD simulation parameters");
+    mdsim_opts.add_options()
+	("particles,N", po::value(&npart_), "number of particles")
+	("density,d", po::value(&density_), "particle density")
+	("timestep,t", po::value(&timestep_), "simulation timestep")
+	("temperature,T", po::value(&temp_), "initial temperature")
+	("steps,s", po::value(&steps_), "number of simulation steps")
+	("average,S", po::value(&avgsteps_), "number of average accumulation steps")
+	;
+
+    po::options_description cuda_opts("CUDA options");
+    cuda_opts.add_options()
+	("device", po::value(&device_), "CUDA device")
+	("blocks", po::value(&blocks_), "number of blocks per grid")
+	("threads", po::value(&threads_), "number of threads per block")
+	;
+
+    po::options_description misc_opts("Other options");
+    misc_opts.add_options()
+	("seed,R", po::value(&rngseed_), "random number generator integer seed")
+	("input,I", po::value<vector<string> >(), "parameter input file")
+	("version,V", "output version and exit")
+	("help,h", "display this help and exit")
+	;
+
+    po::options_description opts;
+    opts.add(mdsim_opts).add(cuda_opts).add(misc_opts);
+
+    po::variables_map vm;
+
+    try {
+	// parse command line options
+	po::store(po::parse_command_line(argc, argv, opts), vm);
+
+	// parse optional parameter input files
+	if (vm.count("input")) {
+	    vector<string> const& files = vm["input"].as<vector<string> >();
+
+	    for (vector<string>::const_iterator it = files.begin(); it != files.end(); ++it) {
+		ifstream ifs(it->c_str());
+		if (ifs.fail()) {
+		    cerr << PROGRAM_NAME ": could not open parameter input file '" << *it << "'\n";
+		    throw options::exit_exception(EXIT_FAILURE);
+		}
+
+		po::store(po::parse_config_file(ifs, opts), vm);
+	    }
+	}
+    }
+    catch (std::exception const& e) {
+	cerr << PROGRAM_NAME ": " << e.what() << "\n";
+	cerr << "Try `" PROGRAM_NAME " --help' for more information.\n";
+	throw options::exit_exception(EXIT_FAILURE);
+    }
+
+    po::notify(vm);
+
+    if (vm.count("help")) {
+	cout << "Usage: " PROGRAM_NAME " [OPTION]...\n" << opts << "\n";
+	throw options::exit_exception(EXIT_SUCCESS);
+    }
+
+    if (vm.count("version")) {
+	cout << PROGRAM_NAME " (" PROGRAM_VERSION ")\n"
+	    "\n" PROGRAM_COPYRIGHT "\n" "This is free software. "
+	    "You may redistribute copies of it under the terms of\n"
+	    "the GNU General Public License "
+	    "<http://www.gnu.org/licenses/gpl.html>.\n"
+	    "There is NO WARRANTY, to the extent permitted by law.\n";
+	throw options::exit_exception(EXIT_SUCCESS);
+    }
+}
+
+} // namespace mdsim
---
+++ src/options.hpp
@@ -0,0 +1,128 @@
+/* Molecular Dynamics simulation program options
+ *
+ * Copyright (C) 2008  Peter Colberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MDSIM_OPTIONS_HPP
+#define MDSIM_OPTIONS_HPP
+
+#include <stdint.h>
+
+
+namespace mdsim
+{
+
+/**
+ * Molecular Dynamics simulation program options
+ */
+class options
+{
+public:
+    class exit_exception
+    {
+    public:
+	exit_exception(int status) : status_(status) {}
+
+	int status() const
+	{
+	    return status_;
+	}
+
+    private:
+	int status_;
+    };
+
+public:
+    options();
+
+    void parse(int argc, char** argv);
+
+    unsigned int npart() const
+    {
+	return npart_;
+    }
+
+    double density() const
+    {
+	return density_;
+    }
+
+    double timestep() const
+    {
+	return timestep_;
+    }
+
+    double temp() const
+    {
+	return temp_;
+    }
+
+    uint64_t steps() const
+    {
+	return steps_;
+    }
+
+    uint64_t avgsteps() const
+    {
+	return avgsteps_;
+    }
+
+    unsigned int device() const
+    {
+	return device_;
+    }
+
+    unsigned int blocks() const
+    {
+	return blocks_;
+    }
+
+    unsigned int threads() const
+    {
+	return threads_;
+    }
+
+    unsigned int rngseed() const
+    {
+	return rngseed_;
+    }
+
+private:
+    /** number of particles */
+    unsigned int npart_;
+    /** density */
+    double density_;
+    /** simulation timestep */
+    double timestep_;
+    /** initial temperature */
+    double temp_;
+    /** number of simulation steps */
+    uint64_t steps_;
+    /** number of steps for average accumulation */
+    uint64_t avgsteps_;
+    /** CUDA device */
+    unsigned int device_;
+    /** number of blocks per grid */
+    unsigned int blocks_;
+    /** number of threads per block */
+    unsigned int threads_;
+    /** random number generator seed */
+    unsigned int rngseed_;
+};
+
+} // namespace mdsim
+
+#endif /* ! MDSIM_OPTIONS_HPP */
