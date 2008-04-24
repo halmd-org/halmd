51b00d555c56160a870f76cd1633e28181807a8d
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-04-24 23:30:10 +0200

    Eliminate dimension distinction from trajectories output.

--- src/ljfluid.hpp
+++ src/ljfluid.hpp
@@ -292,15 +292,9 @@ template <typename T>
 void ljfluid<T>::trajectories(std::ostream& os) const
 {
     for (size_t i = 0; i < npart; ++i) {
-#ifdef DIM_3D
-	os << part.pos[i].x << "\t" << part.pos[i].y << "\t" << part.pos[i].z << "\t";
-	os << part.vel[i].x << "\t" << part.vel[i].y << "\t" << part.vel[i].z << "\n";
-#else
-	os << part.pos[i].x << "\t" << part.pos[i].y << "\t";
-	os << part.vel[i].x << "\t" << part.vel[i].y << "\n";
-#endif
+	os << T(part.pos[i]) << "\t" << T(part.vel[i]) << "\n";
     }
-    os << std::endl << std::endl;
+    os << "\n\n";
 }
 
 }
