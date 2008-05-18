9746f0e7445a41e6be00db762e2fd4299006d409
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-05-19 01:52:03 +0200

    Simplify CUDA vector container API.

--- include/cuda_wrapper/host/vector.hpp
+++ include/cuda_wrapper/host/vector.hpp
@@ -1,4 +1,4 @@
-/* cuda_wrapper/host/vector.hpp
+/* CUDA host memory vector
  *
  * Copyright (C) 2007  Peter Colberg
  *
@@ -19,31 +19,16 @@
 #ifndef CUDA_HOST_VECTOR_HPP
 #define CUDA_HOST_VECTOR_HPP
 
-#include <cuda/cuda_runtime.h>
-#include <algorithm>
-#include <vector>
-#include <assert.h>
 #include <cuda_wrapper/host/allocator.hpp>
-#include <cuda_wrapper/memory.hpp>
-#include <cuda_wrapper/vector.hpp>
-#include <cuda_wrapper/symbol.hpp>
-#include <cuda_wrapper/stream.hpp>
 #include <cuda_wrapper/function.hpp>
+#include <vector>
 
-namespace cuda
-{
 
-template <typename T>
-class vector;
-
-template <typename T>
-class symbol;
-
-namespace host
+namespace cuda { namespace host
 {
 
 /**
- * vector container class for linear host memory
+ * CUDA host memory vector
  */
 template <typename T, typename Alloc = allocator<T> >
 class vector : public std::vector<T, Alloc>
@@ -61,167 +46,18 @@ public:
     /**
      * initialize host vector with copies of value
      */
-    vector(size_type size, const value_type& value = value_type()) : _Base(size, value)
+    vector(size_type size, value_type const& value = value_type()) : _Base(size, value)
     {
     }
 
     /**
      * initialize host vector with copies of value
      */
-    vector(config dim, const value_type& value = value_type()) : _Base(dim.threads(), value)
+    vector(config const& dim, value_type const& value = value_type()) : _Base(dim.threads(), value)
     {
-    }
-
-    /**
-     * initialize host vector with content of host vector
-     */
-    vector(const vector_type& src) : _Base(src.size())
-    {
-	copy(src, *this);
-    }
-
-    /**
-     * initialize host vector with content of device vector
-     */
-    vector(const cuda::vector<value_type>& src) : _Base(src.size())
-    {
-	copy(src, *this);
-    }
-
-    /**
-     * assign content of host vector to host vector
-     */
-    template <typename _Allocator>
-    vector_type& operator=(const vector<value_type, _Allocator>& src)
-    {
-	if (this != &src) {
-	    resize(src.size());
-	    copy(src, *this);
-	}
-	return *this;
-    }
-
-    /**
-     * assign content of device vector to host vector
-     */
-    vector_type& operator=(const cuda::vector<value_type>& src)
-    {
-	resize(src.size());
-	copy(src, *this);
-	return *this;
-    }
-
-    /**
-     * assign content of device symbol to host vector
-     */
-    vector_type& operator=(const cuda::symbol<value_type>& src)
-    {
-	resize(src.size());
-	copy(src, *this);
-	return *this;
-    }
-
-    /**
-     * assign copies of value to host vector
-     */
-    vector_type& operator=(const value_type& value)
-    {
-	for (size_type i = 0; i < _Base::size(); i++) {
-	    (*this)[i] = value;
-	}
-	return *this;
     }
 };
 
-
-/**
- * vector container class for linear page-locked host memory
- */
-template <typename T>
-class vector<T, allocator<T> > : public std::vector<T, allocator<T> >
-{
-public:
-    typedef allocator<T> _Alloc;
-    typedef std::vector<T, allocator<T> > _Base;
-    typedef vector<T, allocator<T> > vector_type;
-    typedef T value_type;
-    typedef size_t size_type;
-
-public:
-    vector() {}
-
-    /**
-     * initialize host vector with copies of value
-     */
-    vector(size_type size, const value_type& value = value_type()) : _Base(size, value)
-    {
-    }
-
-    /**
-     * initialize host vector with content of host vector
-     */
-    vector(const vector_type& src) : _Base(src.size())
-    {
-	copy(src, *this);
-    }
-
-    /**
-     * initialize host vector with content of device vector
-     */
-    vector(const cuda::vector<value_type>& src) : _Base(src.size())
-    {
-	copy(src, *this);
-    }
-
-
-
-    /**
-     * assign content of host vector to host vector
-     */
-    template <typename _Allocator>
-    vector_type& operator=(const vector<value_type, _Allocator>& src)
-    {
-	if (this != &src) {
-	    resize(src.size());
-	    copy(src, *this);
-	}
-	return *this;
-    }
-
-    /**
-     * assign content of device vector to host vector
-     */
-    vector_type& operator=(const cuda::vector<value_type>& src)
-    {
-	resize(src.size());
-	copy(src, *this);
-	return *this;
-    }
-
-    /**
-     * assign content of device symbol to host vector
-     */
-    vector_type& operator=(const cuda::symbol<value_type>& src)
-    {
-	resize(src.size());
-	copy(src, *this);
-	return *this;
-    }
-
-    /**
-     * assign copies of value to host vector
-     */
-    vector_type& operator=(const value_type& value)
-    {
-	for (size_type i = 0; i < _Base::size(); i++) {
-	    (*this)[i] = value;
-	}
-	return *this;
-    }
-};
-
-}
-
-}
+}} // namespace cuda::host
 
 #endif /* ! CUDA_HOST_VECTOR_HPP */
