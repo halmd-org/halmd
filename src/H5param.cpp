6006ffa65dde11d55d56613328cadfbf049215ca
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-05-26 18:42:21 +0200

    Add HDF5-capable class to gather global simulation parameters.

---
+++ src/H5param.cpp
@@ -0,0 +1,153 @@
+/* Molecular Dynamics simulation parameters
+ *
+ * Copyright (C) 2008  Peter Colberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "H5param.hpp"
+#include "exception.hpp"
+#include "version.h"
+
+
+namespace mdsim
+{
+
+/**
+ * read parameters from HDF5 file
+ */
+void H5param::read(H5::Group const& root)
+{
+    try {
+	H5::Attribute attr;
+	H5::Group node;
+
+	// Lennard-Jones fluid simulation parameters
+	node = root.openGroup("ljfluid");
+
+	// positional coordinates dimension
+	attr = node.openAttribute("dimension");
+	attr.read(H5::PredType::NATIVE_UINT, &dimension_);
+	// number of particles
+	attr = node.openAttribute("particles");
+	attr.read(H5::PredType::NATIVE_UINT, &particles_);
+	// particle density
+	attr = node.openAttribute("density");
+	attr.read(H5::PredType::NATIVE_DOUBLE, &density_);
+	// periodic box length
+	attr = node.openAttribute("box_length");
+	attr.read(H5::PredType::NATIVE_DOUBLE, &box_length_);
+	// simulation timestep
+	attr = node.openAttribute("timestep");
+	attr.read(H5::PredType::NATIVE_DOUBLE, &timestep_);
+	// Lennard-Jones potential cutoff distance
+	attr = node.openAttribute("cutoff_distance");
+	attr.read(H5::PredType::NATIVE_DOUBLE, &cutoff_distance_);
+
+	// correlation function parameters
+	node = root.openGroup("autocorrelation");
+
+	// number of simulation steps
+	attr = node.openAttribute("steps");
+	attr.read(H5::PredType::NATIVE_UINT64, &steps_);
+	// block size
+	attr = node.openAttribute("block_size");
+	attr.read(H5::PredType::NATIVE_UINT, &block_size_);
+	// block shift
+	attr = node.openAttribute("block_shift");
+	attr.read(H5::PredType::NATIVE_UINT, &block_shift_);
+	// block count
+	attr = node.openAttribute("block_count");
+	attr.read(H5::PredType::NATIVE_UINT, &block_count_);
+	// maximum number of samples per block
+	attr = node.openAttribute("max_samples");
+	attr.read(H5::PredType::NATIVE_UINT64, &max_samples_);
+    }
+    catch (H5::Exception const& e) {
+	throw exception("failed to read parameters from HDF5 input file");
+    }
+}
+
+/**
+ * write parameters to HDF5 file
+ */
+void H5param::write(H5::Group root) const
+{
+    try {
+	H5::Attribute attr;
+	H5::Group node;
+
+	// Lennard-Jones fluid simulation parameters
+	node = root.createGroup("ljfluid");
+
+	// positional coordinates dimension
+	attr = node.createAttribute("dimension", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &dimension_);
+	// number of particles
+	attr = node.createAttribute("particles", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &particles_);
+	// number of cells per dimension
+	attr = node.createAttribute("cells", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &cells_);
+	// particle density
+	attr = node.createAttribute("density", H5::PredType::NATIVE_DOUBLE, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_DOUBLE, &density_);
+	// periodic box length
+	attr = node.createAttribute("box_length", H5::PredType::NATIVE_DOUBLE, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_DOUBLE, &box_length_);
+	// cell length
+	attr = node.createAttribute("cell_length", H5::PredType::NATIVE_DOUBLE, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_DOUBLE, &cell_length_);
+	// simulation timestep
+	attr = node.createAttribute("timestep", H5::PredType::NATIVE_DOUBLE, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_DOUBLE, &timestep_);
+	// Lennard-Jones potential cutoff distance
+	attr = node.createAttribute("cutoff_distance", H5::PredType::NATIVE_DOUBLE, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_DOUBLE, &cutoff_distance_);
+
+	// correlation function parameters
+	node = root.createGroup("autocorrelation");
+
+	// number of simulation steps
+	attr = node.createAttribute("steps", H5::PredType::NATIVE_UINT64, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT64, &steps_);
+	// block size
+	attr = node.createAttribute("block_size", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &block_size_);
+	// block shift
+	attr = node.createAttribute("block_shift", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &block_shift_);
+	// block count
+	attr = node.createAttribute("block_count", H5::PredType::NATIVE_UINT, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT, &block_count_);
+	// maximum number of samples per block
+	attr = node.createAttribute("max_samples", H5::PredType::NATIVE_UINT64, H5S_SCALAR);
+	attr.write(H5::PredType::NATIVE_UINT64, &max_samples_);
+
+	// program info
+	node = root.createGroup("program");
+
+	// program name
+	attr = node.createAttribute("name", H5::StrType(H5::PredType::C_S1, 256), H5S_SCALAR);
+	attr.write(H5::StrType(H5::PredType::C_S1, 256), PROGRAM_NAME);
+	// program version
+	attr = node.createAttribute("version", H5::StrType(H5::PredType::C_S1, 256), H5S_SCALAR);
+	attr.write(H5::StrType(H5::PredType::C_S1, 256), PROGRAM_VERSION);
+    }
+    catch (H5::Exception const& e) {
+	throw exception("failed to write parameters to HDF5 input file");
+    }
+}
+
+} // namespace mdsim
---
+++ src/H5param.hpp
@@ -0,0 +1,300 @@
+/* Molecular Dynamics simulation parameters
+ *
+ * Copyright (C) 2008  Peter Colberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MDSIM_H5PARAM_HPP
+#define MDSIM_H5PARAM_HPP
+
+#include <H5Cpp.h>
+#include <stdint.h>
+#include <string>
+
+
+namespace mdsim
+{
+
+/**
+ * Molecular Dynamics simulation parameters
+ */
+class H5param
+{
+public:
+    /** read parameters from HDF5 file */
+    void read(H5::Group const& root);
+    /** write parameters to HDF5 file */
+    void write(H5::Group root) const;
+
+    /**
+     * returns positional coordinates dimension
+     */
+    unsigned int const& dimension() const
+    {
+	return dimension_;
+    }
+
+    /**
+     * set positional coordinates dimension
+     */
+    void dimension(unsigned int const& value)
+    {
+	dimension_ = value;
+    }
+
+    /**
+     * returns number of particles
+     */
+    unsigned int const& particles() const
+    {
+	return particles_;
+    }
+
+    /**
+     * set number of particles
+     */
+    void particles(unsigned int const& value)
+    {
+	particles_ = value;
+    }
+
+    /**
+     * set number of cells per dimension
+     */
+    void cells(unsigned int const& value)
+    {
+	cells_ = value;
+    }
+
+    /**
+     * returns number of CUDA execution threads
+     */
+    unsigned int const& threads() const
+    {
+	return threads_;
+    }
+
+    /**
+     * set number of CUDA execution threads
+     */
+    void threads(unsigned int const& value)
+    {
+	threads_ = value;
+    }
+
+    /**
+     * returns number of CUDA execution blocks
+     */
+    unsigned int const& blocks() const
+    {
+	return blocks_;
+    }
+
+    /**
+     * set number of CUDA execution blocks
+     */
+    void blocks(unsigned int const& value)
+    {
+	blocks_ = value;
+    }
+
+    /**
+     * returns particle density
+     */
+    double const& density() const
+    {
+	return density_;
+    }
+
+    /**
+     * set particle density
+     */
+    void density(double const& value)
+    {
+	density_ = value;
+    }
+
+    /**
+     * returns periodic box length
+     */
+    double const& box_length() const
+    {
+	return box_length_;
+    }
+
+    /**
+     * set periodic box length
+     */
+    void box_length(double const& value)
+    {
+	box_length_ = value;
+    }
+
+    /**
+     * set cell length
+     */
+    void cell_length(double const& value)
+    {
+	cell_length_ = value;
+    }
+
+    /**
+     * returns simulation timestep
+     */
+    double const& timestep() const
+    {
+	return timestep_;
+    }
+
+    /**
+     * returns simulation timestep
+     */
+    void timestep(double const& value)
+    {
+	timestep_ = value;
+    }
+
+    /**
+     * returns number of simulation steps
+     */
+    uint64_t const& steps() const
+    {
+	return steps_;
+    }
+
+    /**
+     * set number of simulation steps
+     */
+    void steps(uint64_t const& value)
+    {
+	steps_ = value;
+    }
+
+    /**
+     * returns block size
+     */
+    unsigned int const& block_size() const
+    {
+	return block_size_;
+    }
+
+    /**
+     * set block size
+     */
+    void block_size(unsigned int const& value)
+    {
+	block_size_ = value;
+    }
+
+    /**
+     * returns block shift
+     */
+    unsigned int const& block_shift() const
+    {
+	return block_shift_;
+    }
+
+    /**
+     * set block shift
+     */
+    void block_shift(unsigned int const& value)
+    {
+	block_shift_ = value;
+    }
+
+    /**
+     * returns block count
+     */
+    unsigned int const& block_count() const
+    {
+	return block_count_;
+    }
+
+    /**
+     * set block count
+     */
+    void block_count(unsigned int const& value)
+    {
+	block_count_ = value;
+    }
+
+    /**
+     * returns maximum number of samples per block
+     */
+    uint64_t const& max_samples() const
+    {
+	return max_samples_;
+    }
+
+    /**
+     * set maximum number of samples per block
+     */
+    void max_samples(uint64_t const& value)
+    {
+	max_samples_ = value;
+    }
+
+    /**
+     * returns Lennard-Jones potential cutoff distance
+     */
+    double const& cutoff_distance() const
+    {
+	return cutoff_distance_;
+    }
+
+    /**
+     * set Lennard-Jones potential cutoff distance
+     */
+    void cutoff_distance(double const& value)
+    {
+	cutoff_distance_ = value;
+    }
+
+private:
+    /** positional coordinates dimension */
+    unsigned int dimension_;
+    /** number of particles */
+    unsigned int particles_;
+    /** number of cells per dimension */
+    unsigned int cells_;
+    /** number of CUDA execution threads */
+    unsigned int threads_;
+    /** number of CUDA execution blocks */
+    unsigned int blocks_;
+    /** particle density */
+    double density_;
+    /** periodic box length */
+    double box_length_;
+    /** cell length */
+    double cell_length_;
+    /** simulation timestep */
+    double timestep_;
+    /** number of simulation steps */
+    uint64_t steps_;
+    /** block size */
+    unsigned int block_size_;
+    /** block shift */
+    unsigned int block_shift_;
+    /** block count */
+    unsigned int block_count_;
+    /** maximum number of samples per block */
+    uint64_t max_samples_;
+    /** Lennard-Jones potential cutoff distance */
+    double cutoff_distance_;
+};
+
+} // namespace mdsim
+
+#endif /* ! MDSIM_H5PARAM_HPP */
