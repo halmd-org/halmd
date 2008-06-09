ab16ccd128aa4abc52db349b58b0781a8b1bf853
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-06-09 16:15:15 +0200

    Handle signals and abort simulation on keyboard interrupt.

--- src/mdsim/mdsim.hpp
+++ src/mdsim/mdsim.hpp
@@ -20,6 +20,7 @@
 #define MDSIM_MDSIM_HPP
 
 #include <boost/bind.hpp>
+#include <signal.h>
 #include <stdint.h>
 #include "H5param.hpp"
 #include "autocorrelation.hpp"
@@ -47,6 +48,10 @@ public:
     void operator()();
 
 private:
+    /** signal handler */
+    static void handle_signal(int signum);
+
+private:
     /** program options */
     options const& opts;
     /** global simulation parameters */
@@ -55,6 +60,9 @@ private:
     ljfluid<dimension, T> fluid;
     /** block algorithm parameters */
     block_param<dimension, T> block;
+
+    /** signal number */
+    static int signal_;
 };
 
 /**
@@ -216,9 +224,21 @@ void mdsim<dimension, T>::operator()()
     tcf << param;
     tep << param;
 
+    // install signal handlers
+    boost::array<sighandler_t, 3> sigh;
+    sigh[0] = signal(SIGHUP, handle_signal);
+    sigh[1] = signal(SIGINT, handle_signal);
+    sigh[2] = signal(SIGTERM, handle_signal);
+
     LOG("starting MD simulation");
 
     for (uint64_t step = 0; step < param.steps(); ++step) {
+	// abort simulation on signal
+	if (signal_) {
+	    LOG_WARNING("caught signal " << signal_ << " at simulation step " << step);
+	    break;
+	}
+
 	// sample autocorrelation functions
 	fluid.sample(boost::bind(&autocorrelation<dimension, T>::sample, boost::ref(tcf), _1, _2));
 	// sample thermodynamic equilibrium properties
@@ -232,6 +252,11 @@ void mdsim<dimension, T>::operator()()
 
     LOG("finished MD simulation");
 
+    // restore previous signal handlers
+    signal(SIGHUP, sigh[0]);
+    signal(SIGINT, sigh[1]);
+    signal(SIGTERM, sigh[2]);
+
     // write autocorrelation function results to HDF5 file
     tcf.write();
     // write thermodynamic equilibrium properties to HDF5 file
@@ -242,6 +267,19 @@ void mdsim<dimension, T>::operator()()
     tep.close();
     traj.close();
 }
+
+/**
+ * signal handler
+ */
+template <unsigned dimension, typename T>
+void mdsim<dimension, T>::handle_signal(int signum)
+{
+    // store signal number in global variable
+    signal_ = signum;
+}
+
+template <unsigned dimension, typename T>
+int mdsim::mdsim<dimension, T>::signal_(0);
 
 } // namespace mdsim
 
