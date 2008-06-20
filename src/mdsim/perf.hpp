accc077a7685d54cf11b8fd02fbb670077dcfaeb
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-06-20 19:44:19 +0200

    Write measured times to performance data file in seconds.

--- src/mdsim/perf.hpp
+++ src/mdsim/perf.hpp
@@ -108,7 +108,7 @@ void perf<dimension, T>::write(perf_type const& times)
     H5::DataType utid(H5::PredType::NATIVE_UINT64);
 
     // milliseconds per clock tick
-    const double clk = 1000. / sysconf(_SC_CLK_TCK);
+    const double tick = 1. / sysconf(_SC_CLK_TCK);
 
     try {
 	H5::Group root(file_.createGroup("/times"));
@@ -118,10 +118,10 @@ void perf<dimension, T>::write(perf_type const& times)
 	    H5::Group group(root.createGroup(it->first));
 
 	    for (typename perf_type::mapped_type::const_iterator acc = it->second.begin(); acc != it->second.end(); ++acc) {
-		// average time in milliseconds
-		const double mean = clk * acc->second.mean();
-		// standard deviation in milliseconds
-		const double sigma = clk * acc->second.std();
+		// average time in seconds
+		const double mean = acc->second.mean() * tick;
+		// standard deviation in seconds
+		const double sigma = acc->second.std() * tick;
 		// number of calls
 		const uint64_t count = acc->second.count();
 
@@ -131,10 +131,10 @@ void perf<dimension, T>::write(perf_type const& times)
 		time.createAttribute("count", utid, H5S_SCALAR).write(utid, &count);
 
 		if (acc->second.count() > 1) {
-		    LOG(it->first << "/" << acc->first << " average time: " << std::setprecision(4) << mean << " ms (" << std::setprecision(4) << sigma << " ms)");
+		    LOG(it->first << "/" << acc->first << " average time: " << std::setprecision(4) << (mean * 1.E3) << " ms (" << std::setprecision(4) << (sigma * 1.E3) << " ms)");
 		}
 		else {
-		    LOG(it->first << "/" << acc->first << " time: " << std::setprecision(4) << mean << " ms");
+		    LOG(it->first << "/" << acc->first << " time: " << std::setprecision(4) << (mean * 1.E3) << " ms");
 		}
 	    }
 	}
