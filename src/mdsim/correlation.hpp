383eee42f3f97f814234dc488c46239dcacb63b2
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-07-04 01:53:35 +0200

    Log notice if sampling on a block level is finished.

--- src/mdsim/correlation.hpp
+++ src/mdsim/correlation.hpp
@@ -393,7 +393,7 @@ void correlation<dimension, T, U>::sample(vector_type const& r, vector_type cons
     sample_type sample(r, v, m_q_vector);
 
     for (unsigned int i = 0; i < m_block_count; ++i) {
-	if (m_block_samples[i] > m_max_samples)
+	if (m_block_samples[i] >= m_max_samples)
 	    continue;
 	if (step % m_block_freq[i])
 	    continue;
@@ -407,6 +407,9 @@ void correlation<dimension, T, U>::sample(vector_type const& r, vector_type cons
 		m_block[i].clear();
 	    }
 	    m_block_samples[i]++;
+	    if (m_max_samples == m_block_samples[i]) {
+		LOG("finished sampling on block level " << i << " at step " << step);
+	    }
 	}
     }
 }
