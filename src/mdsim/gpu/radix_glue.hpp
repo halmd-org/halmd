5508374c77c58746437b134a0efa01bf21213aa1
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-07-30 22:03:52 +0200

    Fix GPU wrapper function templates for radix sort.

--- src/mdsim/gpu/radix_glue.hpp
+++ src/mdsim/gpu/radix_glue.hpp
@@ -44,7 +44,7 @@ struct __permute
 };
 
 template <typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-inline void permute(T0& t0, T1& t1, T2& t2, T3& t3, T4& t4, T5& t5, T6& t6)
+inline void permute(T0 const& t0, T1& t1, T2 const& t2, T3& t3, T4 const& t4, T5 const& t5, T6 const& t6)
 {
     __permute<typename T3::value_type>::ref(t0, t1, t2, t3, t4, t5, t6);
 }
