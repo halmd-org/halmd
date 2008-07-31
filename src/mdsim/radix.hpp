c144881e7affc82596bb00238a7173f7ee122d43
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2008-07-31 11:41:44 +0200

    Add delayed allocation to parallel radix sort class template.

--- src/mdsim/radix.hpp
+++ src/mdsim/radix.hpp
@@ -34,18 +34,33 @@ template <typename T>
 class radix_sort
 {
 public:
+    radix_sort() : blocks(0), threads(0) {}
+
     /**
      * allocate parallel radix sort for given element count
      */
-    radix_sort(uint const& count, uint const& blocks, uint const& threads)
-    : count(count), blocks(blocks), threads(threads), scan(blocks * threads * gpu::radix::BUCKETS_PER_THREAD, gpu::radix::BUCKET_SIZE)
-    {
-	// allocate radix count buckets
-	g_bucket.resize(blocks * threads * gpu::radix::BUCKETS_PER_THREAD);
+    radix_sort(uint const& count, uint const& blocks, uint const& threads) : blocks(blocks), threads(threads), g_bucket(bucket_size()), g_key(count), g_val(count), g_scan(scan_size(), scan_threads()) {}
 
-	// allocate temporary sort key and value buffers
-	g_key.resize(count);
-	g_val.resize(count);
+    /**
+     * reallocate parallel radix sort for given element count
+     */
+    void resize(uint const& count, uint const& blocks, uint const& threads)
+    {
+	radix_sort temp(count, blocks, threads);
+	swap(temp);
+    }
+
+    /**
+     * swap dimensions and device memory with another parallel radix sort
+     */
+    void swap(radix_sort& r)
+    {
+	std::swap(blocks, r.blocks);
+	std::swap(threads, r.threads);
+	g_bucket.swap(r.g_bucket);
+	g_key.swap(r.g_key);
+	g_val.swap(r.g_val);
+	g_scan.swap(r.g_scan);
     }
 
     /**
@@ -53,26 +68,24 @@ public:
      */
     void operator()(cuda::vector<uint>& g_key_, cuda::vector<T>& g_val_, cuda::stream& stream)
     {
-	assert(g_key_.size() == count);
-	assert(g_val_.size() == count);
-
-	using namespace boost;
+	assert(g_key_.size() == g_key.size());
+	assert(g_val_.size() == g_val.size());
 
 	// assign GPU dual buffers, as in the CUDA SDK radix sort example
-	reference_wrapper<cuda::vector<uint> > key[2] = { ref(g_key_), ref(g_key) };
-	reference_wrapper<cuda::vector<T> > val[2] = { ref(g_val_), ref(g_val) };
+	boost::reference_wrapper<cuda::vector<uint> > key[2] = { boost::ref(g_key_), boost::ref(g_key) };
+	boost::reference_wrapper<cuda::vector<T> > val[2] = { boost::ref(g_val_), boost::ref(g_val) };
 
 	for (uint r = 0; r < 32; r += gpu::radix::RADIX) {
 	    // compute partial radix counts
-	    cuda::configure(blocks, threads, threads * gpu::radix::BUCKETS_PER_THREAD * sizeof(uint), stream);
-	    gpu::radix::histogram_keys(key[0].get(), g_bucket, count, r);
+	    cuda::configure(blocks, threads, shared_mem(), stream);
+	    gpu::radix::histogram_keys(key[0].get(), g_bucket, g_key.size(), r);
 
 	    // parallel prefix sum over radix counts
-	    scan(g_bucket, stream);
+	    g_scan(g_bucket, stream);
 
 	    // permute array
-	    cuda::configure(blocks, threads, threads * gpu::radix::BUCKETS_PER_THREAD * sizeof(uint), stream);
-	    gpu::radix::permute(key[0].get(), key[1].get(), val[0].get(), val[1].get(), g_bucket, count, r);
+	    cuda::configure(blocks, threads, shared_mem(), stream);
+	    gpu::radix::permute(key[0].get(), key[1].get(), val[0].get(), val[1].get(), g_bucket, g_key.size(), r);
 
 	    // swap GPU dual buffers
 	    std::swap(key[0], key[1]);
@@ -81,10 +94,43 @@ public:
     }
 
 private:
-    const uint count, blocks, threads;
+    /**
+     * returns shared memory size in bytes for GPU radix kernels
+     */
+    uint shared_mem()
+    {
+	return threads * gpu::radix::BUCKETS_PER_THREAD * sizeof(uint);
+    }
+
+    /**
+     * returns element count for GPU prefix sum
+     */
+    uint scan_size()
+    {
+	return blocks * threads * gpu::radix::BUCKETS_PER_THREAD;
+    }
+
+    /**
+     * returns number of CUDA threads per block for GPU prefix sum
+     */
+    uint scan_threads()
+    {
+	return gpu::radix::BUCKET_SIZE;
+    }
+
+    /**
+     * returns radix count bucket element count
+     */
+    uint bucket_size()
+    {
+	return blocks * threads * gpu::radix::BUCKETS_PER_THREAD;
+    }
+
+private:
+    uint blocks, threads;
     cuda::vector<uint> g_bucket, g_key;
     cuda::vector<T> g_val;
-    prefix_sum<uint> scan;
+    prefix_sum<uint> g_scan;
 };
 
 } // namespace mdsim
