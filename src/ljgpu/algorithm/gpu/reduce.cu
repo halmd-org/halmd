a08f308f303a73d7f94feb3cf2dfc163ee3062fe
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2009-01-14 00:01:59 +0100

    gpu/reduce: implement reduction by arbitrary transformations.

--- src/ljgpu/algorithm/gpu/reduce.cu
+++ src/ljgpu/algorithm/gpu/reduce.cu
@@ -25,126 +25,123 @@ namespace ljgpu { namespace cu { namespace reduce
 {
 
 /**
- * blockwise sum
+ * unary transformations
  */
-template<typename input_type, typename output_type>
-__global__ void sum(input_type const* g_in, output_type* g_block_sum, uint n)
+template <typename T, typename U>
+__device__ T identity_(U v)
 {
-    extern __shared__ output_type s_sum[];
+    return v;
+}
+
+template <>
+__device__ float3 identity_(float4 v)
+{
+    return make_float3(v.x, v.y, v.z);
+}
+
+template <>
+__device__ float4 identity_(float3 v)
+{
+    return make_float4(v.x, v.y, v.z, 0);
+}
+
+template <typename T, typename U>
+__device__ T square_(U v)
+{
+    return v * v;
+}
+
+/**
+ * binary transformations
+ */
+template <typename T>
+__device__ T sum_(T v1, T v2)
+{
+    return v1 + v2;
+}
+
+/**
+ * parallel reduction
+ */
+template <typename input_type, typename output_type,
+	  output_type (*input_function)(input_type),
+	  output_type (*reduce_function)(output_type, output_type),
+	  output_type (*output_function)(output_type),
+	  typename T, typename U>
+__device__ void reduce(T const* g_in, U* g_block_sum, uint n)
+{
+    extern __shared__ output_type s_vv[];
 
     // load values from global device memory
-    output_type sum = 0;
+    output_type vv = 0;
     for (uint i = GTID; i < n; i += GTDIM) {
-	sum += g_in[i];
+	input_type v = identity_<input_type>(g_in[i]);
+	vv = reduce_function(vv, input_function(v));
     }
-    // sum for this thread
-    s_sum[TID] = sum;
+    // reduced value for this thread
+    s_vv[TID] = vv;
     __syncthreads();
 
-    // compute sum for all threads in block
+    // compute reduced value for all threads in block
     if (TID < 256) {
-	sum = sum + s_sum[TID + 256];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 256]);
+	s_vv[TID] = vv;
     }
     __syncthreads();
     if (TID < 128) {
-	sum = sum + s_sum[TID + 128];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 128]);
+	s_vv[TID] = vv;
     }
     __syncthreads();
     if (TID < 64) {
-	sum = sum + s_sum[TID + 64];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 64]);
+	s_vv[TID] = vv;
     }
     __syncthreads();
     if (TID < 32) {
-	sum = sum + s_sum[TID + 32];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 32]);
+	s_vv[TID] = vv;
     }
     // no further syncs needed within execution warp of 32 threads
     if (TID < 16) {
-	sum = sum + s_sum[TID + 16];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 16]);
+	s_vv[TID] = vv;
     }
     if (TID < 8) {
-	sum = sum + s_sum[TID + 8];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 8]);
+	s_vv[TID] = vv;
     }
     if (TID < 4) {
-	sum = sum + s_sum[TID + 4];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 4]);
+	s_vv[TID] = vv;
     }
     if (TID < 2) {
-	sum = sum + s_sum[TID + 2];
-	s_sum[TID] = sum;
+	vv = reduce_function(vv, s_vv[TID + 2]);
+	s_vv[TID] = vv;
     }
     if (TID < 1) {
-	sum = sum + s_sum[TID + 1];
-	// store block sum in global memory
-	g_block_sum[blockIdx.x] = sum;
+	vv = reduce_function(vv, s_vv[TID + 1]);
+	// store block reduced value in global memory
+	g_block_sum[blockIdx.x] = identity_<U>(output_function(vv));
     }
+}
+
+/**
+ * blockwise sum
+ */
+template <typename input_type, typename output_type, typename T, typename U>
+__global__ void sum(T const* g_in, U* g_block_sum, uint n)
+{
+    reduce<input_type, output_type, identity_, sum_, identity_>(g_in, g_block_sum, n);
 }
 
 /**
  * blockwise absolute maximum
  */
-template <typename input_type, typename coalesced_input_type, typename output_type>
-__global__ void max(coalesced_input_type const* g_in, output_type* g_block_max, uint n)
+template <typename input_type, typename output_type, typename T, typename U>
+__global__ void max(T const* g_in, U* g_block_max, uint n)
 {
-    extern __shared__ output_type s_vv[];
-
-    // load vectors from global device memory
-    output_type vv = 0;
-    for (uint i = GTID; i < n; i += GTDIM) {
-	input_type v = unpack(g_in[i]);
-	vv = fmaxf(vv, v * v);
-    }
-    // maximum squared value for this thread
-    s_vv[TID] = vv;
-    __syncthreads();
-
-    // compute maximum for all threads in block
-    if (TID < 256) {
-	vv = fmaxf(vv, s_vv[TID + 256]);
-	s_vv[TID] = vv;
-    }
-    __syncthreads();
-    if (TID < 128) {
-	vv = fmaxf(vv, s_vv[TID + 128]);
-	s_vv[TID] = vv;
-    }
-    __syncthreads();
-    if (TID < 64) {
-	vv = fmaxf(vv, s_vv[TID + 64]);
-	s_vv[TID] = vv;
-    }
-    __syncthreads();
-    if (TID < 32) {
-	vv = fmaxf(vv, s_vv[TID + 32]);
-	s_vv[TID] = vv;
-    }
-    // no further syncs needed within execution warp of 32 threads
-    if (TID < 16) {
-	vv = fmaxf(vv, s_vv[TID + 16]);
-	s_vv[TID] = vv;
-    }
-    if (TID < 8) {
-	vv = fmaxf(vv, s_vv[TID + 8]);
-	s_vv[TID] = vv;
-    }
-    if (TID < 4) {
-	vv = fmaxf(vv, s_vv[TID + 4]);
-	s_vv[TID] = vv;
-    }
-    if (TID < 2) {
-	vv = fmaxf(vv, s_vv[TID + 2]);
-	s_vv[TID] = vv;
-    }
-    if (TID < 1) {
-	vv = fmaxf(vv, s_vv[TID + 1]);
-	// store block absolute maximum in global memory
-	g_block_max[blockIdx.x] = sqrtf(vv);
-    }
+    reduce<input_type, output_type, square_, fmaxf, sqrtf>(g_in, g_block_max, n);
 }
 
 }}} // namespace ljgpu::cu::reduce
@@ -156,9 +153,10 @@ namespace ljgpu { namespace gpu
  * device function wrappers
  */
 cuda::function<void(float const*, dfloat*, uint)>
-	       reduce::sum(cu::reduce::sum);
+	       reduce::sum(cu::reduce::sum<float, dfloat>);
 cuda::function<void(float4 const*, float*, uint),
 	       void(float2 const*, float*, uint)>
-	       reduce::max(cu::reduce::max<float3>, cu::reduce::max<float2>);
+	       reduce::max(cu::reduce::max<float3, float>,
+			   cu::reduce::max<float2, float>);
 
 }} // namespace ljgpu::gpu
