6f64253f8d8ec6380f7181a20fb53761e3d7088a
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2009-01-19 21:55:28 +0100

    math/gpu/pack: store vector references to ensure atomic shared memory operations.

--- src/ljgpu/math/gpu/pack.cuh
+++ src/ljgpu/math/gpu/pack.cuh
@@ -26,222 +26,138 @@
 namespace ljgpu { namespace cu
 {
 
-struct __ref_float2_int
+template <typename T1, typename T2>
+class __pack;
+
+template <>
+class __pack<vector<float, 2>&, int&>
 {
-    float &x, &y;
-    int &z;
+public:
+    typedef vector<float, 2> vector_type;
+    typedef __pack<vector_type&, int&> type;
 
-    __device__ inline __ref_float2_int(float& x, float& y, int& z)
-	: x(x), y(y), z(z) {}
+    __device__ inline __pack(vector_type& v, int& s): v(v), s(s) {}
 
-    __device__ inline __ref_float2_int& operator=(float3 const& v)
+    __device__ inline type& operator=(float3 const& w)
     {
-	x = v.x;
-	y = v.y;
-	z = __float_as_int(v.z);
+	v = vector_type(w.x, w.y);
+	s = __float_as_int(w.z);
 	return *this;
     }
 
-    __device__ inline __ref_float2_int& operator=(float4 const& v)
+    __device__ inline type& operator=(float4 const& w)
     {
-	x = v.x;
-	y = v.y;
-	z = __float_as_int(v.z);
+	v = vector_type(w.x, w.y);
+	s = __float_as_int(w.z);
 	return *this;
     }
 
     __device__ inline operator float4() const
     {
-	return make_float4(x, y, __int_as_float(z), 0);
+	return make_float4(v.x, v.y, __int_as_float(s), 0);
     }
 
     __device__ inline operator float3() const
     {
-	return make_float3(x, y, __int_as_float(z));
+	return make_float3(v.x, v.y, __int_as_float(s));
     }
+
+private:
+    vector_type& v;
+    int& s;
 };
 
-struct __cref_float2_int
+template <>
+class __pack<vector<float, 2> const&, int const&>
 {
-    float const &x, &y;
-    int const &z;
+public:
+    typedef vector<float, 2> vector_type;
+    typedef __pack<vector_type const&, int const&> type;
 
-    __device__ inline __cref_float2_int(float const& x, float const& y, int const& z)
-	: x(x), y(y), z(z) {}
+    __device__ inline __pack(vector_type const& v, int const& s): v(v), s(s) {}
 
     __device__ inline operator float4() const
     {
-	return make_float4(x, y, __int_as_float(z), 0);
+	return make_float4(v.x, v.y, __int_as_float(s), 0);
     }
 
     __device__ inline operator float3() const
     {
-	return make_float3(x, y, __int_as_float(z));
+	return make_float3(v.x, v.y, __int_as_float(s));
     }
+
+private:
+    vector_type const& v;
+    int const& s;
 };
 
-struct __ref_float3
+template <>
+class __pack<vector<float, 3>&, int&>
 {
-    float &x, &y, &z;
+public:
+    typedef vector<float, 3> vector_type;
+    typedef __pack<vector_type&, int&> type;
 
-    __device__ inline __ref_float3(float& x, float& y, float& z)
-	: x(x), y(y), z(z) {}
+    __device__ inline __pack(vector_type& v, int& s): v(v), s(s) {}
 
-    __device__ inline __ref_float3& operator=(float3 const& v)
+    __device__ inline type& operator=(float4 const& w)
     {
-	x = v.x;
-	y = v.y;
-	z = v.z;
-	return *this;
-    }
-
-    __device__ inline __ref_float3& operator=(float4 const& v)
-    {
-	x = v.x;
-	y = v.y;
-	z = v.z;
-	return *this;
-    }
-
-    __device__ inline operator float4() const
-    {
-	return make_float4(x, y, z, 0);
-    }
-
-    __device__ inline operator float3() const
-    {
-	return make_float3(x, y, z);
-    }
-};
-
-struct __cref_float3
-{
-    float const &x, &y, &z;
-
-    __device__ inline __cref_float3(float const& x, float const& y, float const& z)
-	: x(x), y(y), z(z) {}
-
-    __device__ inline operator float4() const
-    {
-	return make_float4(x, y, z, 0);
-    }
-
-    __device__ inline operator float3() const
-    {
-	return make_float3(x, y, z);
-    }
-};
-
-struct __ref_float3_int
-{
-    float &x, &y, &z;
-    int &w;
-
-    __device__ inline __ref_float3_int(float& x, float& y, float& z, int& w)
-	: x(x), y(y), z(z), w(w) {}
-
-    __device__ inline __ref_float3_int& operator=(float4 const& v)
-    {
-	x = v.x;
-	y = v.y;
-	z = v.z;
-	w = __float_as_int(v.w);
+	v = vector_type(w.x, w.y, w.z);
+	s = __float_as_int(w.w);
 	return *this;
     }
 
     __device__ inline operator float4() const
     {
-	return make_float4(x, y, z, __int_as_float(w));
+	return make_float4(v.x, v.y, v.z, __int_as_float(s));
     }
+
+private:
+    vector_type& v;
+    int& s;
 };
 
-struct __cref_float3_int
+template <>
+class __pack<vector<float, 3> const&, int const&>
 {
-    float const &x, &y, &z;
-    int const &w;
+public:
+    typedef vector<float, 3> vector_type;
+    typedef __pack<vector_type const&, int const&> type;
 
-    __device__ inline __cref_float3_int(float const& x, float const& y, float const& z, int const& w)
-	: x(x), y(y), z(z), w(w) {}
+    __device__ inline __pack(vector_type const& v, int const& s): v(v), s(s) {}
 
     __device__ inline operator float4() const
     {
-	return make_float4(x, y, z, __int_as_float(w));
+	return make_float4(v.x, v.y, v.z, __int_as_float(s));
     }
+
+private:
+    vector_type const& v;
+    int const& s;
 };
 
-struct __ref_float4
+__device__ inline __pack<vector<float, 2>&, int&>
+operator,(vector<float, 2>& v, int& s)
 {
-    float &x, &y, &z, &w;
-
-    __device__ inline __ref_float4(float& x, float& y, float& z, float& w)
-	: x(x), y(y), z(z), w(w) {}
-
-    __device__ inline __ref_float4& operator=(float4 const& v)
-    {
-	x = v.x;
-	y = v.y;
-	z = v.z;
-	w = v.w;
-	return *this;
-    }
-
-    __device__ inline operator float4() const
-    {
-	return make_float4(x, y, z, w);
-    }
-};
-
-struct __cref_float4
-{
-    float const &x, &y, &z, &w;
-
-    __device__ inline __cref_float4(float const& x, float const& y, float const& z, float const& w)
-	: x(x), y(y), z(z), w(w) {}
-
-    __device__ inline operator float4() const
-    {
-	return make_float4(x, y, z, w);
-    }
-};
-
-__device__ inline __ref_float2_int operator,(vector<float, 2>& v, int& s)
-{
-    return __ref_float2_int(v.x, v.y, s);
+    return __pack<vector<float, 2>&, int&>(v, s);
 }
 
-__device__ inline __cref_float2_int operator,(vector<float, 2> const& v, int const& s)
+__device__ inline __pack<vector<float, 2> const&, int const&>
+operator,(vector<float, 2> const& v, int const& s)
 {
-    return __cref_float2_int(v.x, v.y, s);
+    return __pack<vector<float, 2> const&, int const&>(v, s);
 }
 
-__device__ inline __ref_float3 operator,(vector<float, 2>& v, float& s)
+__device__ inline __pack<vector<float, 3>&, int&>
+operator,(vector<float, 3>& v, int& s)
 {
-    return __ref_float3(v.x, v.y, s);
+    return __pack<vector<float, 3>&, int&>(v, s);
 }
 
-__device__ inline __cref_float3 operator,(vector<float, 2> const& v, float const& s)
+__device__ inline __pack<vector<float, 3> const&, int const&>
+operator,(vector<float, 3> const& v, int const& s)
 {
-    return __cref_float3(v.x, v.y, s);
-}
-
-__device__ inline __ref_float3_int operator,(vector<float, 3>& v, int& s)
-{
-    return __ref_float3_int(v.x, v.y, v.z, s);
-}
-
-__device__ inline __cref_float3_int operator,(vector<float, 3> const& v, int const& s)
-{
-    return __cref_float3_int(v.x, v.y, v.z, s);
-}
-
-__device__ inline __ref_float4 operator,(vector<float, 3>& v, float& s)
-{
-    return __ref_float4(v.x, v.y, v.z, s);
-}
-
-__device__ inline __cref_float4 operator,(vector<float, 3> const& v, float const& s)
-{
-    return __cref_float4(v.x, v.y, v.z, s);
+    return __pack<vector<float, 3> const&, int const&>(v, s);
 }
 
 }} // namespace ljgpu::cu
