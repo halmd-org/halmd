57b1bd6fd3dac3603dfa510720c47d75a5cd83da
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2009-01-25 00:34:55 +0100

    math/gpu/dsfun: add double-single division from DSFUN90.

--- src/ljgpu/math/gpu/dsfun.cuh
+++ src/ljgpu/math/gpu/dsfun.cuh
@@ -91,6 +91,68 @@ __device__ __host__ inline void __dsmul(float &c0, T &c1, const float a0, const 
 }
 
 /**
+ * This divides the DS number DSA by the DS number DSB to yield the DS quotient DSC.
+ */
+__device__ __host__ inline void __dsdiv(float &dsc0, float &dsc1, float const dsa0, float const dsa1, float const dsb0, float const dsb1)
+{
+    // Compute a DP approximation to the quotient.
+
+    float s1 = dsa0 / dsb0;
+    //>
+    // On systems with a fused multiply add, such as IBM systems, it is faster to
+    // uncomment the next two lines and comment out the following lines until //>.
+    // On other systems, do the opposite.
+
+    float c11 = s1 * dsb0;
+    float c21 = s1 * dsb0 - c11;
+
+    // This splits s1 and dsb0 into high-order and low-order words.
+
+    // float const split = 8193;
+    // float cona = s1 * split;
+    // float conb = dsb0 * split;
+    // float a1 = cona - (cona - s1);
+    // float b1 = conb - (conb - dsb0);
+    // float a2 = s1 - a1;
+    // float b2 = dsb0 - b1;
+
+    // Multiply s1 * dsb0 using Dekker's method.
+
+    // float c11 = s1 * dsb0;
+    // float c21 = (((a1 * b1 - c11) + a1 * b2) + a2 * b1) + a2 * b2;
+    //>
+    // Compute s1 * dsb1 (only high-order word is needed).
+
+    float c2 = s1 * dsb1;
+
+    // Compute (c11, c21) + c2 using Knuth's trick.
+
+    float t1 = c11 + c2;
+    float e = t1 - c11;
+    float t2 = ((c2 - e) + (c11 - (t1 - e))) + c21;
+
+    // The result is t1 + t2, after normalization.
+
+    float t12 = t1 + t2;
+    float t22 = t2 - (t12 - t1);
+
+    // Compute dsa - (t12, t22) using Knuth's trick.
+
+    float t11 = dsa0 - t12;
+    e = t11 - dsa0;
+    float t21 = ((-t12 - e) + (dsa0 - (t11 - e))) + dsa1 - t22;
+
+    // Compute high-order word of (t11, t21) and divide by dsb0.
+
+    float s2 = (t11 + t21) / dsb0;
+
+    // The result is s1 + s2, after normalization.
+
+    dsc0 = s1 + s2;
+    dsc1 = s2 - (dsc0 - s1);
+}
+
+/**
  * double-single floating point value
  */
 __device__ __host__  struct dfloat
@@ -132,6 +194,12 @@ __device__ __host__ inline dfloat& operator*=(dfloat& v, dfloat const& w)
     return v;
 }
 
+__device__ __host__ inline dfloat& operator/=(dfloat& v, dfloat const& w)
+{
+    __dsdiv(v.f0, v.f1, v.f0, v.f1, w.f0, w.f1);
+    return v;
+}
+
 __device__ __host__ inline dfloat operator+(dfloat v, dfloat const& w)
 {
     v += w;
@@ -147,6 +215,12 @@ __device__ __host__ inline dfloat operator-(dfloat v, dfloat const& w)
 __device__ __host__ inline dfloat operator*(dfloat v, dfloat const& w)
 {
     v *= w;
+    return v;
+}
+
+__device__ __host__ inline dfloat operator/(dfloat v, dfloat const& w)
+{
+    v /= w;
     return v;
 }
 
