f5010076b248bb5cb1cb76496083e4c2f6883acd
Peter Colberg <peter.colberg@physik.uni-muenchen.de>
2009-04-05 21:50:16 +0200

    nvlock: use canonical CUDA toolkit include path.

--- src/nvlock/nvlock.cpp
+++ src/nvlock/nvlock.cpp
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <cuda/cuda.h>
+#include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/file.h>
