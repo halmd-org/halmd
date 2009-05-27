449e1a191c8698e8b51070f484e4c11f361f814d
Peter Colberg <peter@colberg.org>
2009-05-28 00:03:31 +0200

    Polish ljgpu logo with petrol blue “Anna”.

--- doc/_images/logo.py
+++ doc/_images/logo.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python
 from pylab import *
+import sys
+if len(sys.argv) < 2:
+    sys.exit("Usage: logo.py [OUTPUT] ...")
 fig = figure(figsize=(1, 1))
 ax = axes((0, 0, 1, 1), frameon=False)
 lj = lambda r: 4 * (pow(r, -12) - pow(r, -6))
 x = linspace(0.969, 2.95, 1000)
-ax.plot(x, lj(x), color="darkblue", lw=2, alpha=0.8)
-ax.plot(x, lj(x), color="darkblue", lw=4, alpha=0.6)
-ax.plot(x, lj(x), color="darkblue", lw=6, alpha=0.4)
-ax.plot(x, lj(x), color="darkblue", lw=8, alpha=0.2)
-ax.plot(x, lj(x), color="darkblue", lw=10, alpha=0.1)
+ax.plot(x, lj(x), color="#00545c", lw=2, alpha=0.8)
+ax.plot(x, lj(x), color="#00545c", lw=4, alpha=0.6)
+ax.plot(x, lj(x), color="#00545c", lw=6, alpha=0.4)
+ax.plot(x, lj(x), color="#00545c", lw=8, alpha=0.2)
+ax.plot(x, lj(x), color="#00545c", lw=10, alpha=0.1)
 J = array(((0, 1, 1), (1, 1, 2)))
 G = array(((0, 0, 1, 1), (1, 2, 1, 2)))
 P = array(((0, 0, 0, 1), (0, 1, 2, 2)))
 U = array(((0, 0, 1), (0, 2, 0)))
-ax.scatter(J[0] * 0.3 + 1.95, J[1] * 0.3 - 0.95, s=120, color="darkblue", alpha=0.2)
-ax.scatter(J[0] * 0.3 + 1.95, J[1] * 0.3 - 0.95, s=40, color="darkblue", alpha=0.8)
+ax.scatter(J[0] * 0.3 + 1.95, J[1] * 0.3 - 0.95, s=120, color="#00545c", alpha=0.2)
+ax.scatter(J[0] * 0.3 + 1.95, J[1] * 0.3 - 0.95, s=40, color="#00545c", alpha=0.8)
 ax.scatter(G[0] * 0.3 + 1.35, G[1] * 0.3 + 0.35, s=120, color="red", alpha=0.2)
 ax.scatter(G[0] * 0.3 + 1.35, G[1] * 0.3 + 0.35, s=40, color="red", alpha=0.8)
 ax.scatter(P[0] * 0.3 + 1.95, P[1] * 0.3 + 0.35, s=120, color="green", alpha=0.2)
 ax.scatter(P[0] * 0.3 + 1.95, P[1] * 0.3 + 0.35, s=40, color="green", alpha=0.8)
-ax.scatter(U[0] * 0.3 + 2.55, U[1] * 0.3 + 0.35, s=120, color="orange", alpha=0.2)
-ax.scatter(U[0] * 0.3 + 2.55, U[1] * 0.3 + 0.35, s=40, color="orange", alpha=0.8)
+ax.scatter(U[0] * 0.3 + 2.55, U[1] * 0.3 + 0.35, s=120, color="darkblue", alpha=0.2)
+ax.scatter(U[0] * 0.3 + 2.55, U[1] * 0.3 + 0.35, s=40, color="darkblue", alpha=0.8)
 ax.axis((0.8, 3.1, -1.15, 1.15))
 for tick in ax.xaxis.get_major_ticks():
     tick.set_visible(False)
 for tick in ax.yaxis.get_major_ticks():
     tick.set_visible(False)
-savefig("logo.png")
+for fn in sys.argv[1:]:
+    savefig(fn)
