75d1983ec8a8a6f374d6ed0a0d11a23ff6d61c74
Peter Colberg <peter@colberg.org>
2009-12-14 23:55:43 +0100

    Render project name in logo.
    Using the (non-free) Trebuchet font which is the default Sphinx header font.
    
    Note that the UTF-8 apostrophe (’) had to be dispensed with, as
    matplotlib has trouble rendering that character at the correct
    line height if preceded by a capital letter.

--- doc/_static/halmd.py
+++ doc/_static/halmd.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from pylab import *
 import sys
 if len(sys.argv) < 2:
     sys.exit("Usage: halmd.py [OUTPUT] ...")
-fig = figure(figsize=(1, 1))
-ax = axes((0, 0, 1, 1), frameon=False)
+fig = figure(figsize=(8, 1))
+ax = axes((0, 0, 0.125, 1), frameon=False)
 lj = lambda r: 4 * (pow(r, -12) - pow(r, -6))
 x = linspace(0.969, 2.95, 1000)
 ax.plot(x, lj(x), color="#00545c", lw=2, alpha=0.8)
@@ -25,12 +26,19 @@ ax.scatter(L[0] * 0.3 + 2.55, L[1] * 0.3 + 0.35, s=120, color="darkblue", alpha=
 ax.scatter(L[0] * 0.3 + 2.55, L[1] * 0.3 + 0.35, s=40, color="darkblue", alpha=0.8)
 ax.scatter(M[0] * 0.3 + 1.95, M[1] * 0.3 - 0.95, s=120, color="#00545c", alpha=0.2)
 ax.scatter(M[0] * 0.3 + 1.95, M[1] * 0.3 - 0.95, s=40, color="#00545c", alpha=0.8)
-ax.scatter(D[0] * 0.3 + 2.55, D[1] * 0.3 - 0.95, s=120, color="#00545c", alpha=0.2)
-ax.scatter(D[0] * 0.3 + 2.55, D[1] * 0.3 - 0.95, s=40, color="#00545c", alpha=0.8)
+ax.scatter(D[0] * 0.3 + 2.55, D[1] * 0.3 - 0.95, s=120, color="grey", alpha=0.2)
+ax.scatter(D[0] * 0.3 + 2.55, D[1] * 0.3 - 0.95, s=40, color="grey", alpha=0.8)
 ax.axis((0.8, 3.1, -1.15, 1.15))
 for tick in ax.xaxis.get_major_ticks():
     tick.set_visible(False)
 for tick in ax.yaxis.get_major_ticks():
     tick.set_visible(False)
+rc("font", **{"family": "sans-serif", "sans-serif": ["Trebuchet MS"]})
+fig.text(0.145, 0.50, "HALMD: HAL's MD package",
+        color="#00545c", alpha=0.8, fontsize=42,
+        ha="left", va="center", transform=ax.transAxes)
+fig.text(0.155, 0.15, "Highly Accelerated Large-scale Molecular Dynamics",
+        color="grey", alpha=0.8, fontsize=14,
+        ha="left", va="center", transform=ax.transAxes)
 for fn in sys.argv[1:]:
     savefig(fn)
