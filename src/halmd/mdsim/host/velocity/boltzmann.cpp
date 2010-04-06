6ffcf9a937d21a56eaa82fc15df003e9e5509bd7
Peter Colberg <peter.colberg@utoronto.ca>
2010-04-06 15:39:47 -0400

    Add Maxwell-Boltzmann velocity distribution generator.

---
+++ src/halmd/mdsim/host/velocity/boltzmann.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2008-2010  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <halmd/mdsim/host/velocity/boltzmann.hpp>
+#include <halmd/util/log.hpp>
+
+namespace halmd { namespace mdsim { namespace host { namespace velocity
+{
+
+using namespace boost;
+using namespace std;
+
+template <int dimension, typename float_type>
+boltzmann<dimension, float_type>::boltzmann(particle_ptr particle, random_ptr random, options const& vm)
+    // dependency injection
+    : particle(static_pointer_cast<particle_type>(particle))
+    , random(static_pointer_cast<random_type>(random))
+{
+    // parse options
+    temp_ = vm["temperature"].as<float>();
+}
+
+/**
+ * Initialise velocities from Maxwell-Boltzmann distribution
+ */
+template <int dimension, typename float_type>
+void boltzmann<dimension, float_type>::set()
+{
+    float_type r, sigma = sqrt(temp_);
+    for (size_t i = 0; i < particle->nbox; ++i) {
+        vector_type& v = particle->v[i];
+        if (dimension == 3) {
+            if (i % 2) {
+                v[0] = r;
+                random->normal(v[1], v[2], sigma);
+            }
+            else {
+                random->normal(v[0], v[1], sigma);
+                random->normal(v[2], r, sigma);
+            }
+        }
+        else {
+            random->normal(v[0], v[1], sigma);
+        }
+    }
+    LOG("assigned Maxwell-Boltzmann velocity distribution: T = " << temp_);
+}
+
+// explicit instantiation
+#ifndef USE_HOST_SINGLE_PRECISION
+template class boltzmann<3, double>;
+template class boltzmann<2, double>;
+#else
+template class boltzmann<3, float>;
+template class boltzmann<2, float>;
+#endif
+
+}}}} // namespace halmd::mdsim::host::velocity
---
+++ src/halmd/mdsim/host/velocity/boltzmann.hpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2008-2010  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_MDSIM_HOST_VELOCITY_BOLTZMANN_HPP
+#define HALMD_MDSIM_HOST_VELOCITY_BOLTZMANN_HPP
+
+#include <halmd/math/vector2d.hpp>
+#include <halmd/math/vector3d.hpp>
+#include <halmd/mdsim/host/particle.hpp>
+#include <halmd/mdsim/host/random.hpp>
+#include <halmd/mdsim/velocity.hpp>
+#include <halmd/options.hpp>
+
+namespace halmd { namespace mdsim { namespace host { namespace velocity
+{
+
+template <int dimension, typename float_type>
+class boltzmann : public mdsim::velocity<dimension, float_type>
+{
+public:
+    typedef vector<float_type, dimension> vector_type;
+
+    typedef host::particle<dimension, float_type> particle_type;
+    typedef boost::shared_ptr<mdsim::particle<dimension, float_type> > particle_ptr;
+    typedef host::random random_type;
+    typedef boost::shared_ptr<mdsim::random> random_ptr;
+
+public:
+    boost::shared_ptr<particle_type> particle;
+    boost::shared_ptr<random_type> random;
+
+public:
+    boltzmann(particle_ptr particle, random_ptr random, options const& vm);
+    virtual ~boltzmann() {};
+    void set();
+
+protected:
+    /** temperature */
+    float_type temp_;
+};
+
+}}}} // namespace halmd::mdsim::host::velocity
+
+#endif /* ! HALMD_MDSIM_HOST_VELOCITY_BOLTZMANN_HPP */
