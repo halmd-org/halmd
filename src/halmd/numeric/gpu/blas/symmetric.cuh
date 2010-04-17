7fc65db4158599daf761ae9cf2118961b0b0e309
Peter Colberg <peter.colberg@utoronto.ca>
2010-04-17 14:36:16 -0400

    Add GPU function to compute storage index for lower symmetric matrix.

---
+++ src/halmd/numeric/gpu/blas/symmetric.cuh
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2008-2010  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_NUMERIC_GPU_BLAS_SYMMETRIC_CUH
+#define HALMD_NUMERIC_GPU_BLAS_SYMMETRIC_CUH
+
+#include <boost/type_traits/is_convertible.hpp>
+#include <boost/utility/enable_if.hpp>
+#include <cuda_runtime.h>
+
+#include <halmd/numeric/gpu/blas/dsfloat.cuh>
+#include <halmd/numeric/gpu/blas/vector/storage.cuh>
+
+namespace halmd { namespace numeric { namespace gpu { namespace blas
+{
+
+namespace symmetric_matrix
+{
+
+/**
+ * Compute storage index for lower symmetric matrix
+ */
+__device__ inline unsigned int lower_index(unsigned int row, unsigned int col)
+{
+    unsigned int row_lower = col > row ? col : row;
+    unsigned int col_lower = col < row ? col : row;
+    return col_lower + ((row_lower * (row_lower + 1)) / 2);
+}
+
+} // namespace symmetric_matrix
+
+}}}} // namespace halmd::numeric::gpu::blas
+
+#endif /* ! HALMD_NUMERIC_GPU_BLAS_SYMMETRIC_CUH */
