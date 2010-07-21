a73b8b49d20d158a4e77b8b33d6db835e11638cc
Peter Colberg <peter.colberg@utoronto.ca>
2010-07-21 18:31:43 -0400

    test: fix GPU rand48 random number generator unit test.

--- src/halmd/test/random/gpu/rand48.cpp
+++ src/halmd/test/random/gpu/rand48.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2008  Peter Colberg
+ * Copyright © 2008-2010  Peter Colberg and Felix Höfling
  *
  * This file is part of HALMD.
  *
@@ -17,145 +17,89 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_random_gpu_rand48
+#include <boost/test/unit_test.hpp>
+
 #include <algorithm>
-#include <boost/array.hpp>
-#include <boost/foreach.hpp>
-#include <boost/program_options.hpp>
-#include <cmath>
+#include <cstdlib>
 #include <iomanip>
-#include <iostream>
-#include <libgen.h>
-#include <stdexcept>
-#include <stdio.h>
 #include <vector>
 
 #include <halmd/random/gpu/rand48.hpp>
+#include <halmd/random/gpu/random_kernel.hpp>
 #include <halmd/util/timer.hpp>
-
-using namespace halmd;
-
-namespace po = boost::program_options;
-
-#define PROGRAM_NAME basename(argv[0])
 
 //
 // Parallel GPU rand48 random number generator test
 //
 
-int main(int argc, char **argv)
+// "global fixture:" select CUDA device
+struct set_cuda_device {
+    set_cuda_device() {
+        try {
+            cuda::device::set(0);
+        }
+        catch (cuda::error const& e) {
+            std::cerr << "CUDA error: " << e.what() << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    ~set_cuda_device() {}  // release device here?
+};
+
+BOOST_GLOBAL_FIXTURE( set_cuda_device );
+
+BOOST_AUTO_TEST_CASE( compare_variates )
 {
-    // program options
-    uint count, blocks, threads, seed;
-    unsigned short device;
-    bool verbose;
+    const unsigned blocks = 64;
+    const unsigned threads = 128;
+    const unsigned seed = 42;
+    const unsigned count = 10000000u;
 
-    try {
-        // parse command line options
-        po::options_description opts("Program options");
-        opts.add_options()
-            ("count,N", po::value<uint>(&count)->default_value(10000000),
-             "random number count")
-            ("seed,S", po::value<uint>(&seed)->default_value(42),
-             "random number generator seed")
-            ("device,D", po::value<unsigned short>(&device)->default_value(0),
-             "CUDA device")
-            ("blocks,B", po::value<uint>(&blocks)->default_value(64),
-             "number of blocks in grid")
-            ("threads,T", po::value<uint>(&threads)->default_value(128),
-             "number of threads per block")
-            ("verbose,v", po::bool_switch(&verbose),
-             "print results")
-            ("help,h", "display this help and exit");
+    BOOST_TEST_MESSAGE("number of integers: " << count);
+    BOOST_TEST_MESSAGE("blocks: " << blocks);
+    BOOST_TEST_MESSAGE("threads: " << threads);
+    BOOST_TEST_MESSAGE("seed: " << seed);
 
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, opts), vm);
-        po::notify(vm);
+    using halmd::random::gpu::rand48;
+    halmd::high_resolution_timer start, stop;
 
-        if (count < 1) {
-            throw std::logic_error("number of elements must be non-zero");
-        }
-        if (blocks < 1) {
-            throw std::logic_error("number of blocks must be non-zero");
-        }
-        if (threads < 16) {
-            throw std::logic_error("number of threads must be at least 16");
-        }
+    // seed GPU random number generator
+    rand48 rng(blocks, threads);
+    start.record();
+    rng.seed(seed);
+    cuda::copy(rng.rng(), halmd::random::gpu::get_random_kernel<rand48::rng_type>().rng);
+    stop.record();
 
-        // print help
-        if (vm.count("help")) {
-            std::cerr << "Usage: " << PROGRAM_NAME << " [OPTION]...\n\n" << opts << "\n";
-            return EXIT_SUCCESS;
-        }
-    }
-    catch (std::exception const& e) {
-        std::cerr << PROGRAM_NAME << ": " << e.what() << "\n";
-        std::cerr << "Try `" << PROGRAM_NAME << " --help' for more information.\n";
-        return EXIT_FAILURE;
-    }
+    BOOST_TEST_MESSAGE("seed GPU time: " << std::fixed << std::setprecision(3)
+                       << (stop - start) * 1e3 << " ms");
 
-    try {
-        cuda::device::set(device);
-        boost::array<high_resolution_timer, 2> start, stop;
+    // parallel GPU rand48
+    cuda::vector<uint> g_array(count);
+    start.record();
+    cuda::configure(rng.dim.grid, rng.dim.block);
+    halmd::random::gpu::get_random_kernel<rand48::rng_type>().get(g_array, g_array.size());
+    cuda::thread::synchronize();
+    stop.record();
 
-        std::cout << "integers:\t\t" << std::setw(13) << count << "\n";
-        std::cout << "blocks: \t\t" << std::setw(13) << blocks << "\n";
-        std::cout << "threads:\t\t" << std::setw(13) << threads << "\n";
-        std::cout << "seed:   \t\t" << std::setw(13) << seed << "\n";
-        std::cout << std::endl;
+    BOOST_TEST_MESSAGE("rand48 GPU time: " << std::fixed << std::setprecision(3)
+                       << (stop - start) * 1e3 << " ms");
 
-        // seed GPU random number generator
-        halmd::random::gpu::rand48 rng(blocks, threads);
-        start[0].record();
-        rng.seed(seed);
-        stop[0].record();
+    cuda::host::vector<uint> h_array(count);
+    cuda::copy(g_array, h_array);
 
-        // parallel GPU rand48
-        cuda::vector<uint> g_array(count);
-        start[1].record();
-        // FIXME rng.get(g_array);
-        stop[1].record();
+    // serial GNU C library rand48
+    std::vector<uint> h_array2(count);
+    srand48(seed);
+    start.record();
+    std::generate(h_array2.begin(), h_array2.end(), mrand48);
+    stop.record();
 
-        cuda::host::vector<uint> h_array(count);
-        cuda::copy(g_array, h_array);
+    BOOST_TEST_MESSAGE("rand48 CPU time: " << std::fixed << std::setprecision(3)
+                       << (stop - start) * 1e3 << " ms");
 
-        // serial GNU C library rand48
-        std::vector<uint> h_array2(count);
-        srand48(seed);
-        real_timer timer;
-        timer.start();
-        std::generate(h_array2.begin(), h_array2.end(), mrand48);
-        timer.stop();
-
-        if (verbose) {
-            // write results to stdout
-            for (uint i = 0; i < count; ++i) {
-                std::cout << "[" << std::setw(6) << i << "]\t[GPU] " << std::setw(10)
-                          << h_array[i] << ",\t[CPU] " << std::setw(10) << h_array2[i]
-                          << ((h_array[i] != h_array2[i]) ? " << MISMATCH\n" : "\n");
-            }
-        }
-
-        // print measured GPU times
-        boost::array<std::string, 4> title = {{ "seed", "rand48", }};
-        for (uint j = 0; j < start.size(); ++j) {
-            std::cout << title[j] << " GPU time:  \t" << std::setw(10) << std::fixed << std::setprecision(3) << (stop[j] - start[j]) * 1e3 << " ms" << std::endl;
-        }
-        // print measured CPU time
-        std::cout << "rand48 CPU time:  \t" << std::setw(10) << std::fixed << std::setprecision(3) << timer.elapsed() * 1e3 << " ms" << std::endl;
-
-        // verify results
-        if (!std::equal(h_array.begin(), h_array.end(), h_array2.begin())) {
-            throw std::logic_error("GPU and CPU random number mismatch");
-        }
-    }
-    catch (cuda::error const& e) {
-        std::cerr << PROGRAM_NAME << ": CUDA ERROR: " << e.what() << "\n";
-        return EXIT_FAILURE;
-    }
-    catch (std::exception const& e) {
-        std::cerr << PROGRAM_NAME << ": ERROR: " << e.what() << "\n";
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    // compare GPU and CPU variates
+    BOOST_CHECK_MESSAGE(std::equal(h_array.begin(), h_array.end(), h_array2.begin()),
+                        "random variates mismatch");
 }
