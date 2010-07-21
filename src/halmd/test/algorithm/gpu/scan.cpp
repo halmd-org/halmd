6b1bbffd5cebdbd549a9d8d9fa9a5c5774621141
Peter Colberg <peter.colberg@utoronto.ca>
2010-07-21 19:31:40 -0400

    test: migrate scan test to Boost Unit Test Framework.

--- src/halmd/test/algorithm/gpu/scan.cpp
+++ src/halmd/test/algorithm/gpu/scan.cpp
@@ -17,128 +17,89 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_scan
+#include <boost/test/unit_test.hpp>
+#include <boost/test/parameterized_test.hpp>
+
 #include <algorithm>
-#include <boost/array.hpp>
-#include <boost/foreach.hpp>
-#include <boost/program_options.hpp>
-#include <cmath>
+#include <boost/assign.hpp>
 #include <iomanip>
-#include <iostream>
-#include <libgen.h>
-#include <stdexcept>
-#include <stdio.h>
 #include <vector>
 
 #include <halmd/algorithm/gpu/scan.hpp>
+#include <halmd/test/tools/cuda.hpp>
 #include <halmd/util/timer.hpp>
 
-using namespace halmd;
+BOOST_GLOBAL_FIXTURE( set_cuda_device );
 
-namespace po = boost::program_options;
-
-#define PROGRAM_NAME basename(argv[0])
-
-int main(int argc, char **argv)
+void compare_scan( size_t count )
 {
-    // program options
-    uint count, threads;
-    unsigned short device;
-    bool verbose;
+    const unsigned threads = 256;
 
-    try {
-        // parse command line options
-        po::options_description opts("Program options");
-        opts.add_options()
-            ("count,N", po::value<uint>(&count)->default_value(10000),
-             "number of elements")
-            ("device,D", po::value<unsigned short>(&device)->default_value(0),
-             "CUDA device")
-            ("threads,T", po::value<uint>(&threads)->default_value(256),
-             "number of threads per block")
-            ("verbose,v", po::bool_switch(&verbose),
-             "print results")
-            ("help,h", "display this help and exit");
+    BOOST_TEST_MESSAGE( "Scanning " << count << " elements");
 
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, opts), vm);
-        po::notify(vm);
+    halmd::high_resolution_timer start, stop;
 
-        if (count < 2) {
-            throw std::logic_error("number of elements must be greater than 1");
-        }
-        if (threads == 0 || threads & (threads - 1)) {
-            throw std::logic_error("number of threads must be a power of 2");
-        }
-
-        // print help
-        if (vm.count("help")) {
-            std::cerr << "Usage: " << PROGRAM_NAME << " [OPTION]...\n\n" << opts << "\n";
-            return EXIT_SUCCESS;
-        }
+    // generate array of ascending integers
+    cuda::host::vector<uint> h_array(count);
+    cuda::vector<uint> g_array(count);
+    for (uint i = 0; i < count; ++i) {
+        h_array[i] = i + 1;
     }
-    catch (std::exception const& e) {
-        std::cerr << PROGRAM_NAME << ": " << e.what() << "\n";
-        std::cerr << "Try `" << PROGRAM_NAME << " --help' for more information.\n";
-        return EXIT_FAILURE;
+    cuda::copy(h_array, g_array);
+
+    // parallel exclusive prefix sum
+    halmd::algorithm::gpu::scan<uint> scan(count, threads);
+    cuda::host::vector<uint> h_array2(count);
+    start.record();
+    scan(g_array);
+    cuda::thread::synchronize();
+    stop.record();
+    cuda::copy(g_array, h_array2);
+
+    BOOST_TEST_MESSAGE("GPU time: " << std::fixed << std::setprecision(3)
+                       << (stop - start) * 1e3 << " ms");
+
+    // serial prefix sum
+    std::vector<uint> h_array3(count);
+    start.record();
+    h_array3[0] = 0;
+    for (uint i = 1; i < count; ++i) {
+        h_array3[i] = h_array[i - 1] + h_array3[i - 1];
     }
+    stop.record();
 
-    try {
-        cuda::device::set(device);
-        high_resolution_timer start, stop;
+    BOOST_TEST_MESSAGE("CPU time: " << std::fixed << std::setprecision(3)
+                       << (stop - start) * 1e3 << " ms");
 
-        // generate array of ascending integers
-        cuda::host::vector<uint> h_array(count);
-        cuda::vector<uint> g_array(count);
-        for (uint i = 0; i < count; ++i)
-            h_array[i] = i + 1;
-        cuda::copy(h_array, g_array);
-
-        // parallel exclusive prefix sum
-        algorithm::gpu::scan<uint> scan(count, threads);
-        cuda::host::vector<uint> h_array2(count);
-        start.record();
-        scan(g_array);
-        cuda::thread::synchronize();
-        stop.record();
-        cuda::copy(g_array, h_array2);
-
-        // serial prefix sum
-        std::vector<uint> h_array3(count);
-        real_timer timer;
-        timer.start();
-        h_array3[0] = 0;
-        for (uint i = 1; i < count; ++i)
-            h_array3[i] = h_array[i - 1] + h_array3[i - 1];
-        timer.stop();
-
-        if (verbose) {
-            // write results to stdout
-            for (uint i = 0; i < count; ++i) {
-                std::cout << "a[" << std::setw(6) << i << "] = " << std::setw(6)
-                          << h_array[i] << ",\t[GPU] " << std::setw(10) << h_array2[i]
-                          << ",\t[CPU] " << std::setw(10) << h_array3[i]
-                          << ((h_array2[i] != h_array3[i]) ? " << MISMATCH\n" : "\n");
-            }
-        }
-
-        std::cout << "GPU time: " << std::fixed << std::setprecision(3)
-                  << (stop - start) * 1e3 << " ms\n"
-                  << "CPU time: " << std::fixed << std::setprecision(3)
-                  << timer.elapsed() * 1e3 << " ms\n";
-
-        // verify results
-        if (!std::equal(h_array2.begin(), h_array2.end(), h_array3.begin())) {
-            throw std::logic_error("GPU and CPU prefix sum mismatch");
-        }
-    }
-    catch (cuda::error const& e) {
-        std::cerr << PROGRAM_NAME << ": CUDA ERROR: " << e.what() << "\n";
-        return EXIT_FAILURE;
-    }
-    catch (std::exception const& e) {
-        std::cerr << PROGRAM_NAME << ": ERROR: " << e.what() << "\n";
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    // compare results
+    BOOST_CHECK_MESSAGE(std::equal(h_array2.begin(), h_array2.end(), h_array3.begin()),
+                        "GPU and CPU prefix sum mismatch");
 }
+
+int init_unit_test_suite()
+{
+    using namespace boost::unit_test::framework;
+    using namespace boost::assign;
+
+    std::vector<size_t> count;
+    count += 10000000
+           , 1000000
+           , 100000
+           , 10000
+           , 1024
+           , 1000
+           , 512
+           , 100
+           , 10
+           , 2
+           ;
+
+    master_test_suite().add(
+        BOOST_PARAM_TEST_CASE(&compare_scan, count.begin(), count.end()));
+
+    return 0;
+}
+
+static int _dummy = init_unit_test_suite();
