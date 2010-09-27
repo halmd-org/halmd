3e7ab62e377204e1c1b44834acdf2dbbbea57c26
Peter Colberg <peter.colberg@utoronto.ca>
2010-09-27 15:46:23 -0400

    Reimplement options_parser.
    Split command line parsing and config file parsing into separate functions.
    
    Split error/help/version printing into functions.
    
    Provide a helper function for main().
    
    Register options description with Lua for module options.

---
+++ src/halmd/options.cpp
@@ -0,0 +1,193 @@
+/*
+ * Copyright © 2008-2010  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <boost/algorithm/string.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/casts.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+#include <fstream>
+#include <iostream>
+
+#include <halmd/options.hpp>
+#include <halmd/utility/date_time.hpp>
+#include <halmd/utility/lua.hpp>
+
+using namespace boost;
+using namespace std;
+
+namespace halmd
+{
+
+/**
+ * setup program options description
+ */
+options_parser::options_parser(po::options_description const& desc)
+  : desc_(desc)
+{
+    desc_.add_options()
+        ("output,o",
+         po::value<string>()->default_value(output_file_basename())->notifier(
+             boost::lambda::bind(
+                 &format_local_time
+               , boost::lambda::ll_const_cast<string&>(boost::lambda::_1)
+               , boost::lambda::_1
+             )
+         ),
+         "output file prefix")
+        ("config,C", po::value<vector<string> >(),
+         "parameter input file")
+        ("trajectory,J", po::value<string>(),
+         "trajectory input file")
+        ("version",
+         "output version and exit")
+        ("help",
+         "display this help and exit")
+        ;
+}
+
+/**
+ * parse command line options
+ */
+void options_parser::parse_command_line(int argc, char** argv)
+{
+    po::command_line_parser parser(argc, argv);
+    po::parsed_options parsed(parser.options(desc_).run());
+    po::store(parsed, vm_);
+    po::notify(vm_);
+}
+
+/**
+ * parse config file options
+ *
+ * @param file_name path to configuration file
+ */
+void options_parser::parse_config_file(std::string const& file_name)
+{
+    ifstream ifs(file_name.c_str());
+    if (ifs.fail()) {
+        throw runtime_error("could not open parameter file '" + file_name + "'");
+    }
+    po::parsed_options parsed(po::parse_config_file(ifs, desc_));
+    po::store(parsed, vm_);
+    po::notify(vm_);
+}
+
+/**
+ * print options parser error message to stderr
+ *
+ * @param error exception deriving from std::exception
+ */
+void options_parser::print_error(std::exception const& error)
+{
+    cerr << PROGRAM_NAME ": " << error.what() << endl;
+    cerr << "Try `" PROGRAM_NAME " --help' for more information." << endl;
+}
+
+/**
+ * print options help message to stdout
+ */
+void options_parser::print_help()
+{
+    cout << "Usage: " PROGRAM_NAME " [OPTION]..." << endl << endl
+         << desc_ << endl;
+}
+
+/**
+ * print version information to stdout
+ */
+void options_parser::print_version()
+{
+    cout << PROJECT_NAME " (" PROGRAM_DESC ") " PROGRAM_VERSION << endl << endl
+         << PROGRAM_COPYRIGHT << endl
+         << "This is free software. "
+            "You may redistribute copies of it under the terms of" << endl
+         << "the GNU General Public License "
+            "<http://www.gnu.org/licenses/gpl.html>." << endl
+         << "There is NO WARRANTY, to the extent permitted by law." << endl;
+}
+
+
+/**
+ * parse command line and config file options
+ *
+ * This is a helper function for main().
+ */
+void options_parser::parse(int argc, char** argv)
+{
+    try {
+        parse_command_line(argc, argv);
+    }
+    catch (std::exception const& e) {
+        print_error(e);
+        throw exit_exception(EXIT_FAILURE);
+    }
+
+    if (vm_.count("help")) {
+        print_help();
+        throw exit_exception(EXIT_SUCCESS);
+    }
+    if (vm_.count("version")) {
+        print_version();
+        throw exit_exception(EXIT_SUCCESS);
+    }
+
+    try {
+        if (vm_.count("config")) {
+            vector<string> const& config = vm_["config"].as<vector<string> >();
+            for_each(
+                config.begin()
+              , config.end()
+              , bind(&options_parser::parse_config_file, this, _1)
+            );
+        }
+    }
+    catch (std::exception const& e) {
+        print_error(e);
+        throw exit_exception(EXIT_FAILURE);
+    }
+}
+
+/**
+ * register Lua C++ wrapper
+ */
+static luabind::scope register_lua()
+{
+    using namespace luabind;
+    return
+        namespace_("halmd_wrapper")
+        [
+            class_<options_parser>("options_parser")
+                .def("parsed", &options_parser::parsed)
+        ]
+      , namespace_("boost")
+        [
+            namespace_("program_options")
+            [
+                class_<po::options_description>("options_description") //< only register class
+            ]
+        ];
+}
+
+static lua_registry::iterator dummy = (
+    lua_registry::get()->push_back( register_lua() )
+  , lua_registry::get()->end()
+);
+
+} // namespace halmd
---
+++ src/halmd/options.hpp
@@ -0,0 +1,215 @@
+/*
+ * Copyright © 2008-2010  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_OPTIONS_HPP
+#define HALMD_OPTIONS_HPP
+
+#include <boost/array.hpp>
+#include <boost/assign.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/multi_array.hpp>
+#include <boost/program_options.hpp>
+#include <string>
+#include <vector>
+
+#include <halmd/version.h> //< needed for PROGRAM_NAME
+
+namespace halmd
+{
+
+// abbreviated namespace with top-level scope
+namespace po = boost::program_options;
+
+/**
+ * Command-line options parser
+ */
+class options_parser
+{
+public:
+    options_parser(po::options_description const& desc);
+    void parse_command_line(int argc, char** argv);
+    void parse_config_file(std::string const& file_name);
+    void print_error(std::exception const& error);
+    void print_help();
+    void print_version();
+
+    //! returns parsed program options
+    po::variables_map const& parsed()
+    {
+        return vm_;
+    }
+
+    void parse(int argc, char** arg);
+
+private:
+    po::options_description desc_; //< options description
+    po::variables_map vm_; //< options map
+
+    static char const* output_file_basename()
+    {
+       return PROGRAM_NAME "_%Y%m%d_%H%M%S";
+    }
+};
+
+/**
+ * Exit exception
+ */
+class exit_exception
+{
+public:
+    //! set exit code
+    exit_exception(int code) : code_(code) {}
+
+    //! returns exit code
+    int code() const throw()
+    {
+        return code_;
+    }
+
+private:
+    int code_; //< exit code
+};
+
+/**
+ * Accumulating program option value
+ */
+template <typename T, typename charT = char>
+class accumulating_value
+  : public boost::program_options::typed_value<T,charT>
+{
+    //
+    // Originally written by Bryan Green <bgreen@nas.nasa.gov>
+    // http://article.gmane.org/gmane.comp.lib.boost.user/29084
+    //
+
+public:
+    accumulating_value(T* store_to = 0)
+      : boost::program_options::typed_value<T,charT>(store_to)
+      , origin(0)
+    {
+        (void) boost::program_options::typed_value<T,charT>::zero_tokens();
+    }
+
+    accumulating_value* default_value(const T &v)
+    {
+        // setting a default value sets the origin to that value
+        origin = v;
+        (void) boost::program_options::typed_value<T,charT>::default_value(v);
+        return this;
+    }
+
+    accumulating_value* default_value(const T &v, const std::string& textual)
+    {
+        // setting a default value sets the origin to that value
+        origin = v;
+        (void) boost::program_options::typed_value<T,charT>::default_value(v, textual);
+        return this;
+    }
+
+    void xparse(boost::any& value_store, const std::vector<std::basic_string<charT> >& new_tokens) const
+    {
+        // if this is the first occurrence of the option, initialize it to the origin
+        if (value_store.empty()) {
+            value_store = boost::any(origin);
+        }
+        ++boost::any_cast<T&>(value_store);
+    }
+
+private:
+    T origin; //< the numeric origin from which to increment upward
+};
+
+template <typename T>
+accumulating_value<T>* accum_value(T *v)
+{
+    accumulating_value<T>* r = new accumulating_value<T>(v);
+    return r;
+}
+
+template <typename T>
+accumulating_value<T>* accum_value()
+{
+    return accum_value<T>(0);
+}
+
+} // namespace halmd
+
+namespace std
+{
+
+/**
+ * extract comma-separated option values into fixed-size array
+ */
+template <typename T, size_t size>
+std::istream& operator>>(std::istream& is, boost::array<T, size>& value)
+{
+    BOOST_FOREACH(T& v, value) {
+        std::string str;
+        getline(is, str, ',');
+        v = boost::lexical_cast<T>(str);
+    }
+    return is;
+}
+
+template <typename T, size_t size>
+std::ostream& operator<<(std::ostream& os, boost::array<T, size> const& value)
+{
+    BOOST_FOREACH(T const& v, value) {
+        if (&v != &value.front()) {
+            os << ',';
+        }
+        os << v;
+    }
+    return os;
+}
+
+/**
+ * extract comma-separated option values into variable-size array
+ */
+template <typename T>
+std::istream& operator>>(std::istream& is, boost::multi_array<T, 1>& value)
+{
+    std::vector<T> v;
+    std::string str;
+    while (!is.eof()) {
+        getline(is, str, ',');
+        v.push_back(boost::lexical_cast<T>(str));
+    }
+    boost::array<size_t, 1> extents = boost::assign::list_of(v.size());
+    value.resize(extents);
+    std::copy(v.begin(), v.end(), value.begin());
+    return is;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, boost::multi_array<T, 1> const& value)
+{
+    BOOST_FOREACH(T const& v, value) {
+        if (&v != &(*value.begin())) {
+            os << ',';
+        }
+        os << v;
+    }
+    return os;
+}
+
+} // namespace std
+
+#endif /* ! HALMD_OPTIONS_HPP */
