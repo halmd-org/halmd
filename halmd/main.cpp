halmd-org/halmd

commit 23512f0ce17e82453816ce6d8444faa5c771d61e

Move options parser to utility/options_parser.

halmd/main.cpp
@@ -20,9 +20,9 @@
 #include <boost/algorithm/string/join.hpp>
 
 #include <halmd/io/logger.hpp>
-#include <halmd/options.hpp>
 #include <halmd/script.hpp>
 #include <halmd/utility/hostname.hpp>
+#include <halmd/utility/options_parser.hpp>
 #include <halmd/version.h>
 
 using namespace boost;

halmd/utility/options_parser.cpp
@@ -26,7 +26,7 @@
 #include <iostream>
 
 #include <halmd/io/logger.hpp> //< logger::warning
-#include <halmd/options.hpp>
+#include <halmd/utility/options_parser.hpp>
 #include <halmd/utility/date_time.hpp>
 #include <halmd/utility/lua_wrapper/lua_wrapper.hpp>
 #include <halmd/version.h>

halmd/utility/options_parser.hpp
@@ -17,8 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef HALMD_OPTIONS_HPP
-#define HALMD_OPTIONS_HPP
+#ifndef HALMD_OPTIONS_PARSER_HPP
+#define HALMD_OPTIONS_PARSER_HPP
 
 #include <lua.hpp>
 
@@ -76,4 +76,4 @@ private:
 
 } // namespace halmd
 
-#endif /* ! HALMD_OPTIONS_HPP */
+#endif /* ! HALMD_OPTIONS_PARSER_HPP */

halmd/CMakeLists.txt
@@ -79,7 +79,6 @@ endif(GIT_EXECUTABLE)
 if(NOT HALMD_BACKEND_EXECUTABLES)
   add_executable(halmd
     main.cpp
-    options.cpp
     sampler.cpp
     script.cpp
     version.h

halmd/utility/CMakeLists.txt
@@ -1,5 +1,6 @@
 add_library(halmd_utility
   hostname.cpp
+  options_parser.cpp
   profiler.cpp
 )
 

commit 7d32f73a0dfc2d87b1a8291b9f87ad26f41bdb4f

version.h: fix generation of <halmd/version.h> in pristine build tree.
This fixes a regression from commit 23512f0, which moved options parser to a
subdirectory, which breaks the CMake rule for generating version.h as CMake
rules do not propagate across directories.

Instead of moving the options parser back to the main directory, we strip the
options parser of the functionality *not* related to parsing: The main program
options (e.g. --version, --help) are defined as well as handled in main().
This removes the dependency of the options parser on <halmd/version.h>.

halmd/main.cpp
@@ -18,9 +18,14 @@
  */
 
 #include <boost/algorithm/string/join.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/casts.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
 
 #include <halmd/io/logger.hpp>
 #include <halmd/script.hpp>
+#include <halmd/utility/date_time.hpp>
 #include <halmd/utility/hostname.hpp>
 #include <halmd/utility/options_parser.hpp>
 #include <halmd/version.h>
@@ -32,6 +37,10 @@ using namespace std;
 
 /**
  * Run HAL’s MD package
+ *
+ * This function loads the Lua scripting engine, parses program options
+ * from the command line and optionally a config file, sets up logging,
+ * and runs the Lua simulation script.
  */
 int main(int argc, char **argv)
 {
@@ -42,16 +51,68 @@ int main(int argc, char **argv)
     try {
         static script script; //< load Lua script engine
 
-        options_parser options(script.options());
+        //
+        // assemble program options
+        //
+        po::options_description desc(script.options());
+        desc.add_options()
+            ("output,o",
+             po::value<string>()->default_value(PROGRAM_NAME "_%Y%m%d_%H%M%S")->notifier(
+                 boost::lambda::bind(
+                     &format_local_time
+                   , boost::lambda::ll_const_cast<string&>(boost::lambda::_1)
+                   , boost::lambda::_1
+                 )
+             ),
+             "output file prefix")
+            ("config,C", po::value<vector<string> >(),
+             "parameter input file")
+            ("trajectory,J", po::value<string>(),
+             "trajectory input file")
+            ("verbose,v", po::accum_value<int>()->default_value(logger::warning),
+             "increase verbosity")
+            ("version",
+             "output version and exit")
+            ("help",
+             "display this help and exit")
+            ;
+
+        //
+        // parse program options from command line and config file
+        //
+        options_parser options(desc);
         try {
             options.parse(argc, argv);
         }
-        catch (exit_exception const& e) {
-            return e.code();
+        catch (po::error const& e) {
+            cerr << PROGRAM_NAME ": " << e.what() << endl;
+            cerr << "Try `" PROGRAM_NAME " --help' for more information." << endl;
+            return EXIT_FAILURE;
         }
         po::variables_map vm(options.parsed());
 
-        script.parsed(vm); //< pass command line options to Lua
+        //
+        // print version information to stdout
+        //
+        if (vm.count("version")) {
+            cout << PROJECT_NAME " (" PROGRAM_DESC ") " PROGRAM_VERSION << endl << endl
+                 << PROGRAM_COPYRIGHT << endl
+                 << "This is free software. "
+                    "You may redistribute copies of it under the terms of" << endl
+                 << "the GNU General Public License "
+                    "<http://www.gnu.org/licenses/gpl.html>." << endl
+                 << "There is NO WARRANTY, to the extent permitted by law." << endl;
+            return EXIT_SUCCESS;
+        }
+
+        //
+        // print options help message to stdout
+        //
+        if (vm.count("help")) {
+            cout << "Usage: " PROGRAM_NAME " [OPTION]..." << endl << endl
+                 << desc << endl;
+            return EXIT_SUCCESS;
+        }
 
         log.log_to_console(
             static_cast<logger::severity_level>(vm["verbose"].as<int>())
@@ -73,6 +134,8 @@ int main(int argc, char **argv)
 #endif
         LOG("command line: " << join(vector<string>(argv, argv + argc), " "));
         LOG("host name: " << host_name());
+
+        script.parsed(vm); //< pass command line options to Lua
 
         script.run();
     }

halmd/script.cpp
@@ -135,7 +135,7 @@ po::options_description script::options()
         throw;
     }
 
-    return desc; //< avoid variable in main() at the expense of return-by-value
+    return desc;
 }
 
 /**

halmd/utility/options_parser.cpp
@@ -17,18 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <boost/algorithm/string.hpp>
-#include <boost/lambda/bind.hpp>
-#include <boost/lambda/casts.hpp>
 #include <boost/lambda/lambda.hpp>
-#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+#include <boost/lambda/bind.hpp>
 #include <fstream>
 #include <iostream>
 
-#include <halmd/io/logger.hpp> //< logger::warning
 #include <halmd/utility/options_parser.hpp>
-#include <halmd/utility/date_time.hpp>
-#include <halmd/version.h>
 
 using namespace boost;
 using namespace std;
@@ -36,37 +30,11 @@ using namespace std;
 namespace halmd
 {
 
-// define here to avoid program-wide dependency on <halmd/version.h>
-static char const* default_output_file_name = PROGRAM_NAME "_%Y%m%d_%H%M%S";
-
 /**
  * setup program options description
  */
 options_parser::options_parser(po::options_description const& desc)
-  : desc_(desc)
-{
-    desc_.add_options()
-        ("output,o",
-         po::value<string>()->default_value(default_output_file_name)->notifier(
-             boost::lambda::bind(
-                 &format_local_time
-               , boost::lambda::ll_const_cast<string&>(boost::lambda::_1)
-               , boost::lambda::_1
-             )
-         ),
-         "output file prefix")
-        ("config,C", po::value<vector<string> >(),
-         "parameter input file")
-        ("trajectory,J", po::value<string>(),
-         "trajectory input file")
-        ("verbose,v", po::accum_value<int>()->default_value(logger::warning),
-         "increase verbosity")
-        ("version",
-         "output version and exit")
-        ("help",
-         "display this help and exit")
-        ;
-}
+  : desc_(desc) {}
 
 /**
  * parse command line options
@@ -76,13 +44,16 @@ void options_parser::parse_command_line(int argc, char** argv)
     using namespace po::command_line_style;
     po::command_line_parser parser(argc, argv);
     parser.options(desc_);
+
     // pass an empty positional options description to the command line
     // parser to warn the user of unintentional positional options
     po::positional_options_description pd;
     parser.positional(pd);
-    // disallow abbreviated options, which breaks forward compatibility of
+
+    // disallow abbreviated options, which break forward compatibility of
     // user's scripts as new options are added and create ambiguities
     parser.style(default_style & ~allow_guessing);
+
     po::parsed_options parsed(parser.run());
     po::store(parsed, vm_);
     po::notify(vm_);
@@ -105,77 +76,21 @@ void options_parser::parse_config_file(std::string const& file_name)
 }
 
 /**
- * print options parser error message to stderr
- *
- * @param error exception deriving from std::exception
- */
-void options_parser::print_error(std::exception const& error) const
-{
-    cerr << PROGRAM_NAME ": " << error.what() << endl;
-    cerr << "Try `" PROGRAM_NAME " --help' for more information." << endl;
-}
-
-/**
- * print options help message to stdout
- */
-void options_parser::print_help() const
-{
-    cout << "Usage: " PROGRAM_NAME " [OPTION]..." << endl << endl
-         << desc_ << endl;
-}
-
-/**
- * print version information to stdout
- */
-void options_parser::print_version() const
-{
-    cout << PROJECT_NAME " (" PROGRAM_DESC ") " PROGRAM_VERSION << endl << endl
-         << PROGRAM_COPYRIGHT << endl
-         << "This is free software. "
-            "You may redistribute copies of it under the terms of" << endl
-         << "the GNU General Public License "
-            "<http://www.gnu.org/licenses/gpl.html>." << endl
-         << "There is NO WARRANTY, to the extent permitted by law." << endl;
-}
-
-
-/**
  * parse command line and config file options
  *
  * This is a helper function for main().
  */
 void options_parser::parse(int argc, char** argv)
 {
-    try {
-        parse_command_line(argc, argv);
-    }
-    catch (std::exception const& e) {
-        print_error(e);
-        throw exit_exception(EXIT_FAILURE);
-    }
+    parse_command_line(argc, argv);
 
-    if (vm_.count("help")) {
-        print_help();
-        throw exit_exception(EXIT_SUCCESS);
-    }
-    if (vm_.count("version")) {
-        print_version();
-        throw exit_exception(EXIT_SUCCESS);
-    }
-
-    try {
-        if (vm_.count("config")) {
-            vector<string> const& config = vm_["config"].as<vector<string> >();
-            for_each(
-                config.begin()
-              , config.end()
-              , lambda::bind(&options_parser::parse_config_file, this, lambda::_1)
-            );
-        }
-    }
-    catch (std::exception const& e) {
-        print_error(e);
-        throw exit_exception(EXIT_FAILURE);
+    if (vm_.count("config")) {
+        vector<string> const& config = vm_["config"].as<vector<string> >();
+        for_each(
+            config.begin()
+          , config.end()
+          , lambda::bind(&options_parser::parse_config_file, this, lambda::_1)
+        );
     }
 }
 

halmd/utility/options_parser.hpp
@@ -34,9 +34,6 @@ public:
     options_parser(po::options_description const& desc);
     void parse_command_line(int argc, char** argv);
     void parse_config_file(std::string const& file_name);
-    void print_error(std::exception const& error) const;
-    void print_help() const;
-    void print_version() const;
 
     //! returns parsed program options
     po::variables_map const& parsed() const
@@ -49,25 +46,6 @@ public:
 private:
     po::options_description desc_; //< options description
     po::variables_map vm_; //< options map
-};
-
-/**
- * Exit exception
- */
-class exit_exception
-{
-public:
-    //! set exit code
-    exit_exception(int code) : code_(code) {}
-
-    //! returns exit code
-    int code() const throw()
-    {
-        return code_;
-    }
-
-private:
-    int code_; //< exit code
 };
 
 } // namespace halmd