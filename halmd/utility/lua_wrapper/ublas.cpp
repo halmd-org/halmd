e888cf44d991b39805ae42463e4aac158986883a
Peter Colberg <peter.colberg@utoronto.ca>
2010-11-19 20:01:25 -0500

    lua_wrapper/ublas: wrap __tostring operator for symmetric matrices.
    The Lua operator __tostring is invoked upon print or tostring.

--- halmd/utility/lua_wrapper/ublas.cpp
+++ halmd/utility/lua_wrapper/ublas.cpp
@@ -17,9 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/symmetric.hpp>
 #include <boost/numeric/ublas/vector.hpp>
+#include <luabind/luabind.hpp>
+#include <luabind/operator.hpp>
 
 #include <halmd/utility/lua_wrapper/ublas.hpp>
 
@@ -39,20 +42,35 @@ int luaopen(lua_State* L)
         [
             class_<vector<double> >("vector_double")
                 .def("data", (unbounded_array<double> const& (vector<double>::*)() const) &vector<double>::data)
+
           , class_<vector<float> >("vector_float")
                 .def("data", (unbounded_array<float> const& (vector<float>::*)() const) &vector<float>::data)
+
           , class_<matrix<double> >("matrix_double")
                 .def("data", (unbounded_array<double> const& (matrix<double>::*)() const) &matrix<double>::data)
+
           , class_<matrix<float> >("matrix_float")
                 .def("data", (unbounded_array<float> const& (matrix<float>::*)() const) &matrix<float>::data)
+
           , class_<symmetric_matrix<double, lower> >("symmetric_matrix_double_lower")
                 .def("data", (unbounded_array<double> const& (symmetric_matrix<double, lower>::*)() const) &symmetric_matrix<double, lower>::data)
+                .def(tostring(self))
+                .def(tostring(const_self))
+
           , class_<symmetric_matrix<float, lower> >("symmetric_matrix_float_lower")
                 .def("data", (unbounded_array<float> const& (symmetric_matrix<float, lower>::*)() const) &symmetric_matrix<float, lower>::data)
+                .def(tostring(self))
+                .def(tostring(const_self))
+
           , class_<symmetric_matrix<double, upper> >("symmetric_matrix_double_upper")
                 .def("data", (unbounded_array<double> const& (symmetric_matrix<double, upper>::*)() const) &symmetric_matrix<double, upper>::data)
+                .def(tostring(self))
+                .def(tostring(const_self))
+
           , class_<symmetric_matrix<float, upper> >("symmetric_matrix_float_upper")
                 .def("data", (unbounded_array<float> const& (symmetric_matrix<float, upper>::*)() const) &symmetric_matrix<float, upper>::data)
+                .def(tostring(self))
+                .def(tostring(const_self))
         ]
     ];
     return 0;
