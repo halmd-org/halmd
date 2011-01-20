0cb2f632ef1939e719b708e654290c4c081bbba5
Felix Höfling <hoefling@mf.mpg.de>
2011-01-20 13:26:19 +0100

    algorithm::gpu::tuple: add support for 3-tuple

--- halmd/algorithm/gpu/tuple.cuh
+++ halmd/algorithm/gpu/tuple.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2010  Peter Colberg
+ * Copyright © 2010-2011  Peter Colberg and Felix Höfling
  *
  * This file is part of HALMD.
  *
@@ -35,13 +35,15 @@
 // Sigh…
 //
 
+// we support n-tuples for n=1, 2, 3
+
 namespace halmd
 {
 namespace algorithm { namespace gpu
 {
 
 // forward declaration
-template <typename T0, typename T1 = void>
+template <typename T0, typename T1 = void, typename T2 = void>
 struct tuple;
 
 template <int i, typename T0>
@@ -65,6 +67,27 @@ __device__ get(tuple<T0, T1> const& t)
     return t.t1;
 }
 
+template <int i, typename T0, typename T1, typename T2>
+typename boost::enable_if<boost::is_same<boost::mpl::int_<0>, boost::mpl::int_<i> >, T0>::type
+__device__ get(tuple<T0, T1, T2> const& t)
+{
+    return t.t0;
+}
+
+template <int i, typename T0, typename T1, typename T2>
+typename boost::enable_if<boost::is_same<boost::mpl::int_<1>, boost::mpl::int_<i> >, T1>::type
+__device__ get(tuple<T0, T1, T2> const& t)
+{
+    return t.t1;
+}
+
+template <int i, typename T0, typename T1, typename T2>
+typename boost::enable_if<boost::is_same<boost::mpl::int_<2>, boost::mpl::int_<i> >, T2>::type
+__device__ get(tuple<T0, T1, T2> const& t)
+{
+    return t.t2;
+}
+
 template <typename T0>
 class tuple<T0>
 {
@@ -83,7 +106,7 @@ public:
 };
 
 template <typename T0, typename T1>
-class tuple
+class tuple<T0, T1>
 {
     T0 t0; T1 t1;
     friend T0 get<0>(tuple<T0, T1> const&);
@@ -101,6 +124,27 @@ public:
     }
 };
 
+template <typename T0, typename T1, typename T2>
+class tuple
+{
+    T0 t0; T1 t1; T2 t2;
+    friend T0 get<0>(tuple<T0, T1, T2> const&);
+    friend T1 get<1>(tuple<T0, T1, T2> const&);
+    friend T2 get<2>(tuple<T0, T1, T2> const&);
+public:
+    __device__ tuple(T0 t0, T1 t1, T2 t2) : t0(t0), t1(t1), t2(t2) {}
+    __device__ tuple(T0 t0, T1 t1) : t0(t0), t1(t1) {}
+    __device__ tuple(T0 t0) : t0(t0) {}
+    __device__ tuple() {}
+    template <typename TT0, typename TT1, typename TT2>
+    __device__ tuple(tuple<TT0, TT1, TT2> const& t)
+      : t0(get<0>(t)), t1(get<1>(t)), t2(get<2>(t)) {}
+    template <typename TT0, typename TT1, typename TT2>
+    __device__ tuple& operator=(tuple<TT0, TT1, TT2> const& t) {
+        t0 = get<0>(t); t1 = get<1>(t); t2 = get<2>(t); return *this;
+    }
+};
+
 template <typename T0>
 __device__ tuple<T0> make_tuple(T0 t0)
 {
@@ -113,6 +157,12 @@ __device__ tuple<T0, T1> make_tuple(T0 t0, T1 t1)
     return tuple<T0, T1>(t0, t1);
 }
 
+template <typename T0, typename T1, typename T2>
+__device__ tuple<T0, T1, T2> make_tuple(T0 t0, T1 t1, T2 t2)
+{
+    return tuple<T0, T1, T2>(t0, t1, t2);
+}
+
 template <typename T0>
 __device__ tuple<T0&> tie(T0& t0)
 {
@@ -123,6 +173,12 @@ template <typename T0, typename T1>
 __device__ tuple<T0&, T1&> tie(T0& t0, T1& t1)
 {
     return tuple<T0&, T1&>(t0, t1);
+}
+
+template <typename T0, typename T1, typename T2>
+__device__ tuple<T0&, T1&, T2&> tie(T0& t0, T1& t1, T2& t2)
+{
+    return tuple<T0&, T1&, T2&>(t0, t1, t2);
 }
 
 }} // namespace algorithm::gpu
