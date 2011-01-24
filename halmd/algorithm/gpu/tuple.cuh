ae906a5f455d07a9726b29a9e4b090657c9daaeb
Peter Colberg <peter.colberg@utoronto.ca>
2011-01-24 13:25:11 -0500

    gpu/tuple: fix FTBFS with CUDA 3.2.
    If we define _all_ get<> functions first and _all_ tuple<> classes
    afterwards, NVCC 3.2 fails to compile with the following error:
    
    halmd/algorithm/gpu/tuple.cuh: In instantiation of ‘halmd::algorithm::gpu::tuple<float, float, void>’:
    halmd/numeric/mp/dsfloat.hpp:105:   instantiated from here
    halmd/algorithm/gpu/tuple.cuh:112: error: ambiguous template specialization ‘get<0>’ for ‘float halmd::algorithm::gpu::get(const halmd::algorithm::gpu::tuple<float, float, void>&)’
    halmd/algorithm/gpu/tuple.cuh: In instantiation of ‘halmd::algorithm::gpu::tuple<float, float, void>’:
    halmd/numeric/mp/dsfloat.hpp:105:   instantiated from here
    halmd/algorithm/gpu/tuple.cuh:113: error: ambiguous template specialization ‘get<1>’ for ‘float halmd::algorithm::gpu::get(const halmd::algorithm::gpu::tuple<float, float, void>&)’
    
    This error is triggered by the friend declarations in tuple<>.
    
    A work around is to order functions and template classes according
    to the tuple arity, i.e. get<> and tuple<> for 1-tuple, get<> and
    tuple<> for 2-tuple, ...

--- halmd/algorithm/gpu/tuple.cuh
+++ halmd/algorithm/gpu/tuple.cuh
@@ -46,12 +46,43 @@ namespace algorithm { namespace gpu
 template <typename T0, typename T1 = void, typename T2 = void>
 struct tuple;
 
+// Note the order of get and tuple specialisations is important.
+//
+// If we define _all_ get<> functions first and _all_ tuple<> classes
+// afterwards, NVCC 3.2 fails to compile with the following error:
+//
+// error: ambiguous template specialization ‘get<0>’
+// error: ambiguous template specialization ‘get<1>’
+//
+// This error is triggered by the friend declarations in tuple<>.
+//
+// A work around is to order functions and template classes according
+// to the tuple arity, i.e. get<> and tuple<> for 1-tuple, get<> and
+// tuple<> for 2-tuple, ...
+
 template <int i, typename T0>
 typename boost::enable_if<boost::is_same<boost::mpl::int_<0>, boost::mpl::int_<i> >, T0>::type
 __device__ get(tuple<T0> const& t)
 {
     return t.t0;
 }
+
+template <typename T0>
+class tuple<T0>
+{
+    T0 t0;
+    friend T0 get<0>(tuple<T0> const&);
+public:
+    __device__ tuple(T0 t0) : t0(t0) {}
+    __device__ tuple() {}
+    template <typename TT0>
+    __device__ tuple(tuple<TT0> const& t)
+      : t0(get<0>(t)) {}
+    template <typename TT0>
+    __device__ tuple& operator=(tuple<TT0> const& t) {
+        t0 = get<0>(t); return *this;
+    }
+};
 
 template <int i, typename T0, typename T1>
 typename boost::enable_if<boost::is_same<boost::mpl::int_<0>, boost::mpl::int_<i> >, T0>::type
@@ -66,6 +97,25 @@ __device__ get(tuple<T0, T1> const& t)
 {
     return t.t1;
 }
+
+template <typename T0, typename T1>
+class tuple<T0, T1>
+{
+    T0 t0; T1 t1;
+    friend T0 get<0>(tuple<T0, T1> const&);
+    friend T1 get<1>(tuple<T0, T1> const&);
+public:
+    __device__ tuple(T0 t0, T1 t1) : t0(t0), t1(t1) {}
+    __device__ tuple(T0 t0) : t0(t0) {}
+    __device__ tuple() {}
+    template <typename TT0, typename TT1>
+    __device__ tuple(tuple<TT0, TT1> const& t)
+      : t0(get<0>(t)), t1(get<1>(t)) {}
+    template <typename TT0, typename TT1>
+    __device__ tuple& operator=(tuple<TT0, TT1> const& t) {
+        t0 = get<0>(t); t1 = get<1>(t); return *this;
+    }
+};
 
 template <int i, typename T0, typename T1, typename T2>
 typename boost::enable_if<boost::is_same<boost::mpl::int_<0>, boost::mpl::int_<i> >, T0>::type
@@ -87,42 +137,6 @@ __device__ get(tuple<T0, T1, T2> const& t)
 {
     return t.t2;
 }
-
-template <typename T0>
-class tuple<T0>
-{
-    T0 t0;
-    friend T0 get<0>(tuple<T0> const&);
-public:
-    __device__ tuple(T0 t0) : t0(t0) {}
-    __device__ tuple() {}
-    template <typename TT0>
-    __device__ tuple(tuple<TT0> const& t)
-      : t0(get<0>(t)) {}
-    template <typename TT0>
-    __device__ tuple& operator=(tuple<TT0> const& t) {
-        t0 = get<0>(t); return *this;
-    }
-};
-
-template <typename T0, typename T1>
-class tuple<T0, T1>
-{
-    T0 t0; T1 t1;
-    friend T0 get<0>(tuple<T0, T1> const&);
-    friend T1 get<1>(tuple<T0, T1> const&);
-public:
-    __device__ tuple(T0 t0, T1 t1) : t0(t0), t1(t1) {}
-    __device__ tuple(T0 t0) : t0(t0) {}
-    __device__ tuple() {}
-    template <typename TT0, typename TT1>
-    __device__ tuple(tuple<TT0, TT1> const& t)
-      : t0(get<0>(t)), t1(get<1>(t)) {}
-    template <typename TT0, typename TT1>
-    __device__ tuple& operator=(tuple<TT0, TT1> const& t) {
-        t0 = get<0>(t); t1 = get<1>(t); return *this;
-    }
-};
 
 template <typename T0, typename T1, typename T2>
 class tuple
