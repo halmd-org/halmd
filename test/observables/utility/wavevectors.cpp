99c0785d152ad67454421c5ca0f2ba3a803bacee
Felix Höfling <hoefling@mf.mpg.de>
2011-02-02 12:05:55 +0100

    wavevector: make test templated for 2, 3 dimensions

--- test/observables/utility/wavevectors.cpp
+++ test/observables/utility/wavevectors.cpp
@@ -19,10 +19,14 @@
 
 #define BOOST_TEST_MODULE observables_utility_wavevectors
 #include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
 
 #include <boost/assign.hpp>
 #include <boost/foreach.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/int.hpp>
 #include <boost/shared_ptr.hpp>
+#include <numeric>
 
 #include <halmd/numeric/blas/fixed_vector.hpp>
 #include <halmd/observables/utility/wavevectors.hpp>
@@ -34,7 +38,9 @@ using namespace std;
 
 using observables::utility::construct_wavevector_shells;
 
-BOOST_AUTO_TEST_CASE( wavevectors )
+typedef mpl::list<mpl::int_<2>, mpl::int_<3> > dimension_list;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( wavevectors, DIM, dimension_list )
 {
     // enable logging to console
     shared_ptr<logger> log(new logger);
@@ -46,11 +52,14 @@ BOOST_AUTO_TEST_CASE( wavevectors )
 #endif
     );
 
-    typedef fixed_vector<double, 3> vector_type;
-    typedef fixed_vector<unsigned int, 3> index_type;
+    enum { dimension = DIM::value };
+
+    typedef fixed_vector<double, dimension> vector_type;
+    typedef fixed_vector<unsigned int, dimension> index_type;
 
     vector<double> wavenumbers = list_of(0.3)(0.7)(1.0)(1.5)(2.0)(25.0);
-    const vector_type box_length = list_of(10.)(10.)(20.);
+    const vector_type box_length =
+        (dimension == 3) ? list_of(10.)(10.)(20.) : list_of(20.)(20.);
     double epsilon = 0.05;
     unsigned int max_count = 10;
 
@@ -59,15 +68,23 @@ BOOST_AUTO_TEST_CASE( wavevectors )
 
     // check conditions on constructed wavevectors
     BOOST_FOREACH (double q, wavenumbers) {
+        // check total count per shell
         BOOST_CHECK(wavevectors.count(q) <= max_count);
-        typedef multimap<double, vector_type>::const_iterator iterator_type;
+        typedef typename multimap<double, vector_type>::const_iterator iterator_type;
         typedef pair<iterator_type, iterator_type> range_type;
+        unsigned int sum = 0;
         for (range_type shell = wavevectors.equal_range(q); shell.first != shell.second; ++shell.first) {
+            // check that magnitude is within the tolerance
             vector_type const& q_vector = shell.first->second;
+            BOOST_CHECK_SMALL(norm_2(q_vector) / q - 1, epsilon);
+
+            // check ascending sum of Miller indices
             const vector_type q_basis = element_div(vector_type(2 * M_PI), box_length);
             index_type hkl = static_cast<index_type>(round(element_div(q_vector, q_basis)));
             hkl /= greatest_common_divisor(hkl);
-            BOOST_CHECK_SMALL(norm_2(q_vector) / q - 1, epsilon);
+            unsigned int sum_ = accumulate(hkl.begin(), hkl.end(), 0u, plus<unsigned int>());
+            BOOST_CHECK_MESSAGE(sum_ >= sum, "incorrect order of wavevectors");
+            sum_ = sum;
         }
     }
 }
