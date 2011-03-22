halmd-org/halmd

commit 1c62832e6a6015dea54db57b15de21ea02aaacd3

observables: remove base class observable.
After the migration to a signal-based sampler, we don't require a common
base class for thermodynamics and ssf anymore. This has the advantage of
cleaner interfaces without virtual stub methods, e.g. an empty virtual
method prepare().

halmd/observables/ssf.hpp
@@ -24,8 +24,8 @@
 #include <lua.hpp>
 #include <vector>
 
+#include <halmd/io/statevars/writer.hpp>
 #include <halmd/numeric/blas/fixed_vector.hpp>
-#include <halmd/observables/observable.hpp>
 #include <halmd/observables/density_mode.hpp>
 #include <halmd/utility/profiler.hpp>
 #include <halmd/utility/signal.hpp>
@@ -48,12 +48,10 @@ namespace observables
 
 template <int dimension>
 class ssf
-  : public observable<dimension>
 {
 public:
-    typedef observable<dimension> _Base;
+    typedef io::statevars::writer<dimension> writer_type;
     typedef observables::density_mode<dimension> density_mode_type;
-    typedef typename _Base::writer_type writer_type;
     typedef halmd::utility::profiler profiler_type;
     typedef halmd::signal<void (double)> signal_type;
     typedef typename signal_type::slot_function_type slot_function_type;
@@ -79,8 +77,6 @@ public:
     virtual void register_runtimes(profiler_type& profiler);
 
     virtual void register_observables(writer_type& writer);
-
-    virtual void prepare() {};
 
     // compute ssf from sample of density Fourier modes and store with given time
     virtual void sample(double time);

halmd/observables/thermodynamics.hpp
@@ -23,13 +23,15 @@
 #include <boost/foreach.hpp>
 #include <boost/numeric/ublas/symmetric.hpp>
 #include <lua.hpp>
+#include <utility> // pair
 #include <vector>
 
+#include <halmd/io/statevars/writer.hpp>
 #include <halmd/mdsim/box.hpp>
 #include <halmd/mdsim/type_traits.hpp>
 #include <halmd/numeric/blas/blas.hpp>
-#include <halmd/observables/observable.hpp>
 #include <halmd/utility/profiler.hpp>
+#include <halmd/utility/signal.hpp>
 
 namespace halmd
 {
@@ -46,14 +48,14 @@ namespace observables
 
 template <int dimension>
 class thermodynamics
-  : public observable<dimension>
 {
 public:
-    typedef observable<dimension> _Base;
+    typedef io::statevars::writer<dimension> writer_type;
     typedef mdsim::box<dimension> box_type;
-    typedef typename _Base::writer_type writer_type;
     typedef halmd::utility::profiler profiler_type;
     typedef typename mdsim::type_traits<dimension, double>::vector_type vector_type;
+    typedef typename signal<void (double)>::slot_function_type slot_function_type;
+    typedef std::pair<slot_function_type, slot_function_type> slot_function_pair_type;
 
     struct runtime
     {
@@ -71,6 +73,8 @@ public:
     void register_runtimes(profiler_type& profiler);
     virtual void register_observables(writer_type& writer);
 
+    // preparations before MD step
+    virtual void prepare() = 0;
     // sample macroscopic state variables and store with given time
     virtual void sample(double time);
 

halmd/observables/CMakeLists.txt
@@ -1,6 +1,5 @@
 add_library(halmd_observables
   density_mode.cpp
-  observable.cpp
   phase_space.cpp
   sampler.cpp
   ssf.cpp

halmd/observables/gpu/thermodynamics.cpp
@@ -131,7 +131,6 @@ double thermodynamics<dimension, float_type>::hypervirial()
 template <int dimension, typename float_type>
 void thermodynamics<dimension, float_type>::luaopen(lua_State* L)
 {
-    typedef typename _Base::_Base _Base_Base;
     using namespace luabind;
     static string class_name("thermodynamics_" + lexical_cast<string>(dimension) + "_");
     module(L)
@@ -142,7 +141,7 @@ void thermodynamics<dimension, float_type>::luaopen(lua_State* L)
             [
                 namespace_("gpu")
                 [
-                    class_<thermodynamics, shared_ptr<_Base_Base>, bases<_Base, _Base_Base> >(class_name.c_str())
+                    class_<thermodynamics, shared_ptr<_Base>, _Base>(class_name.c_str())
                         .def(constructor<
                             shared_ptr<particle_type>
                           , shared_ptr<box_type>
@@ -159,7 +158,7 @@ namespace // limit symbols to translation unit
 
 __attribute__((constructor)) void register_lua()
 {
-    lua_wrapper::register_(2) //< distance of derived to base class
+    lua_wrapper::register_(1) //< distance of derived to base class
     [
         &thermodynamics<3, float>::luaopen
     ]

halmd/observables/host/thermodynamics.cpp
@@ -91,7 +91,6 @@ typename thermodynamics<dimension, float_type>::vector_type thermodynamics<dimen
 template <int dimension, typename float_type>
 void thermodynamics<dimension, float_type>::luaopen(lua_State* L)
 {
-    typedef typename _Base::_Base _Base_Base;
     using namespace luabind;
     static string class_name("thermodynamics_" + lexical_cast<string>(dimension) + "_");
     module(L)
@@ -102,7 +101,7 @@ void thermodynamics<dimension, float_type>::luaopen(lua_State* L)
             [
                 namespace_("host")
                 [
-                    class_<thermodynamics, shared_ptr<_Base_Base>, bases<_Base, _Base_Base> >(class_name.c_str())
+                    class_<thermodynamics, shared_ptr<_Base>, _Base>(class_name.c_str())
                         .def(constructor<
                             shared_ptr<particle_type>
                           , shared_ptr<box_type>
@@ -119,7 +118,7 @@ namespace // limit symbols to translation unit
 
 __attribute__((constructor)) void register_lua()
 {
-    lua_wrapper::register_(2) //< distance of derived to base class
+    lua_wrapper::register_(1) //< distance of derived to base class
 #ifndef USE_HOST_SINGLE_PRECISION
     [
         &thermodynamics<3, double>::luaopen

halmd/observables/observable.cpp
@@ -1,77 +1,0 @@
-/*
- * Copyright © 2010  Felix Höfling
- *
- * This file is part of HALMD.
- *
- * HALMD is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <boost/bind.hpp>
-
-#include <halmd/observables/observable.hpp>
-#include <halmd/utility/lua_wrapper/lua_wrapper.hpp>
-
-using namespace boost;
-using namespace std;
-
-namespace halmd
-{
-namespace observables
-{
-
-template <typename observable_type>
-typename observable_type::slot_function_pair_type
-sample_wrapper(shared_ptr<observable_type> observable)
-{
-    return make_pair(
-        bind(&observable_type::prepare, observable)
-      , bind(&observable_type::sample, observable, _1)
-    );
-}
-
-template <int dimension>
-void observable<dimension>::luaopen(lua_State* L)
-{
-    using namespace luabind;
-    static string class_name("observable_" + lexical_cast<string>(dimension) + "_");
-    module(L)
-    [
-        namespace_("halmd_wrapper")
-        [
-            class_<observable, shared_ptr<observable> >(class_name.c_str())
-                .def("register_observables", &observable::register_observables)
-                .property("sample", &sample_wrapper<observable>)
-        ]
-    ];
-}
-
-namespace // limit symbols to translation unit
-{
-
-__attribute__((constructor)) void register_lua()
-{
-    lua_wrapper::register_(0) //< distance of derived to base class
-    [
-        &observable<3>::luaopen
-    ]
-    [
-        &observable<2>::luaopen
-    ];
-}
-
-} // namespace
-
-} // namespace observables
-
-} // namespace halmd

halmd/observables/observable.hpp
@@ -1,64 +1,0 @@
-/*
- * Copyright © 2010-2011  Felix Höfling
- *
- * This file is part of HALMD.
- *
- * HALMD is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef HALMD_OBSERVABLES_OBSERVABLE_HPP
-#define HALMD_OBSERVABLES_OBSERVABLE_HPP
-
-#include <lua.hpp>
-#include <utility> // pair
-
-#include <halmd/io/statevars/writer.hpp>
-#include <halmd/utility/signal.hpp>
-
-namespace halmd
-{
-namespace observables
-{
-
-/**
- * base class for observables
- *
- * defines the interface function sample()
- */
-template <int dimension>
-class observable
-{
-public:
-    typedef io::statevars::writer<dimension> writer_type; //< FIXME dimension-independent
-    typedef typename signal<void (double)>::slot_function_type slot_function_type;
-    typedef std::pair<slot_function_type, slot_function_type> slot_function_pair_type;
-
-    static void luaopen(lua_State* L);
-
-    observable() {};
-    virtual ~observable() {}
-    virtual void register_observables(writer_type& writer) = 0;
-
-    // preparations before MD step
-    virtual void prepare() = 0;
-
-    // sample observable and store with given time
-    virtual void sample(double time) = 0;
-};
-
-} // namespace observables
-
-} // namespace halmd
-
-#endif /* ! HALMD_OBSERVABLES_OBSERVABLE_HPP */

halmd/observables/ssf.cpp
@@ -177,6 +177,13 @@ void ssf<dimension>::compute_()
     }
 }
 
+template <typename ssf_type>
+typename ssf_type::slot_function_type
+sample_wrapper(shared_ptr<ssf_type> ssf)
+{
+    return bind(&ssf_type::sample, ssf, _1);
+}
+
 template <int dimension>
 void ssf<dimension>::luaopen(lua_State* L)
 {
@@ -188,7 +195,7 @@ void ssf<dimension>::luaopen(lua_State* L)
         [
             namespace_("observables")
             [
-                class_<ssf, shared_ptr<_Base>, _Base>(class_name.c_str())
+                class_<ssf, shared_ptr<ssf> >(class_name.c_str())
                     .def(constructor<
                         shared_ptr<density_mode_type>
                       , unsigned int
@@ -196,6 +203,7 @@ void ssf<dimension>::luaopen(lua_State* L)
                     .def("register_runtimes", &ssf::register_runtimes)
                     .property("value", &ssf::value)
                     .property("wavevector", &ssf::wavevector)
+                    .property("sample", &sample_wrapper<ssf>)
                     .def("on_sample", &ssf::on_sample)
                     .scope
                     [
@@ -211,7 +219,7 @@ namespace // limit symbols to translation unit
 
 __attribute__((constructor)) void register_lua()
 {
-    lua_wrapper::register_(1) //< distance of derived to base class
+    lua_wrapper::register_(0) //< distance of derived to base class
     [
         &ssf<3>::luaopen
     ]

commit f082ff3e5d32aa7e4f1aa350ddf568399377eb92

observables: fix missing virtual destructors.
This fixes a regression introduced with commit 1c62832.

halmd/observables/ssf.hpp
@@ -73,9 +73,8 @@ public:
         boost::shared_ptr<density_mode_type> density_mode
       , unsigned int npart
     );
-
+    virtual ~ssf() {}
     virtual void register_runtimes(profiler_type& profiler);
-
     virtual void register_observables(writer_type& writer);
 
     // compute ssf from sample of density Fourier modes and store with given time

halmd/observables/thermodynamics.hpp
@@ -70,6 +70,7 @@ public:
     thermodynamics(
         boost::shared_ptr<box_type> box
     );
+    virtual ~thermodynamics() {}
     void register_runtimes(profiler_type& profiler);
     virtual void register_observables(writer_type& writer);
 