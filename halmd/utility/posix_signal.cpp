704c2c0ab96b52eaae33bf3e6c9b1b2a85b0ff1e
Peter Colberg <peter.colberg@utoronto.ca>
2011-03-23 19:37:18 -0400

    utility: write POSIX signal handler module.
    The module is not complete yet. It is still lacking error handling
    of system calls, and testing of multiple concurrent signal handler
    instances. Further missing are Lua bindings and a Lua module.
    
    This commit refs #43.

---
+++ halmd/utility/posix_signal.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright © 2008-2011  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_MDSIM_HPP
+#define HALMD_MDSIM_HPP
+
+#include <boost/lexical_cast.hpp>
+#include <boost/tuple/tuple.hpp>
+
+#include <halmd/utility/posix_signal.hpp>
+
+using namespace boost;
+using namespace std;
+
+namespace halmd
+{
+
+// FIXME error code handling (with boost::system::system_error)
+
+posix_signal::posix_signal()
+{
+    sigemptyset(&set_);
+    pthread_sigmask(0, NULL, &oldset_);
+}
+
+posix_signal::~posix_signal()
+{
+    pthread_sigmask(SIG_SETMASK, &oldset_, NULL);
+}
+
+/**
+ * register signal handler
+ *
+ * @param signum signal number (> 0)
+ * @param slot signal handler function or functor
+ */
+void posix_signal::on_signal(int signum, slot_function_type const& slot)
+{
+    handler_map_type::iterator it;
+    bool inserted;
+    tie(it, inserted) = handler_.insert(make_pair(signum, handler_type()));
+    if (inserted) {
+        sigaddset(&set_, signum);
+        pthread_sigmask(SIG_BLOCK, &set_, NULL);
+    }
+    it->second.connect(slot);
+}
+
+/**
+ * block process until signal is received
+ */
+void posix_signal::wait() const
+{
+    int signum = sigwaitinfo(&set_, NULL);
+    this->handle(signum);
+}
+
+/**
+ * poll signal queue
+ *
+ * @returns true if a signal was handled, false otherwise
+ */
+bool posix_signal::poll() const
+{
+    timespec timeout = { 0, 0 };
+    int signum = sigtimedwait(&set_, NULL, &timeout);
+    if (signum > 0) {
+        this->handle(signum);
+        return true;
+    }
+    return false;
+}
+
+/**
+ * returns signal name, or signal number otherwise
+ *
+ * @param signum signal number (> 0)
+ */
+string posix_signal::name(int signum)
+{
+    switch (signum) {
+      case SIGHUP:
+        return "SIGHUP";
+      case SIGINT:
+        return "SIGINT";
+      case SIGQUIT:
+        return "SIGQUIT";
+      case SIGALRM:
+        return "SIGALRM";
+      case SIGTERM:
+        return "SIGTERM";
+      case SIGUSR1:
+        return "SIGUSR1";
+      case SIGUSR2:
+        return "SIGUSR2";
+      case SIGCONT:
+        return "SIGCONT";
+      case SIGTSTP:
+        return "SIGTSTP";
+      case SIGTTIN:
+        return "SIGTTIN";
+      case SIGTTOU:
+        return "SIGTTOU";
+      default:
+        return lexical_cast<string>(signum);
+    }
+}
+
+/**
+ * handle POSIX signal
+ *
+ * @param signum signal number (> 0)
+ */
+void posix_signal::handle(int signum) const
+{
+    handler_map_type::const_iterator it = handler_.find(signum);
+    if (it == handler_.end()) {
+        throw std::logic_error("blocked unregistered signal " + name(signum));
+    }
+    it->second(signum);
+}
+
+} // namespace halmd
+
+#endif /* ! HALMD_MDSIM_HPP */
---
+++ halmd/utility/posix_signal.hpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2008-2011  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_UTILITY_POSIX_SIGNAL_HPP
+#define HALMD_UTILITY_POSIX_SIGNAL_HPP
+
+#include <boost/unordered_map.hpp>
+#include <csignal>
+#include <string>
+
+#include <halmd/utility/signal.hpp>
+
+namespace halmd
+{
+
+/**
+ * POSIX signal handler
+ */
+class posix_signal
+{
+public:
+    typedef halmd::signal<void (int)> handler_type;
+    typedef handler_type::slot_function_type slot_function_type;
+    typedef boost::unordered_map<int, handler_type> handler_map_type;
+
+    posix_signal();
+    ~posix_signal();
+    void on_signal(int signum, slot_function_type const& slot);
+    void wait() const;
+    bool poll() const;
+    static std::string name(int signum);
+
+private:
+    void handle(int signum) const;
+
+    handler_map_type handler_;
+    sigset_t set_;
+    sigset_t oldset_;
+};
+
+} // namespace halmd
+
+#endif /* ! HALMD_UTILITY_POSIX_SIGNAL_HPP */
