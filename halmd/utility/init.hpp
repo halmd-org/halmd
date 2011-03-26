2ea9e5a6811912b1736bfa0df98ee02a393095d4
Peter Colberg <peter.colberg@utoronto.ca>
2011-03-25 21:08:56 -0400

    Implement portable alternative to __attribute__((constructor)).
    __attribute__((constructor)) is not supported by the XL C++ compiler.

---
+++ halmd/utility/init.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2011  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_UTILITY_INIT_HPP
+#define HALMD_UTILITY_INIT_HPP
+
+/**
+   Portable alternative to __attribute__((constructor)).
+
+   This macro may be used to define a function without arguments to be called
+   at program startup, *before* main(). This may for example be useful to
+   register Lua binding functions for C++ classes.
+
+   Example usage:
+
+   HALMD_INIT( register_lua )
+   {
+        // ...
+   }
+
+   Note that the order of static initialization is undefined. For consequences
+   refer to the C++ FAQ entry about “static initialization order fiasco”.
+
+   http://www.parashift.com/c++-faq-lite/ctors.html#faq-10.14
+ */
+#define HALMD_INIT( name ) namespace { struct name { name(); } _ ## name; } name::name()
+
+#endif /* ! HALMD_UTILITY_INIT_HPP */
