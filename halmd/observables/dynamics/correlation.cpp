03bd5d8c128c2d438265e095efae17645301347c
Felix Höfling <hoefling@mf.mpg.de>
2011-06-10 15:11:22 +0200

    rename dynamics::blocking_scheme to correlation
    This is the main module visible to the outside and should carry a clear name.
    The module dynamics::correlation has been renamed to tcf (time correlation function) which serves as a template for the various functors.

--- halmd/observables/dynamics/correlation.cpp
+++ halmd/observables/dynamics/correlation.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011  Felix Höfling
+ * Copyright © 2011  Peter Colberg and Felix Höfling
  *
  * This file is part of HALMD.
  *
@@ -17,14 +17,126 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/foreach.hpp>
+
+#include <halmd/io/logger.hpp>
 #include <halmd/observables/dynamics/correlation.hpp>
+#include <halmd/utility/lua/lua.hpp>
 
 using namespace boost;
+using namespace std;
 
 namespace halmd
 {
 namespace observables { namespace dynamics
 {
+
+correlation::correlation(
+    unsigned int block_count
+  , unsigned int block_size
+  , unsigned int shift
+  , double resolution
+)
+  // memory allocation
+  : interval_(block_count)
+  , time_(boost::extents[block_count][block_size])
+{
+    // setup sampling intervals for each level
+    interval_.reserve(block_count);
+    unsigned int i = 1;
+    for (unsigned int level = 0; level < block_count; level += 2) {
+        interval_.push_back(i);           // even levels
+        interval_.push_back(i * shift);   // odd levels
+        i *= block_size;
+    }
+
+    // construct associated time grid
+    for (unsigned int i = 0; i < block_count; ++i) {
+        for (unsigned int j = 0; j < block_size; ++j) {
+            time_[i][j] = interval_[i] * j;
+        }
+    }
+}
+
+void correlation::sample(uint64_t step)
+{
+    // trigger update of input sample(s)
+    on_sample_(step);
+
+    LOG_TRACE("[correlation] append sample(s) at step " << step);
+
+    // check integrity of input data
+    BOOST_FOREACH(shared_ptr<block_sample_type> block_sample, block_sample_) {
+        if (block_sample->timestamp() != step) {
+            throw logic_error("input sample was not updated");
+        }
+//         block_sample->make_copy();
+    }
+
+    // iterate over all coarse-graining levels
+    for (unsigned int i = 0; i < interval_.size(); ++i) {
+        if (step % interval_[i] == 0) {
+            // append current sample to block at level 'i' for each sample type
+            LOG_TRACE("[correlation] append sample(s) to blocking level " << i);
+            BOOST_FOREACH(shared_ptr<block_sample_type> block_sample, block_sample_) {
+                block_sample->push_back(i);
+            }
+
+            // process data if block at level 'i' is full
+            //
+            // Checking the first blocking scheme only is sufficient,
+            // since all of them are modified synchronously
+            if (block_sample_.begin()->get()->full(i)) {
+                // call all registered tcf modules
+                // and correlate block data with first entry
+                LOG_TRACE("[correlation] compute correlations at blocking level " << i);
+                BOOST_FOREACH(shared_ptr<tcf_base> tcf, tcf_) {
+                    tcf->compute(i);
+                }
+
+                // discard first entry at level 'i' for each block structure
+                BOOST_FOREACH(shared_ptr<block_sample_type> block_sample, block_sample_) {
+                    block_sample->pop_front(i);
+                }
+            }
+        }
+    }
+}
+
+void correlation::finalise(uint64_t step)
+{
+    // iterate over all coarse-graining levels
+    for (unsigned int i = 0; i < interval_.size(); ++i) {
+        // process remaining data at level 'i'
+        while (!block_sample_.begin()->get()->empty(i)) {
+            LOG_TRACE("[correlation] compute correlations at blocking level " << i);
+            // call all registered tcf modules
+            // and correlate block data with first entry
+            BOOST_FOREACH(shared_ptr<tcf_base> tcf, tcf_) {
+                tcf->compute(i);
+            }
+
+            // discard first entry at level 'i' for each block structure
+            BOOST_FOREACH(shared_ptr<block_sample_type> block_sample, block_sample_) {
+                block_sample->pop_front(i);
+            }
+        }
+    }
+}
+
+template <typename T>
+typename signal<void (uint64_t)>::slot_function_type
+sample_wrapper(shared_ptr<T> module)
+{
+    return bind(&T::sample, module, _1);
+}
+
+template <typename T>
+typename signal<void (uint64_t)>::slot_function_type
+finalise_wrapper(shared_ptr<T> module)
+{
+    return bind(&T::finalise, module, _1);
+}
 
 HALMD_LUA_API int luaopen_libhalmd_observables_dynamics_correlation(lua_State* L)
 {
@@ -35,7 +147,13 @@ HALMD_LUA_API int luaopen_libhalmd_observables_dynamics_correlation(lua_State* L
         [
             namespace_("dynamics")
             [
-                class_<correlation_base, shared_ptr<correlation_base> >("correlation_base")
+                class_<correlation, shared_ptr<correlation> >("correlation_")
+                    .def(constructor<unsigned int, unsigned int, unsigned int, double>())
+                    .property("finalise", &finalise_wrapper<correlation>)
+                    .property("sample", &sample_wrapper<correlation>)
+                    .def("add_function", &correlation::add_function)
+                    .def("add_data", &correlation::add_data)
+                    .def("on_sample", &correlation::on_sample)
             ]
         ]
     ];
