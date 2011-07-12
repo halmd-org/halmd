603913feaab229ca6d4ec90fdaa06c8e70aebc02
Peter Colberg <peter.colberg@utoronto.ca>
2011-07-12 12:47:25 -0400

    io::writers::h5md: add H5MD append mode writer.
    This module implements collective writing to one or multiple H5MD
    datasets, where each dataset is a time series. Upon initialisation,
    the writer is assigned a collective H5MD group. A dataset within this
    group is created by connecting a data slot to the on_write signal.
    All datasets share common step and time datasets, which are linked
    into each dataset group upon connection.
    
    The writer provides a common write slot, which may be connected to
    the sampler to write to the datasets at a fixed interval. Further
    signals on_prepend_write and on_append_write are provided to call
    arbitrary slots before and after writing.

---
+++ halmd/io/writers/h5md/append.cpp
@@ -0,0 +1,233 @@
+/*
+ * Copyright © 2011  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <boost/algorithm/string/join.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <luabind/luabind.hpp>
+#include <luabind/out_value_policy.hpp>
+#include <stdexcept>
+
+#include <halmd/io/utility/hdf5.hpp>
+#include <halmd/io/writers/h5md/append.hpp>
+#include <halmd/numeric/blas/fixed_vector.hpp>
+#include <halmd/utility/lua/lua.hpp>
+
+using namespace boost;
+using namespace boost::algorithm; // join
+using namespace std;
+
+namespace halmd {
+namespace io {
+namespace writers {
+namespace h5md {
+
+append::append(
+    file_type const& file
+  , vector<string> const& location
+  , shared_ptr<clock_type const> clock
+)
+  : clock_(clock)
+{
+    if (location.size() < 1) {
+        throw invalid_argument("group location");
+    }
+    group_ = h5xx::open_group(file.root(), join(location, "/"));
+    step_ = h5xx::create_dataset<step_type>(group_, "step");
+    time_ = h5xx::create_dataset<time_type>(group_, "time");
+    group_.unlink("step");
+    group_.unlink("time");
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<T ()> const&
+)
+{
+    return h5xx::create_dataset<T>(group, name);
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<T const& ()> const&
+)
+{
+    return h5xx::create_dataset<T>(group, name);
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<T& ()> const&
+)
+{
+    return h5xx::create_dataset<T>(group, name);
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<vector<T> ()> const& slot
+)
+{
+    vector<T> data = slot();
+    return h5xx::create_dataset<vector<T> >(group, name, data.size());
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<vector<T> const& ()> const& slot
+)
+{
+    vector<T> const& data = slot();
+    return h5xx::create_dataset<vector<T> >(group, name, data.size());
+}
+
+template <typename T>
+static H5::DataSet create_dataset(
+    H5::Group const& group
+  , string const& name
+  , function<vector<T>& ()> const& slot
+)
+{
+    vector<T>& data = slot();
+    return h5xx::create_dataset<vector<T> >(group, name, data.size());
+}
+
+template <typename slot_type>
+static void write_dataset(H5::DataSet dataset, slot_type const& slot)
+{
+    typename slot_type::result_type data = slot();
+    h5xx::write_dataset(dataset, data);
+}
+
+template <typename slot_type>
+void append::on_write(
+    H5::Group& group
+  , slot_type const& slot
+  , vector<string> const& location
+)
+{
+    if (location.size() < 1) {
+        throw invalid_argument("dataset location");
+    }
+    group = h5xx::open_group(group_, join(location, "/"));
+    H5::DataSet dataset = create_dataset(group, "samples", slot);
+    h5xx::link(step_, group, "step");
+    h5xx::link(time_, group, "time");
+    on_write_.connect(bind(&write_dataset<slot_type>, dataset, slot));
+}
+
+void append::on_prepend_write(signal<void (uint64_t)>::slot_function_type const& slot)
+{
+    on_prepend_write_.connect(slot);
+}
+
+void append::on_append_write(signal<void (uint64_t)>::slot_function_type const& slot)
+{
+    on_append_write_.connect(slot);
+}
+
+void append::write(uint64_t step)
+{
+    on_prepend_write_(step);
+    write_step_time();
+    on_write_();
+    on_append_write_(step);
+}
+
+void append::write_step_time()
+{
+    h5xx::write_dataset(step_, clock_->step());
+    h5xx::write_dataset(time_, clock_->time());
+}
+
+static signal<void (uint64_t)>::slot_function_type
+wrap_write(shared_ptr<append> instance)
+{
+    return bind(&append::write, instance, _1);
+}
+
+void append::luaopen(lua_State* L)
+{
+    using namespace luabind;
+    module(L, "libhalmd")
+    [
+        namespace_("io")
+        [
+            namespace_("writers")
+            [
+                namespace_("h5md")
+                [
+                    class_<append, shared_ptr<append> >("append")
+                        .def(constructor<file const&, vector<string> const&, shared_ptr<clock_type const> >())
+                        .property("write", &wrap_write)
+                        .def("on_write", &append::on_write<function<float ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<double ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<float const&()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<double const&()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<float, 2> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<float, 3> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<double, 2> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<double, 3> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<float, 2> const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<float, 3> const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<double, 2> const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<fixed_vector<double, 3> const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<float> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<double> ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<float> const&()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<double> const&()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<float, 2> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<float, 3> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<double, 2> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<double, 3> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<float, 2> > const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<float, 3> > const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<double, 2> > const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<fixed_vector<double, 3> > const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<array<float, 3> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<array<double, 3> > ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<array<float, 3> > const& ()> >, pure_out_value(_2))
+                        .def("on_write", &append::on_write<function<vector<array<double, 3> > const& ()> >, pure_out_value(_2))
+                        .def("on_prepend_write", &append::on_prepend_write)
+                        .def("on_append_write", &append::on_append_write)
+                ]
+            ]
+        ]
+    ];
+}
+
+HALMD_LUA_API int luaopen_libhalmd_io_writers_h5md_append(lua_State* L)
+{
+    append::luaopen(L);
+    return 0;
+}
+
+} // namespace h5md
+} // namespace writers
+} // namespace io
+} // namespace halmd
---
+++ halmd/io/writers/h5md/append.hpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright © 2011  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_IO_WRITERS_H5MD_APPEND_HPP
+#define HALMD_IO_WRITERS_H5MD_APPEND_HPP
+
+#include <lua.hpp>
+
+#include <halmd/io/writers/h5md/file.hpp>
+#include <halmd/mdsim/clock.hpp>
+#include <halmd/utility/signal.hpp>
+
+namespace halmd {
+namespace io {
+namespace writers {
+namespace h5md {
+
+/**
+ * H5MD dataset writer (append mode)
+ *
+ * This module implements collective writing to one or multiple H5MD
+ * datasets, where each dataset is a time series. Upon initialisation,
+ * the writer is assigned a collective H5MD group. A dataset within this
+ * group is created by connecting a data slot to the on_write signal.
+ * All datasets share common step and time datasets, which are linked
+ * into each dataset group upon connection.
+ *
+ * The writer provides a common write slot, which may be connected to
+ * the sampler to write to the datasets at a fixed interval. Further
+ * signals on_prepend_write and on_append_write are provided to call
+ * arbitrary slots before and after writing.
+ */
+class append
+{
+public:
+    typedef h5md::file file_type;
+    typedef mdsim::clock clock_type;
+    typedef clock_type::step_type step_type;
+    typedef clock_type::time_type time_type;
+    typedef signal<void ()> signal_type;
+    typedef signal_type::slot_function_type slot_function_type;
+
+    /** open writer group and create time and step datasets */
+    append(
+        file_type const& file
+      , std::vector<std::string> const& location
+      , boost::shared_ptr<clock_type const> clock
+    );
+    /** append datasets */
+    void write(uint64_t step);
+    /** connect data slot for writing dataset */
+    template <typename slot_type>
+    void on_write(
+        H5::Group& group
+      , slot_type const& slot
+      , std::vector<std::string> const& location
+    );
+    /** connect slot called before writing */
+    void on_prepend_write(signal<void (uint64_t)>::slot_function_type const& slot);
+    /** connect slot called after writing */
+    void on_append_write(signal<void (uint64_t)>::slot_function_type const& slot);
+    /** Lua bindings */
+    static void luaopen(lua_State* L);
+
+private:
+    /** append shared step and time datasets */
+    void write_step_time();
+
+    /** writer group */
+    H5::Group group_;
+    /** signal emitted for writing datasets */
+    signal_type on_write_;
+    /** signal emitted before writing datasets */
+    signal<void (uint64_t)> on_prepend_write_;
+    /** signal emitted before after datasets */
+    signal<void (uint64_t)> on_append_write_;
+    /** simulation step and time */
+    boost::shared_ptr<clock_type const> clock_;
+    /** shared step dataset */
+    H5::DataSet step_;
+    /** shared time dataset */
+    H5::DataSet time_;
+};
+
+} // namespace h5md
+} // namespace writers
+} // namespace io
+} // namespace halmd
+
+#endif /* ! HALMD_IO_WRITERS_H5MD_APPEND_HPP */
