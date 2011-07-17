7fa0b8a1406abadaa727c0d1c5c2e51582c4ed03
Peter Colberg <peter.colberg@utoronto.ca>
2011-07-17 11:28:53 -0400

    {gpu, host}::velocities::phase_space: support module logger.

--- halmd/mdsim/host/velocities/phase_space.hpp
+++ halmd/mdsim/host/velocities/phase_space.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2008-2010  Peter Colberg
+ * Copyright © 2008-2011  Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -20,8 +20,10 @@
 #ifndef HALMD_MDSIM_HOST_VELOCITIES_PHASE_SPACE_HPP
 #define HALMD_MDSIM_HOST_VELOCITIES_PHASE_SPACE_HPP
 
+#include <boost/make_shared.hpp>
 #include <lua.hpp>
 
+#include <halmd/io/logger.hpp>
 #include <halmd/mdsim/host/particle.hpp>
 #include <halmd/mdsim/host/velocity.hpp>
 #include <halmd/observables/host/samples/phase_space.hpp>
@@ -40,17 +42,21 @@ public:
     typedef host::particle<dimension, float_type> particle_type;
     typedef typename particle_type::vector_type vector_type;
     typedef observables::host::samples::phase_space<dimension, float_type> sample_type;
-
-    boost::shared_ptr<particle_type> particle;
-    boost::shared_ptr<sample_type> sample;
+    typedef logger logger_type;
 
     static void luaopen(lua_State* L);
 
     phase_space(
         boost::shared_ptr<particle_type> particle
-      , boost::shared_ptr<sample_type> sample
+      , boost::shared_ptr<sample_type const> sample
+      , boost::shared_ptr<logger_type> logger = boost::make_shared<logger_type>()
     );
     virtual void set();
+
+private:
+    boost::shared_ptr<particle_type> particle_;
+    boost::shared_ptr<sample_type const> sample_;
+    boost::shared_ptr<logger_type> logger_;
 };
 
 } // namespace mdsim
