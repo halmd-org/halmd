1c1e63706db9152a12f4a1b47d3b7b32f8bba8d6
Peter Colberg <peter.colberg@utoronto.ca>
2011-08-01 17:21:54 -0400

    dynamics::blocking_scheme: fix access of block sample through raw pointer.
    Never access a shared_ptr through get(), always use the dereference operator.
    
    http://www.boost.org/doc/libs/release/libs/smart_ptr/shared_ptr.htm#get
    
    While dereferencing an empty shared_ptr triggers an assertion
    error (if NDEBUG is not defined), shared_ptr::get returns NULL.

--- halmd/observables/dynamics/blocking_scheme.cpp
+++ halmd/observables/dynamics/blocking_scheme.cpp
@@ -115,7 +115,7 @@ void blocking_scheme::sample()
             //
             // Checking the first blocking scheme only is sufficient,
             // since all of them are modified synchronously
-            if (block_sample_.begin()->get()->full(i)) {
+            if ((*block_sample_.begin())->full(i)) {
                 // call all registered correlation modules
                 // and correlate block data with first entry
                 LOG_TRACE("compute correlations at blocking level " << i);
@@ -137,7 +137,7 @@ void blocking_scheme::finalise()
     // iterate over all coarse-graining levels
     for (unsigned int i = 0; i < interval_.size(); ++i) {
         // process remaining data at level 'i'
-        while (!block_sample_.begin()->get()->empty(i)) {
+        while (!(*block_sample_.begin())->empty(i)) {
             LOG_TRACE("compute correlations at blocking level " << i);
             // call all registered correlation modules
             // and correlate block data with first entry
