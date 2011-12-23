55d8d6413ef5dcfa57f500cfb14b098ad53933c0
Peter Colberg <peter.colberg@utoronto.ca>
2011-12-23 18:02:13 -0500

    cmake: find Lua interpreter as LUA_EXECUTABLE

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -67,14 +67,31 @@ if(LUA_LIBRARY)
   endif(UNIX AND NOT APPLE)
 endif(LUA_LIBRARY)
 
+find_program(LUA_EXECUTABLE
+  NAMES lua lua52 lua5.2 lua-5.2 lua51 lua5.1 lua-5.1
+  HINTS
+  $ENV{LUA_DIR}
+  PATH_SUFFIXES bin
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local
+  /usr
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+)
+
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LUA_FOUND to TRUE if 
 # all listed variables are TRUE
-find_package_handle_standard_args(Lua  DEFAULT_MSG  LUA_LIBRARIES LUA_INCLUDE_DIR)
+find_package_handle_standard_args(Lua  DEFAULT_MSG  LUA_LIBRARIES LUA_INCLUDE_DIR LUA_EXECUTABLE)
 
 mark_as_advanced(
   LUA_INCLUDE_DIR
   LUA_LIBRARIES
   LUA_LIBRARY
   LUA_MATH_LIBRARY
+  LUA_EXECUTABLE
 )
