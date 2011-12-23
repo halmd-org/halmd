1d6871f4416d5a054a63f477fef473908b985e73
Peter Colberg <peter.colberg@utoronto.ca>
2011-12-23 17:59:45 -0500

    cmake: tidy FindLua
    Convert CMake commands to lowercase

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -13,6 +13,7 @@
 
 #=============================================================================
 # Copyright 2007-2009 Kitware, Inc.
+# Copyright 2011 Peter Colberg
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -24,7 +25,7 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-FIND_PATH(LUA_INCLUDE_DIR lua.h
+find_path(LUA_INCLUDE_DIR lua.h
   HINTS
   $ENV{LUA_DIR}
   PATH_SUFFIXES include/lua52 include/lua5.2 include/lua51 include/lua5.1 include/lua include
@@ -39,7 +40,7 @@ FIND_PATH(LUA_INCLUDE_DIR lua.h
   /opt
 )
 
-FIND_LIBRARY(LUA_LIBRARY 
+find_library(LUA_LIBRARY 
   NAMES lua lua52 lua5.2 lua-5.2 lua51 lua5.1 lua-5.1
   HINTS
   $ENV{LUA_DIR}
@@ -55,21 +56,25 @@ FIND_LIBRARY(LUA_LIBRARY
   /opt
 )
 
-IF(LUA_LIBRARY)
+if(LUA_LIBRARY)
   # include the math library for Unix
-  IF(UNIX AND NOT APPLE)
-    FIND_LIBRARY(LUA_MATH_LIBRARY m)
-    SET( LUA_LIBRARIES "${LUA_LIBRARY};${LUA_MATH_LIBRARY}" CACHE STRING "Lua Libraries")
+  if(UNIX AND NOT APPLE)
+    find_library(LUA_MATH_LIBRARY m)
+    set( LUA_LIBRARIES "${LUA_LIBRARY};${LUA_MATH_LIBRARY}" CACHE STRING "Lua Libraries")
   # For Windows and Mac, don't need to explicitly include the math library
-  ELSE(UNIX AND NOT APPLE)
-    SET( LUA_LIBRARIES "${LUA_LIBRARY}" CACHE STRING "Lua Libraries")
-  ENDIF(UNIX AND NOT APPLE)
-ENDIF(LUA_LIBRARY)
+  else(UNIX AND NOT APPLE)
+    set( LUA_LIBRARIES "${LUA_LIBRARY}" CACHE STRING "Lua Libraries")
+  endif(UNIX AND NOT APPLE)
+endif(LUA_LIBRARY)
 
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LUA_FOUND to TRUE if 
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Lua  DEFAULT_MSG  LUA_LIBRARIES LUA_INCLUDE_DIR)
+find_package_handle_standard_args(Lua  DEFAULT_MSG  LUA_LIBRARIES LUA_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(LUA_INCLUDE_DIR LUA_LIBRARIES LUA_LIBRARY LUA_MATH_LIBRARY)
-
+mark_as_advanced(
+  LUA_INCLUDE_DIR
+  LUA_LIBRARIES
+  LUA_LIBRARY
+  LUA_MATH_LIBRARY
+)
