d8485352a37ca6da60ba52502672bd327d661bb3
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-03-19 20:41:23 -0400

    Purge deprecated Luabind converters for boost::any and std::map

--- halmd/utility/lua/lua.hpp
+++ halmd/utility/lua/lua.hpp
@@ -31,7 +31,6 @@
 #include <halmd/utility/lua/array_converter.hpp>
 #include <halmd/utility/lua/fixed_vector_converter.hpp>
 #include <halmd/utility/lua/long_long_converter.hpp>
-#include <halmd/utility/lua/map_converter.hpp>
 #include <halmd/utility/lua/optional_converter.hpp>
 #include <halmd/utility/lua/ublas.hpp>
 #include <halmd/utility/lua/vector_converter.hpp>
