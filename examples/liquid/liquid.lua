41dfcf2ac413248649b0190b8c307b939d01baa9
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-03-26 12:11:05 -0400

    examples/liquid: parse script arguments, and setup logging and device

--- examples/liquid/liquid.lua
+++ examples/liquid/liquid.lua
@@ -1,5 +1,5 @@
 --
--- Copyright © 2010-2011  Peter Colberg and Felix Höfling
+-- Copyright © 2010-2012  Peter Colberg and Felix Höfling
 --
 -- This file is part of HALMD.
 --
@@ -20,7 +20,7 @@
 local halmd = require("halmd")
 
 -- grab modules
-local device = halmd.device
+local log = halmd.io.log
 local mdsim = halmd.mdsim
 local observables = halmd.observables
 local readers = halmd.io.readers
@@ -29,9 +29,7 @@ local writers = halmd.io.writers
 --
 -- Setup and run simulation
 --
-local function script()
-    -- load the device module to log (optional) GPU properties
-    device() -- singleton
+local function liquid(args)
     -- open (optional) H5MD file and read simulation parameters
     local reader = readers.trajectory()
 
@@ -69,20 +67,42 @@ local function script()
     coroutine.yield(sampler:run())
 end
 
--- FIXME explicitly load modules to collect deprecated module options
-require("halmd.option")
-require("halmd.log")
-require("halmd.device")
-require("halmd.io.readers.trajectory")
-require("halmd.io.writers.trajectory")
-require("halmd.mdsim.box")
-require("halmd.mdsim.force")
-require("halmd.mdsim.integrator")
-require("halmd.mdsim.position")
-require("halmd.mdsim.velocity")
-require("halmd.observables.dynamics.correlation")
-require("halmd.observables.sampler")
-require("halmd.observables.ssf")
-require("halmd.observables.thermodynamics")
+--
+-- Parse command-line arguments.
+--
+local function parse_args()
+    local parser = halmd.utility.program_options.argument_parser()
 
-return script
+    parser:add_argument("output,o", {type = "string", action = function(args, key, value)
+        -- substitute current time
+        args[key] = os.date(value)
+    end, default = "liquid_%Y%m%d_%H%M%S", help = "prefix of output files"})
+
+    parser:add_argument("verbose,v", {type = "accumulate", action = function(args, key, value)
+        local level = {
+            -- console, file
+            {"warning", "info" },
+            {"info"   , "info" },
+            {"debug"  , "debug"},
+            {"trace"  , "trace"},
+        }
+        args[key] = level[value] or level[#level]
+    end, default = 1, help = "increase logging verbosity"})
+
+    parser:add_argument("disable-gpu", {type = "boolean", help = "disable GPU acceleration"})
+    parser:add_argument("devices", {type = "vector", dtype = "integer", help = "CUDA device(s)"})
+
+    return parser:parse_args()
+end
+
+local args = parse_args()
+
+-- log to console
+halmd.io.log.open_console({severity = args.verbose[1]})
+-- log to file
+halmd.io.log.open_file(("%s.log"):format(args.output), {severity = args.verbose[2]})
+-- initialise or disable GPU
+halmd.utility.device({disable_gpu = args.disable_gpu, devices = args.devices})
+
+-- run simulation
+liquid(args)
