bccfbefcba018d9a75e990642daad445a379bd63
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-03-28 17:35:07 -0400

    script: throw exception with error message from stack

--- halmd/script.cpp
+++ halmd/script.cpp
@@ -195,9 +195,9 @@ void script::dofile(string const& filename)
     lua_pushcfunction(L, &script::traceback);
 
     if (luaL_loadfile(L, fn) || lua_pcall(L, 0, 0, 1)) {
-        LOG_ERROR(lua_tostring(L, -1));
-        lua_pop(L, 1); //< remove error message
-        throw runtime_error("failed to run script");
+        string error(lua_tostring(L, -1));
+        lua_pop(L, 1);
+        throw runtime_error(error);
     }
 }
 
