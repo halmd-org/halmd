2ade180df1e387c2a90ead537116aed42917589c
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-04-23 23:38:21 -0400

    utility/gpu/shared_memory: calculate maximum number of threads for shared memory array
    Given the element type of a shared memory array, calculates the maximum
    number of threads per block supported by the GPU architecture, as the
    next-lower power of 2.

---
+++ halmd/utility/gpu/shared_memory.hpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright © 2012  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_UTILITY_GPU_SHARED_MEMORY_HPP
+#define HALMD_UTILITY_GPU_SHARED_MEMORY_HPP
+
+#include <boost/utility/enable_if.hpp>
+
+#include <halmd/config.hpp> // HALMD_GPU_ARCH
+
+namespace halmd {
+
+/**
+ * Maximum number of threads per block.
+ */
+struct max_threads_per_block
+{
+#if HALMD_GPU_ARCH >= 200
+    enum { value = 1024 };
+#else
+    enum { value = 512 };
+#endif
+};
+
+/**
+ * Minimum number of threads per block.
+ *
+ * This is equivalent to the warp size.
+ */
+struct min_threads_per_block
+{
+    enum { value = 32 };
+};
+
+/**
+ * Maximum shared memory in bytes
+ */
+struct max_shared_memory
+{
+#if HALMD_GPU_ARCH >= 200
+    enum { value = 49152 };
+#else
+    enum { value = 16384 };
+#endif
+};
+
+/**
+ * Given an upper bound on the number of threads, calculates next-lower
+ * power of 2. If the number of threads exceeds the maximum number of
+ * threads per block, returns the maximum number of threads per block.
+ */
+template <unsigned int upper, unsigned int accumulator = 1, typename Enable = void>
+struct power_of_two_max_threads;
+
+template <unsigned int upper, unsigned int accumulator>
+struct power_of_two_max_threads<upper, accumulator
+  , typename boost::enable_if_c<
+        (((upper > 1) && (accumulator == max_threads_per_block::value)) || (upper == 1))
+    >::type>
+{
+    enum { value = accumulator };
+};
+
+template <unsigned int upper, unsigned int accumulator>
+struct power_of_two_max_threads<upper, accumulator
+  , typename boost::enable_if_c<
+        ((upper > 1) && (accumulator < max_threads_per_block::value))
+    >::type>
+{
+    enum { value = power_of_two_max_threads<(upper >> 1), (accumulator << 1)>::value };
+};
+
+/**
+ * Given the element type of a shared memory array, calculates the maximum
+ * number of threads per block supported by the GPU architecture, as the
+ * next-lower power of 2.
+ *
+ * This metafunction calculates the maximum number of threads by dividing
+ * the maximum shared memory by the element size, and then rounding the
+ * result down to the next-lower power of 2.
+ *
+ * Note that with PTX version 1.3 and older, kernel arguments are passed
+ * via shared memory. To account for the decrease in available shared
+ * memory, this metafunction substracts 1 from the maximum number of
+ * threads before rounding to the next-lower power of 2.
+ * As an example, if the unrounded number of threads is exactly 512,
+ * subtracting 1 will yield a next-lower power of two of 256 threads.
+ *
+ * For PTX version 2.0 and higher, kernel arguments are passed
+ * via constant memory, thus this correction is not necessary.
+ */
+template <typename T>
+struct shared_memory_max_threads
+{
+#if HALMD_GPU_ARCH >= 200
+    enum { value = power_of_two_max_threads<max_shared_memory::value / sizeof(T)>::value };
+#else
+    enum { value = power_of_two_max_threads<(max_shared_memory::value / sizeof(T)) - 1>::value };
+#endif
+};
+
+} // namespace halmd
+
+#endif /* ! HALMD_UTILITY_GPU_SHARED_MEMORY_HPP */
---
+++ test/unit/utility/gpu/shared_memory.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2012  Peter Colberg
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE shared_memory
+#include <boost/test/unit_test.hpp>
+
+#include <boost/array.hpp>
+#include <halmd/utility/gpu/shared_memory.hpp>
+
+using namespace halmd;
+
+BOOST_AUTO_TEST_CASE( test_power_of_two_max_threads )
+{
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<32>::value), 32 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<33>::value), 32 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<42>::value), 32 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<63>::value), 32 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<64>::value), 64 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<65>::value), 64 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<127>::value), 64 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<128>::value), 128 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<129>::value), 128 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<255>::value), 128 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<256>::value), 256 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<257>::value), 256 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<511>::value), 256 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<512>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<513>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<1023>::value), 512 );
+#if HALMD_GPU_ARCH >= 200
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<1024>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<1025>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2047>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2048>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2049>::value), 1024 );
+#else
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<1024>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<1025>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2047>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2048>::value), 512 );
+    BOOST_CHECK_EQUAL( int(power_of_two_max_threads<2049>::value), 512 );
+#endif
+}
+
+typedef boost::array<float, 7> float7;
+typedef boost::array<double, 4> double4;
+typedef boost::array<double, 8> double8;
+
+BOOST_AUTO_TEST_CASE( test_shared_memory_max_threads )
+{
+#if HALMD_GPU_ARCH >= 200
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<char>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<short>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<float>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<float7>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double4>::value), 1024 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double8>::value), 512 );
+#else
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<char>::value), 512 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<short>::value), 512 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<float>::value), 512 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double>::value), 512 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<float7>::value), 512 );
+    // with PTX 1.3 and older, accounts for shared memory used by kernel arguments
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double4>::value), 256 );
+    BOOST_CHECK_EQUAL( int(shared_memory_max_threads<double8>::value), 128 );
+#endif
+}
