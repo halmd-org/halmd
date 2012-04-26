9c2b32c9519e49794aec8a795d4ae5b609ccee45
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-04-25 20:02:26 -0400

    cmake: move C++ compiler flags to platform.cmake
    Note that CMake includes platform.cmake twice, once for the project()
    command that enables C++, and once for enable_language(CUDA).

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,3 +1,34 @@
+if(DEFINED CMAKE_CXX_COMPILER_ID)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall -std=c++98")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility-inlines-hidden")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "XL")
+
+    set(CMAKE_CXX_FLAGS_INIT "-qrtti=all")
+
+    # We only need object-level inlining (opposed to link-time optimization),
+    # therefore we follow the recommendation for XL 11 that -qipa=inline is
+    # deprecated, and use -qinline instead.
+    # http://publib.boulder.ibm.com/infocenter/iadthelp/v8r0/index.jsp?topic=/com.ibm.xlcpp111.linux.doc/compiler_ref/opt_ipa.html
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O4 -DNDEBUG -qstrict=all -qnoipa -qinline")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT "-O4 -DNDEBUG -qstrict=all -qnoipa -qinline")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-g -O2 -qstrict=all")
+
+  else()
+    message(WARNING "Unsupported CXX compiler: ${CMAKE_CXX_COMPILER_ID}")
+  endif()
+endif()
+
 if(CMAKE_CXX_PLATFORM_ID STREQUAL "Linux")
 
   # On Linux, add --Wl,--as-needed to the default linker flags.
