61c09e0d0c4cffeb1a6af36f9596439a9bde001c
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-01 11:46:24 -0400

    cmake: add -pedantic -Wno-long-long to default GNU C++ compiler flags
    This enables further warnings for non-standard-conform code.
    
    The flag -Wno-long-long is needed to disable warnings about long long
    integers, which are used in the CUDA headers but not defined in the
    C++98 standard.
    
    While the -pendantic flag also enables less useful warnings such as
    duplicate semicolons after BOOST_GLOBAL_FIXTURE (which includes a
    semicolon itself), it reveals a programming error in the h5xx
    library with regard to scalar datasets.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,7 +1,7 @@
 if(DEFINED CMAKE_CXX_COMPILER_ID)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
-    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall -std=c++98")
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall -std=c++98 -pedantic -Wno-long-long")
     set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility-inlines-hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
