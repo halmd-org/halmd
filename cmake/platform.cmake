923fa40008b6d3242a3575721af5ccadc9e41974
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-13 14:13:17 -0400

    cmake: define BOOST_DISABLE_ASSERTS in default Release flags
    While BOOST_ASSERT is defined to a void expression if NDEBUG is defined,
    some Boost code uses BOOST_DISABLE_ASSERTS to exclude variable
    definitions, such as bound_adjustment in Boost.MultiArray.
    
    This fixes a warning with Clang 3.1.
    
    boost/multi_array/base.hpp:475:13: warning: unused variable 'bound_adjustment' [-Wunused-variable]
          index bound_adjustment = stride < 0 ? 1 : 0;
                ^

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -4,17 +4,17 @@ if(DEFINED CMAKE_CXX_COMPILER_ID)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
     set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall -std=c++98 -pedantic -Wno-long-long")
-    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility=hidden")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -DBOOST_DISABLE_ASSERTS -fvisibility=hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 
     set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
-    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility=hidden")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -DBOOST_DISABLE_ASSERTS -fvisibility=hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
 
     set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
-    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility=hidden")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -DBOOST_DISABLE_ASSERTS -fvisibility=hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "XL")
 
