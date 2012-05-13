halmd-org/halmd

commit 9c2b32c9519e49794aec8a795d4ae5b609ccee45

cmake: move C++ compiler flags to platform.cmake
Note that CMake includes platform.cmake twice, once for the project()
command that enables C++, and once for enable_language(CUDA).

cmake/platform.cmake
@@ -1,3 +1,34 @@
+if(DEFINED CMAKE_CXX_COMPILER_ID)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall -std=c++98")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility-inlines-hidden")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "XL")
+
+    set(CMAKE_CXX_FLAGS_INIT "-qrtti=all")
+
+    # We only need object-level inlining (opposed to link-time optimization),
+    # therefore we follow the recommendation for XL 11 that -qipa=inline is
+    # deprecated, and use -qinline instead.
+    # http://publib.boulder.ibm.com/infocenter/iadthelp/v8r0/index.jsp?topic=/com.ibm.xlcpp111.linux.doc/compiler_ref/opt_ipa.html
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O4 -DNDEBUG -qstrict=all -qnoipa -qinline")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT "-O4 -DNDEBUG -qstrict=all -qnoipa -qinline")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-g -O2 -qstrict=all")
+
+  else()
+    message(WARNING "Unsupported CXX compiler: ${CMAKE_CXX_COMPILER_ID}")
+  endif()
+endif()
+
 if(CMAKE_CXX_PLATFORM_ID STREQUAL "Linux")
 
   # On Linux, add --Wl,--as-needed to the default linker flags.

CMakeLists.txt
@@ -89,45 +89,6 @@ find_package(HDF5 QUIET REQUIRED)
 find_package(Lua QUIET REQUIRED)
 find_package(Luabind QUIET REQUIRED)
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "^(GNU|Clang|Intel)$")
-  if(CMAKE_CXX_FLAGS STREQUAL "${CMAKE_CXX_FLAGS_INIT}")
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      set(CMAKE_CXX_FLAGS "-fPIC -Wall -std=c++98" CACHE STRING
-        "Flags used by the compiler during all build types." FORCE)
-    else()
-      set(CMAKE_CXX_FLAGS "-fPIC -Wall" CACHE STRING
-        "Flags used by the compiler during all build types." FORCE)
-    endif()
-  endif()
-  if(CMAKE_CXX_FLAGS_RELEASE STREQUAL "${CMAKE_CXX_FLAGS_RELEASE_INIT}")
-    set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -fvisibility-inlines-hidden" CACHE STRING
-      "Flags used by the compiler during release builds." FORCE)
-  endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "XL")
-  if(CMAKE_CXX_FLAGS STREQUAL "${CMAKE_CXX_FLAGS_INIT}")
-    set(CMAKE_CXX_FLAGS "-qrtti=all" CACHE STRING
-      "Flags used by the compiler during all build types." FORCE)
-  endif()
-  # We only need object-level inlining (opposed to link-time optimization),
-  # therefore we follow the recommendation for XL 11 that -qipa=inline is
-  # deprecated, and use -qinline instead.
-  # http://publib.boulder.ibm.com/infocenter/iadthelp/v8r0/index.jsp?topic=/com.ibm.xlcpp111.linux.doc/compiler_ref/opt_ipa.html
-  if(CMAKE_CXX_FLAGS_RELEASE STREQUAL "${CMAKE_CXX_FLAGS_RELEASE_INIT}")
-    set(CMAKE_CXX_FLAGS_RELEASE "-O4 -DNDEBUG -qstrict=all -qnoipa -qinline" CACHE STRING
-      "Flags used by the compiler during Release builds" FORCE)
-  endif()
-  if(CMAKE_CXX_FLAGS_MINSIZEREL STREQUAL "${CMAKE_CXX_FLAGS_MINSIZEREL_INIT}")
-    set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_RELEASE}" CACHE STRING
-      "Flags used by the compiler during MinSizeRel builds" FORCE)
-  endif()
-  if(CMAKE_CXX_FLAGS_RELWITHDEBINFO STREQUAL "${CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O2 -qstrict=all" CACHE STRING
-      "Flags used by the compiler during RelWithDebInfo builds" FORCE)
-  endif()
-else()
-  message(WARNING "Unsupported CXX compiler: ${CMAKE_CXX_COMPILER_ID}")
-endif()
-
 if(CUDA_FOUND AND NOT HALMD_USE_STATIC_LIBS)
   set(WITH_CUDA TRUE)
   enable_language(CUDA)

commit a16adb8f8a865685a2de210c424004b697414733

cmake: fix regression in compiler flags for Clang and Intel C++ compiler
This fixes a regression introduced in commit:9c2b32c.

The regression became apparent with the following warning (Clang 3.1):

boost/multi_array/base.hpp:475:13: warning: unused variable 'bound_adjustment' [-Wunused-variable]
      index bound_adjustment = stride < 0 ? 1 : 0;
            ^

cmake/platform.cmake
@@ -9,10 +9,12 @@ if(DEFINED CMAKE_CXX_COMPILER_ID)
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 
     set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility=hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
 
     set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG -fvisibility=hidden")
 
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "XL")
 