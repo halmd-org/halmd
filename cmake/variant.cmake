fdefc1ff39baaca92d07f68eb958c582176e9aa6
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-15 18:40:24 -0400

    cmake: move HALMD build variant flags to separate CMake file
    This shortens the top-level CMake file.

---
+++ cmake/variant.cmake
@@ -0,0 +1,53 @@
+set(HALMD_VARIANT_HILBERT_ORDER TRUE CACHE BOOL
+  "Use Hilbert space-filling curve particle ordering")
+if(HALMD_VARIANT_HILBERT_ORDER)
+  add_definitions(-DUSE_HILBERT_ORDER)
+endif(HALMD_VARIANT_HILBERT_ORDER)
+
+set(HALMD_VARIANT_HILBERT_ALT_3D FALSE CACHE BOOL
+  "Use alternative 3D Hilbert curve vertex rules")
+if(HALMD_VARIANT_HILBERT_ALT_3D)
+  add_definitions(-DUSE_HILBERT_ALT_3D)
+endif(HALMD_VARIANT_HILBERT_ALT_3D)
+
+if(HALMD_WITH_GPU)
+  set(HALMD_VARIANT_CELL_SUMMATION_ORDER TRUE CACHE BOOL
+    "Use opposite cell summation order")
+  if(HALMD_VARIANT_CELL_SUMMATION_ORDER)
+    add_definitions(-DUSE_CELL_SUMMATION_ORDER)
+  endif(HALMD_VARIANT_CELL_SUMMATION_ORDER)
+
+  set(HALMD_VARIANT_FORCE_DSFUN TRUE CACHE BOOL
+    "Use double-single precision functions in cell summation")
+  if(HALMD_VARIANT_FORCE_DSFUN)
+    add_definitions(-DUSE_FORCE_DSFUN)
+  endif(HALMD_VARIANT_FORCE_DSFUN)
+
+  set(HALMD_VARIANT_VERLET_DSFUN TRUE CACHE BOOL
+    "Use double-single precision functions in Verlet integrator")
+  if(HALMD_VARIANT_VERLET_DSFUN)
+    add_definitions(-DUSE_VERLET_DSFUN)
+  endif(HALMD_VARIANT_VERLET_DSFUN)
+
+  set(HALMD_DEVICE_SCALE "3" CACHE STRING
+    "Scale/size of the CUDA device (try to reduce in case of insufficient resources)")
+  add_definitions(-DDEVICE_SCALE=${HALMD_DEVICE_SCALE})
+
+endif(HALMD_WITH_GPU)
+
+#
+# The following option only works on x86-64 by default, which always uses the
+# SSE instruction set for floating-point math. On i386, the x87 floating-point
+# unit provides 80-bit extended double precision math internally, causing
+# excess precision even if values are stored in single-precision.
+#
+# See the -mfpmath option in the gcc manpage for details, and
+#
+# Deterministic cross-platform floating point arithmetics
+# http://www.christian-seiler.de/projekte/fpmath/
+#
+set(HALMD_VARIANT_HOST_SINGLE_PRECISION FALSE CACHE BOOL
+  "Use single-precision math in host implementation (requires SSE)")
+if(HALMD_VARIANT_HOST_SINGLE_PRECISION)
+  add_definitions(-DUSE_HOST_SINGLE_PRECISION)
+endif(HALMD_VARIANT_HOST_SINGLE_PRECISION)
