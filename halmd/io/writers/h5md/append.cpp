halmd-org/halmd

commit aeb13a9dfb1aa41fb694fc9295ab7a0758569c50

lua: use particle_group in trajectory writer and phase space sampler
Let the phase_space sampler process a list of particle groups.

This breaks the output of particle masses via the trajectory writer, which
should be reconsidered anyway.

lua/halmd/observables/phase_space.lua.in
@@ -17,12 +17,15 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+require("halmd.device")
 require("halmd.mdsim.box")
 require("halmd.mdsim.clock")
 require("halmd.modules")
+require("halmd.observables.samples.particle_group")
 require("halmd.observables.samples.phase_space")
 
 -- grab modules
+local device = halmd.device
 local mdsim = halmd.mdsim
 local observables = halmd.observables
 -- grab C++ wrappers
@@ -30,6 +33,8 @@ local phase_space_wrapper = assert(libhalmd.observables.phase_space)
 local property = property
 -- grab standard library
 local assert = assert
+local ipairs = ipairs
+local type = type
 
 module("halmd.observables.phase_space", halmd.modules.register)
 
@@ -37,23 +42,37 @@ module("halmd.observables.phase_space", halmd.modules.register)
 -- construct phase_space module
 --
 -- @param args       parameter table
---   args.memory     specify memory location of phase_space sample ("host", "gpu")
+--   args.particle_group    list of pairs (label, particle_group instance)
+--   args.memory            specify memory location of phase_space sample ("host", "gpu"),
+--                          if omitted use "gpu" if device is available
+-- @returns list of pairs (label, phase_space sampler instance)
 --
 function new(args)
     -- parameters
-    local particle = assert(args.particle, "missing parameter: particle")
-    local memory = assert(args.memory)
+    local particle_group = assert(args.particle_group, "missing parameter: particle_group")
+    assert(type(particle_group) == "table", "invalid table")
+    local memory = args.memory or (device() and "gpu" or "host")
+    local logger = assert(args.logger)
 
     -- dependency injection
-    local dimension = assert(particle.dimension)
     local box = mdsim.box() -- singleton
     local clock = mdsim.clock() -- singleton
-    local sample = assert(observables.samples.phase_space{particle = particle, memory = memory})
-    local logger = assert(args.logger)
-    local phase_space = phase_space_wrapper(sample, particle, box, clock, logger)
 
-    -- store phase space sample as property
-    phase_space.sample = property(function(self) return sample end)
+    local phase_space = {}
+    for i, val in ipairs(particle_group) do
+        local label = val[1]
+        local group = val[2]
+        local sample = assert(observables.samples.phase_space{particle_group = group, memory = memory})
+        local sampler = phase_space_wrapper(sample, group, box, clock, logger)
+
+        -- store phase space sample as property
+        sampler.sample = property(function(self) return sample end)
+
+        -- store memory as property for profiler description
+        sampler.memory = property(function(self) return memory end)
+
+        phase_space[i] = { label, sampler }
+    end
 
     return phase_space
 end
@@ -62,7 +81,12 @@ end
 -- connect runtime accumulators to module profiler
 --
 function profile(self, profiler)
-    local memory = assert(self.sample.memory)
-    profiler:on_profile{self, acquire = "acquisition of phase space sample on " .. memory}
-    profiler:on_profile{self, reset = "reset phase space sample on " .. memory}
+    for i, val in ipairs(self) do
+        local label = val[1]
+        local phase_space = val[2]
+        local memory = assert(phase_space.memory)
+        local str = "phase space sample of " .. label .. " particles on " .. memory
+        profiler:on_profile{phase_space, acquire = "acquisition of " .. str}
+        profiler:on_profile{phase_space, reset = "reset " .. str}
+    end
 end

lua/halmd/observables/samples/phase_space.lua.in
@@ -54,23 +54,21 @@ module("halmd.observables.samples.phase_space", halmd.modules.register)
 -- construct phase_space module
 --
 -- @param args       parameter table
---   args.memory     specify memory location of sample ("host", "gpu")
+--   args.particle_group    instance of particle_group specifying data type and size
+--   args.memory            specify memory location of sample ("host", "gpu")
 --
 function new(args)
-    local particle = assert(args.particle, "missing parameter: particle")
-    local memory = assert(args.memory)
+    local particle_group = assert(args.particle_group, "missing parameter: particle_group")
+    local memory = assert(args.memory, "missing parameter: memory")
 
-    local dimension = assert(particle.dimension)
+    local dimension = assert(particle_group.dimension)
     local phase_space
     if device() then
         phase_space = assert(phase_space_wrapper.gpu[memory][dimension])
     else
         phase_space = assert(phase_space_wrapper.host[memory][dimension])
     end
-    local sample = phase_space(particle.nbox)
-
-    -- store memory as property for profiler description
-    sample.memory = property(function(self) return memory end)
+    local sample = phase_space(particle_group.size)
 
     return sample
 end

lua/halmd/io/writers/trajectory.lua.in
@@ -33,24 +33,27 @@ local po = libhalmd.po
 local h5 = libhalmd.h5
 -- grab standard library
 local assert = assert
+local ipairs = ipairs
 local string = string
+local type = type
 
 module("halmd.io.writers.trajectory", halmd.modules.register)
 
 --
 -- construct trajectory writer module
 --
+-- @param args       parameter table
+--   args.particle_group    list of pairs (label, particle_group instance)
+--   args.every             sampling interval, default (0) writes at start and finish only
+--
 function new(args)
-    local particle = assert(args.particle, "missing parameter: particle")
-    local group = assert(args.group, "missing parameter: group")
-    local every = args.every or 1000 -- default value
+    local particle_group = assert(args.particle_group, "missing parameter: particle_group")
+    assert(type(particle_group) == "table", "invalid table")
+    local every = args.every or 0 -- default value
 
     -- create trajectory file
     local file = writers.file{extension = "trj"}
 
-    -- FIXME share phase_space instance with observables::host::density_mode
-    local phase_space = assert(observables.phase_space{particle = particle, memory = "host"})
-    local sample = assert(phase_space.sample)
     local box = mdsim.box() -- singleton
     local sampler = observables.sampler() -- singleton
 
@@ -63,16 +66,31 @@ function new(args)
     writer:on_write(box.origin, {"offset"})
     sampler:on_start{writer.write, tracker = writer}
 
-    -- sample and write phase space at fixed interval
-    local writer = file:writer{location = {"trajectory", group}, mode = "append"}
-    writer:on_prepend_write(phase_space.acquire)
-    writer:on_write(sample.position, {"position"})
-    writer:on_write(sample.velocity, {"velocity"})
-    local _, species = writer:on_write(sample.types, {"species"})
-    sampler:on_sample{writer.write, every, tracker = writer}
+    -- construct list of phase space samplers
+    --
+    -- FIXME share phase_space instance with observables::host::density_mode -- is this a worthy optimisation?
+    local phase_space = assert(observables.phase_space{particle_group = particle_group, memory = "host"})
 
-    -- store attributes
-    species:write_attribute("mass", h5.float_array(), particle.mass)
+    for i, val in ipairs(phase_space) do
+        local label = val[1]
+        local phase_space = val[2]
+        local sample = assert(phase_space.sample)
+
+        -- sample and write phase space at fixed interval
+        local writer = file:writer{location = {"trajectory", label}, mode = "append"}
+        writer:on_prepend_write(phase_space.acquire)
+        writer:on_write(sample.position, {"position"})
+        writer:on_write(sample.velocity, {"velocity"})
+        local _, species = writer:on_write(sample.types, {"species"})
+        if every > 0 then
+            sampler:on_sample{writer.write, every, tracker = writer}
+        else
+            sampler:on_finish{writer.write, tracker = writer}
+        end
+
+        -- store attributes
+--         species:write_attribute("mass", h5.float_array(), particle.mass) -- FIXME
+    end
 
     return writer
 end

commit b51522cd14bbf0b7c7b8a7f6f6df8966aca62686

phase_space: write and read particle masses
This completes support of resuming from an H5MD trajectory file.

Note that this implementation is far from flexible due to the inflexible
phase space sample, which does not allow partial sampling of particle
arrays. This will be resolved in the future, by splitting phase space
sample into its component arrays.

This adds partial support for chemical reactions. Note that we do not
support a varying total number of particles yet, only fluctuations
between species.

The H5MD writer enables compression, therefore the overhead of storing
masses per particle for systems with per-species masses, e.g. a binary
Lennard-Jones mixture, should be negligible.

This commit resolves a regression from commit:aeb13a9.

halmd/io/writers/h5md/append.cpp
@@ -222,6 +222,10 @@ void append::luaopen(lua_State* L)
                         .def("on_write", &append::on_write<raw_array<fixed_vector<double, 2>> const&>, pure_out_value(_2))
                         .def("on_write", &append::on_write<raw_array<fixed_vector<double, 3>>&>, pure_out_value(_2))
                         .def("on_write", &append::on_write<raw_array<fixed_vector<double, 3>> const&>, pure_out_value(_2))
+                        .def("on_write", &append::on_write<raw_array<float>&>, pure_out_value(_2))
+                        .def("on_write", &append::on_write<raw_array<float> const&>, pure_out_value(_2))
+                        .def("on_write", &append::on_write<raw_array<double>&>, pure_out_value(_2))
+                        .def("on_write", &append::on_write<raw_array<double> const&>, pure_out_value(_2))
                         .def("on_write", &append::on_write<raw_array<unsigned int>&>, pure_out_value(_2))
                         .def("on_write", &append::on_write<raw_array<unsigned int> const&>, pure_out_value(_2))
                         .def("on_write", &append::on_write<vector<boost::array<float, 3> > >, pure_out_value(_2))

halmd/io/writers/h5md/truncate.cpp
@@ -206,6 +206,10 @@ void truncate::luaopen(lua_State* L)
                         .def("on_write", &truncate::on_write<raw_array<fixed_vector<double, 2>> const&>, pure_out_value(_2))
                         .def("on_write", &truncate::on_write<raw_array<fixed_vector<double, 3>>&>, pure_out_value(_2))
                         .def("on_write", &truncate::on_write<raw_array<fixed_vector<double, 3>> const&>, pure_out_value(_2))
+                        .def("on_write", &truncate::on_write<raw_array<float>&>, pure_out_value(_2))
+                        .def("on_write", &truncate::on_write<raw_array<float> const&>, pure_out_value(_2))
+                        .def("on_write", &truncate::on_write<raw_array<double>&>, pure_out_value(_2))
+                        .def("on_write", &truncate::on_write<raw_array<double> const&>, pure_out_value(_2))
                         .def("on_write", &truncate::on_write<raw_array<unsigned int>&>, pure_out_value(_2))
                         .def("on_write", &truncate::on_write<raw_array<unsigned int> const&>, pure_out_value(_2))
                         .def("on_write", &truncate::on_write<vector<boost::array<float, 3> > >, pure_out_value(_2))

halmd/observables/gpu/phase_space.cpp
@@ -192,10 +192,12 @@ phase_space<host::samples::phase_space<dimension, float_type> >::acquire()
     assert(particle_group_->size() == sample_->position().size());
     assert(particle_group_->size() == sample_->velocity().size());
     assert(particle_group_->size() == sample_->species().size());
+    assert(particle_group_->size() == sample_->mass().size());
 
     typename sample_type::position_array_type& position = sample_->position();
     typename sample_type::velocity_array_type& velocity = sample_->velocity();
     typename sample_type::species_array_type& species = sample_->species();
+    typename sample_type::mass_array_type& mass = sample_->mass();
 
     // copy particle data using reverse tags as on the GPU
     cuda::configure((particle_group_->size() + threads_ - 1) / threads_, threads_);
@@ -217,7 +219,7 @@ phase_space<host::samples::phase_space<dimension, float_type> >::acquire()
         box_->extend_periodic(r, static_cast<vector_type>(h_image_[i]));
 
         position[tag] = r;
-        velocity[tag] = static_cast<vector_type>(h_v_[i]);
+        tie(velocity[tag], mass[tag]) <<= h_v_[i];
         species[tag] = type;
         ++tag;
     }
@@ -234,6 +236,7 @@ void phase_space<host::samples::phase_space<dimension, float_type> >::set(boost:
     typename sample_type::position_array_type const& sample_position = sample->position();
     typename sample_type::velocity_array_type const& sample_velocity = sample->velocity();
     typename sample_type::species_array_type const& sample_species = sample->species();
+    typename sample_type::mass_array_type const& sample_mass = sample->mass();
 
     assert(sample_position.size() >= particle_group_->size());
     assert(sample_velocity.size() >= particle_group_->size());
@@ -252,7 +255,7 @@ void phase_space<host::samples::phase_space<dimension, float_type> >::set(boost:
     for (std::size_t i : h_group_) {
         assert(i < h_r_.size());
         h_r_[i] <<= tie(sample_position[tag], sample_species[tag]);
-        h_v_[i] = sample_velocity[tag];
+        h_v_[i] <<= tie(sample_velocity[tag], sample_mass[tag]);
         ++tag;
     }
 
@@ -347,6 +350,20 @@ wrap_species(boost::shared_ptr<phase_space_type> phase_space)
     return boost::bind(&species<phase_space_type>, phase_space, boost::shared_ptr<typename phase_space_type::sample_type::species_array_type>());
 }
 
+template <typename phase_space_type>
+static typename phase_space_type::sample_type::mass_array_type const&
+mass(boost::shared_ptr<phase_space_type> const& phase_space)
+{
+    return phase_space->acquire()->mass();
+}
+
+template <typename phase_space_type>
+static boost::function<typename phase_space_type::sample_type::mass_array_type const& ()>
+wrap_mass(boost::shared_ptr<phase_space_type> phase_space)
+{
+    return boost::bind(&mass<phase_space_type>, phase_space);
+}
+
 template <int dimension, typename float_type>
 void phase_space<host::samples::phase_space<dimension, float_type> >::luaopen(lua_State* L)
 {
@@ -360,6 +377,7 @@ void phase_space<host::samples::phase_space<dimension, float_type> >::luaopen(lu
                 .property("position", &wrap_position<phase_space>)
                 .property("velocity", &wrap_velocity<phase_space>)
                 .property("species", &wrap_species<phase_space>)
+                .property("mass", &wrap_mass<phase_space>)
                 .property("dimension", &wrap_dimension<phase_space>)
                 .def("set", &phase_space::set)
                 .scope

halmd/observables/host/phase_space.cpp
@@ -73,10 +73,12 @@ phase_space<dimension, float_type>::acquire()
     typename particle_type::image_array_type& particle_image = particle_->image();
     typename particle_type::velocity_array_type& particle_velocity = particle_->velocity();
     typename particle_type::species_array_type& particle_species = particle_->species();
+    typename particle_type::mass_array_type& particle_mass = particle_->mass();
 
     typename sample_type::position_array_type& sample_position = sample_->position();
     typename sample_type::velocity_array_type& sample_velocity = sample_->velocity();
     typename sample_type::species_array_type& sample_species = sample_->species();
+    typename sample_type::mass_array_type& sample_mass = sample_->mass();
 
     // copy particle data using index map
     std::size_t tag = 0;
@@ -90,6 +92,7 @@ phase_space<dimension, float_type>::acquire()
 
         sample_velocity[tag] = particle_velocity[i];
         sample_species[tag] = particle_species[i];
+        sample_mass[tag] = particle_mass[i];
         ++tag;
     }
 
@@ -105,10 +108,12 @@ void phase_space<dimension, float_type>::set(boost::shared_ptr<sample_type const
     typename particle_type::image_array_type& particle_image = particle_->image();
     typename particle_type::velocity_array_type& particle_velocity = particle_->velocity();
     typename particle_type::species_array_type& particle_species = particle_->species();
+    typename particle_type::mass_array_type& particle_mass = particle_->mass();
 
     typename sample_type::position_array_type const& sample_position = sample->position();
     typename sample_type::velocity_array_type const& sample_velocity = sample->velocity();
     typename sample_type::species_array_type const& sample_species = sample->species();
+    typename sample_type::mass_array_type const& sample_mass = sample->mass();
 
     std::size_t tag = 0;
     for (std::size_t i : *particle_group_) {
@@ -130,6 +135,7 @@ void phase_space<dimension, float_type>::set(boost::shared_ptr<sample_type const
 
         particle_velocity[i] = sample_velocity[tag];
         particle_species[i] = sample_species[tag];
+        particle_mass[i] = sample_mass[tag];
         ++tag;
     }
 }
@@ -200,6 +206,20 @@ wrap_species(boost::shared_ptr<phase_space_type> phase_space)
 }
 
 template <typename phase_space_type>
+static typename phase_space_type::sample_type::mass_array_type const&
+mass(boost::shared_ptr<phase_space_type> const& phase_space)
+{
+    return phase_space->acquire()->mass();
+}
+
+template <typename phase_space_type>
+static boost::function<typename phase_space_type::sample_type::mass_array_type const& ()>
+wrap_mass(boost::shared_ptr<phase_space_type> phase_space)
+{
+    return boost::bind(&mass<phase_space_type>, phase_space);
+}
+
+template <typename phase_space_type>
 static int wrap_dimension(phase_space_type const&)
 {
     return phase_space_type::particle_type::vector_type::static_size;
@@ -220,6 +240,7 @@ void phase_space<dimension, float_type>::luaopen(lua_State* L)
                     .property("position", &wrap_position<phase_space>)
                     .property("velocity", &wrap_velocity<phase_space>)
                     .property("species", &wrap_species<phase_space>)
+                    .property("mass", &wrap_mass<phase_space>)
                     .property("dimension", &wrap_dimension<phase_space>)
                     .def("set", &phase_space::set)
                     .scope

halmd/observables/host/samples/phase_space.cpp
@@ -115,6 +115,28 @@ wrap_species(boost::shared_ptr<phase_space_type> self, boost::function<void ()>&
     };
 }
 
+template <typename phase_space_type>
+static boost::function<std::vector<typename phase_space_type::mass_array_type::value_type>& ()>
+wrap_mass(boost::shared_ptr<phase_space_type> self, boost::function<void ()>& array_to_sample)
+{
+    typedef std::vector<typename phase_space_type::mass_array_type::value_type> array_type;
+    boost::shared_ptr<array_type> array = boost::make_shared<array_type>();
+    array_to_sample = [=]() {
+        if (self->mass().size() != array->size()) {
+            throw std::runtime_error("phase space sample has mismatching size");
+        }
+        std::copy(
+            array->begin()
+          , array->end()
+          , self->mass().begin()
+        );
+        array->clear();
+    };
+    return [=]() -> array_type& {
+        return *array;
+    };
+}
+
 template <int dimension, typename float_type>
 void phase_space<dimension, float_type>::luaopen(lua_State* L)
 {
@@ -135,6 +157,7 @@ void phase_space<dimension, float_type>::luaopen(lua_State* L)
                         .def("position", &wrap_position<phase_space>, pure_out_value(_2))
                         .def("velocity", &wrap_velocity<phase_space>, pure_out_value(_2))
                         .def("species", &wrap_species<phase_space>, pure_out_value(_2))
+                        .def("mass", &wrap_mass<phase_space>, pure_out_value(_2))
                 ]
             ]
         ]

halmd/observables/host/samples/phase_space.hpp
@@ -42,6 +42,7 @@ public:
     typedef raw_array<vector_type> position_array_type;
     typedef raw_array<vector_type> velocity_array_type;
     typedef raw_array<unsigned int> species_array_type;
+    typedef raw_array<float_type> mass_array_type;
     typedef typename mdsim::clock::step_type step_type;
 
     /**
@@ -105,6 +106,22 @@ public:
     }
 
     /**
+     * Returns const reference to particle mass.
+     */
+    mass_array_type const& mass() const
+    {
+        return mass_;
+    }
+
+    /**
+     * Returns non-const reference to particle mass.
+     */
+    mass_array_type& mass()
+    {
+        return mass_;
+    }
+
+    /**
      * Returns simulation step when the sample was taken.
      */
     step_type step() const
@@ -127,6 +144,8 @@ private:
     velocity_array_type velocity_;
     /** particle species */
     species_array_type species_;
+    /** particle mass */
+    mass_array_type mass_;
     /** simulation step when sample was taken */
     step_type step_;
 };
@@ -136,6 +155,7 @@ inline phase_space<dimension, float_type>::phase_space(std::size_t nparticle, st
   : position_(nparticle)
   , velocity_(nparticle)
   , species_(nparticle)
+  , mass_(nparticle)
   , step_(step)
 {
 }

lua/halmd/observables/phase_space.lua.in
@@ -83,9 +83,15 @@ end
 --
 --    :returns: data slot that returns species array in host memory
 --
+-- .. method:: mass()
+--
+--    Returns data slot that acquires phase space sample and returns mass array.
+--
+--    :returns: data slot that returns mass array in host memory
+--
 -- .. method:: set(sample)
 --
---    Set positions, velocities, species from phase space sample.
+--    Set positions, velocities, species and masses from phase space sample.
 --
 -- .. method:: connect()
 --
@@ -171,6 +177,10 @@ local M = module(function(args)
         return assert(samplers.host.species)
     end
 
+    self.mass = function(self)
+        return assert(samplers.host.mass)
+    end
+
     self.writer = function(self, file, args)
         local name = args.group or assert(self.group.label)
         local writer = file:writer({location = {"trajectory", name}, mode = "append"})
@@ -178,6 +188,7 @@ local M = module(function(args)
         writer:on_write(self.position(), {"position"})
         writer:on_write(self.velocity(), {"velocity"})
         writer:on_write(self.species(), {"species"})
+        writer:on_write(self.mass(), {"mass"})
 
         local every = args.every
 
@@ -331,7 +342,7 @@ function M.reader(file, args)
         error(("unsupported positional dimension: %d"):format(dimension), 2)
     end
     local sample = phase_space(nparticle)
-    local names = {"position", "velocity", "species"}
+    local names = {"position", "velocity", "species", "mass"}
     for i = 1, #names do
         local name = names[i]
         local array, array_to_sample = sample[name](sample)