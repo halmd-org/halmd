32db7e4bd441493acd0c0abab6f187d24ff39e15
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-06-02 16:43:51 -0400

    examples/liquid: rename scripts to lennard_jones
    This name is more specific, and consistent with the integration tests.
    
    Move equilibrate from suffix to prefix for faster filename completion.

--- examples/liquid/equilibrate_lennard_jones.lua
+++ examples/liquid/equilibrate_lennard_jones.lua
@@ -149,7 +149,7 @@ local function parse_args()
     parser.add_argument("output,o", {type = "string", action = function(args, key, value)
         -- substitute current time
         args[key] = os.date(value)
-    end, default = "simple_liquid_equilibrate_%Y%m%d_%H%M%S", help = "prefix of output files"})
+    end, default = "equilibrate_lennard_jones_%Y%m%d_%H%M%S", help = "prefix of output files"})
 
     parser.add_argument("verbose,v", {type = "accumulate", action = function(args, key, value)
         local level = {
