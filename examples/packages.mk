e40db0fe63cbd98093acfd476d1a770e475f3597
Peter Colberg <peter@colberg.org>
2012-08-08 19:47:41 -0400

    packages.mk: update HALMD to version 0.2-rc4

--- examples/packages.mk
+++ examples/packages.mk
@@ -1184,10 +1184,10 @@ env-gcc:
 ## HALMD Highly Accerelated Large-Scale Molecular Dynamics
 ##
 
-HALMD_VERSION = 0.2-rc3
+HALMD_VERSION = 0.2-rc4
 HALMD_TARBALL = halmd-$(HALMD_VERSION).tar.bz2
 HALMD_TARBALL_URL = http://downloads.sourceforge.net/project/halmd/halmd/0.2/testing/$(HALMD_TARBALL)
-HALMD_TARBALL_SHA256 = ffc7a3b99efe8dc2dee9c24589c9c3dec52043395fd02f93dcb4ea326b5c0704
+HALMD_TARBALL_SHA256 = 20e4405c15e4848f76a4afd4b8d94c7aada1eddc05811ec74e6c59d315f77189
 HALMD_SOURCE_DIR = halmd-$(HALMD_VERSION)
 HALMD_BUILD_DIR = $(HALMD_SOURCE_DIR)/build/release
 HALMD_INSTALL_DIR = $(PREFIX)/halmd-$(HALMD_VERSION)
