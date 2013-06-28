halmd-org/halmd

commit 1d73ef8ba86e9d00c290e80b01724d59a2fd0276

observables: revise wavevector module
Change internal data structure: store wavevectors as plain array
(std::vector) and add a list of wavevector shells of approximately equal
magnitude in the form of index ranges into the former array. Thereby,
iteration over the wavevector shells is simplified, e.g., in the ssf
module.

Implementation detail: use std::multimap to group the wavevectors by
their magnitude, but copy to std::vector afterwards.

Drop attribute "dimension".

halmd/observables/utility/wavevector.cpp
@@ -17,10 +17,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <boost/bind.hpp>
 #include <cmath>
 #include <exception>
 #include <iterator>
+#include <map>
 #include <sstream>
 
 #include <halmd/algorithm/host/pick_lattice_points.hpp>
@@ -61,79 +61,92 @@ wavevector<dimension>::wavevector(
     LOG("tolerance on magnitude: " << tolerance_);
     LOG("maximum shell size: " << max_count_);
 
-    // construct wavevectors and store as key/value pairs (wavenumber, wavevector)
+    // construct wavevectors and store as key/value pairs (wavenumber, wavevector),
+    // wavenumbers are equivalent if they differ by less than tolerance_
+    auto less_tol = [=] (double x, double y) { return x * (1 + tolerance_) < y; }; // comparison functor
+    multimap<double, vector_type, decltype(less_tol)> wavevector_map(less_tol);
     algorithm::host::pick_lattice_points_from_shell(
         first, last
-      , back_inserter(wavevector_)
+      , inserter(wavevector_map, end(wavevector_map))
       , element_div(vector_type(2 * M_PI), box_length_)
       , tolerance_
       , max_count_
     );
 
-    // sort wavevector map according to keys (wavenumber)
-    stable_sort(
-        begin(wavevector_), end(wavevector_)
-      , bind(less<double>(), bind(&map_type::value_type::first, _1), bind(&map_type::value_type::first, _2))
-    );
+    if (wavevector_map.empty()) {
+        LOG_WARNING("no matching wavevectors");
+        throw logic_error("Constraints on wavevectors are incompatible with geometry of simulation box.");
+    }
 
-    // remove wavenumbers with no compatible wavevectors
-    for (vector<double>::iterator q_it = begin(wavenumber_); q_it != end(wavenumber_); ++q_it) {
-        // find wavevector q with |q| = *q_it
-        auto found = find_if(
-            begin(wavevector_), end(wavevector_)
-          , bind(equal_to<double>(), bind(&map_type::value_type::first, _1), *q_it)
-        );
-        if (found == end(wavevector_)) {
+    for (auto q_it = begin(wavenumber_); q_it != end(wavenumber_); ++q_it) {
+        // find wavevector range with |q| = *q_it
+        auto q_range = wavevector_map.equal_range(*q_it);
+
+        if (q_range.first != q_range.second) {
+            // append wavevectors to wavevector list and store shell
+            typename shell_array_type::value_type idx;
+            idx.first = wavevector_.size();
+            for (auto it = q_range.first; it != q_range.second; ++it) {
+                assert(abs(it->first - *q_it) < *q_it * tolerance_);
+                assert(abs(norm_2(it->second) - *q_it) < 2 * *q_it * tolerance_);
+                wavevector_.push_back(it->second);
+            }
+            idx.second = wavevector_.size();
+            shell_.push_back(idx);
+        }
+        else {
+            // remove wavenumbers with empty wavevector shells
             LOG_WARNING("reciprocal lattice not compatible with |q| ≈ " << *q_it << ", value discarded");
             wavenumber_.erase(q_it--);   // post-decrement iterator, increment at end of loop
         }
     }
 
-    if (wavenumber_.empty()) {
-        LOG_WARNING("empty wavenumber grid");
-        throw std::logic_error("Constraints on wavevectors are incompatible with geometry of simulation box.");
-    }
-
-    LOG_DEBUG("total number of wavevectors: " << wavevector_.size());
+    LOG_DEBUG("total number of wavevectors found: " << wavevector_.size());
 }
 
-template <typename wavevector_type, typename wavenumber_array_type>
-static std::function<wavenumber_array_type const& ()>
-wrap_wavenumber(std::shared_ptr<wavevector_type const> self)
+template <typename wavevector_type>
+static function<typename wavevector_type::wavevector_array_type const& ()>
+wrap_value(shared_ptr<wavevector_type const> self)
 {
-    return [=]() -> wavenumber_array_type const& {
-        return self->wavenumber();
+    return [=]() -> typename wavevector_type::wavevector_array_type const& {
+        return self->value();
     };
 }
 
-template <int dimension>
-static unsigned int wrap_dimension(wavevector<dimension> const&)
+template <typename wavevector_type>
+static function<typename wavevector_type::wavenumber_array_type const& ()>
+wrap_wavenumber(shared_ptr<wavevector_type const> self)
 {
-    return dimension;
+    return [=]() -> typename wavevector_type::wavenumber_array_type const& {
+        return self->wavenumber();
+    };
 }
 
 template <int dimension>
 void wavevector<dimension>::luaopen(lua_State* L)
 {
     using namespace luaponte;
-    static string class_name("wavevector_" + boost::lexical_cast<string>(dimension) + "_");
     module(L, "libhalmd")
     [
         namespace_("observables")
         [
             namespace_("utility")
             [
-                class_<wavevector, std::shared_ptr<wavevector> >(class_name.c_str())
+                class_<wavevector>()
                     .def(constructor<
                          vector<double> const&
                        , vector_type const&
                        , double, unsigned int
                     >())
-                    .property("wavenumber", &wrap_wavenumber<wavevector, wavenumber_array_type>)
-                    .property("value", &wavevector::value)
-                    .property("tolerance", &wavevector::tolerance)
-                    .property("max_count", &wavevector::max_count)
-                    .property("dimension", &wrap_dimension<dimension>)
+                    .property("wavenumber", &wrap_wavenumber<wavevector>)
+                    .property("value", &wrap_value<wavevector>)
+
+               , def("wavevector", &make_shared<wavevector
+                  , vector<double> const&
+                  , vector_type const&
+                  , double
+                  , unsigned int
+                 >)
             ]
         ]
     ];

halmd/observables/gpu/density_mode.cpp
@@ -54,14 +54,14 @@ density_mode<dimension, float_type>::density_mode(
     );
     // copy wavevectors to CUDA device
     try {
-        cuda::host::vector<gpu_vector_type> q(nq_);
-        for (unsigned int i = 0; i < nq_; ++i) {
-            // select wavevector from pair (wavenumber, wavevector),
-            // cast from fixed_vector<double, ...> to fixed_vector<float, ...>
-            // and finally to gpu_vector_type (float4 or float2)
-            q[i] = static_cast<vector_type>(wavevector_->value()[i].second);
+        // cast from fixed_vector<double, ...> to fixed_vector<float, ...>
+        // and finally to gpu_vector_type (float4 or float2)
+        auto const& q = wavevector_->value();
+        cuda::host::vector<gpu_vector_type> h_q(nq_);
+        for (unsigned int i = 0; i < q.size(); ++i) {
+            h_q[i] = static_cast<vector_type>(q[i]);
         }
-        cuda::copy(q, g_q_);
+        cuda::copy(h_q, g_q_);
     }
     catch (cuda::error const&) {
         LOG_ERROR("failed to copy wavevectors to device");

halmd/observables/host/density_mode.cpp
@@ -53,9 +53,11 @@ density_mode<dimension, float_type>::acquire()
 
     LOG_TRACE("acquire sample");
 
+    auto const& wavevector = wavevector_->value(); // array of wavevectors
+
     // re-allocate memory which allows modules (e.g., dynamics::blocking_scheme)
     // to hold a previous copy of the sample
-    rho_sample_ = make_shared<sample_type>(wavevector_->value().size());
+    rho_sample_ = make_shared<sample_type>(wavevector.size());
 
     // compute density modes
     mode_array_type& rho_vector = rho_sample_->rho();
@@ -71,8 +73,8 @@ density_mode<dimension, float_type>::acquire()
         vector_type const& r = position[i];
         // 2nd loop: iterate over wavevectors
         auto rho_q = begin(rho_vector);
-        for (auto const& q_pair : wavevector_->value()) {
-            float_type q_r = inner_prod(static_cast<vector_type>(q_pair.second), r);
+        for (auto const& q : wavevector) {
+            float_type q_r = inner_prod(q, r);
             *rho_q++ += mode_type(cos(q_r), -sin(q_r));
         }
     }

halmd/observables/ssf.cpp
@@ -62,32 +62,30 @@ ssf<dimension>::sample(density_mode_type const& mode1, density_mode_type const& 
 
     LOG_TRACE("sampling");
 
-    typename rho_vector_type::const_iterator rho_q1 = mode1.rho().begin();
-    typename rho_vector_type::const_iterator rho_q2 = mode2.rho().begin();
+    typename rho_vector_type::const_iterator rho1_begin = mode1.rho().begin();
+    typename rho_vector_type::const_iterator rho2_begin = mode2.rho().begin();
     typename result_type::iterator result = result_.begin();
 
-    typename wavevector_type::map_type const& wavevector = wavevector_->value();
-    typename wavevector_type::map_type::const_iterator q = wavevector.begin();
-    typename wavevector_type::map_type::const_iterator q_next = q; ++q_next;
-
     // accumulate products of density modes with equal wavenumber,
-    // iterate over sorted list of wavevectors
-    accumulator<double> acc;
-    while (q != wavevector.end()) {
-        // compute Re[rho_q rho_q^*], add result to output accumulator
-        double re = (real(*rho_q1) * real(*rho_q2) + imag(*rho_q1) * imag(*rho_q2));
-        acc(re / norm_);
-        // find end of range with equal wavenumber
-        if (q_next == wavevector.end() || q->first != q_next->first) {
-            // transform accumulator to array (mean, error_of_mean, count)
-            (*result)[0] = mean(acc);
-            (*result)[1] = count(acc) > 1 ? error_of_mean(acc) : 0;
-            (*result)[2] = count(acc);
-            acc.reset();
-            // next wavenumber: increment output iterator
-            ++result;
+    // iterate over wavevector shells encoded as index ranges to the wavevector array
+    for (auto idx_range : wavevector_->shell()) {
+        accumulator<double> acc;
+        auto rho_q1 = rho1_begin + idx_range.first;
+        auto rho_q2 = rho2_begin + idx_range.first;
+        // iterate over wavevectors and density modes simultaneously
+        for (size_t i = idx_range.first; i != idx_range.second; ++i, ++rho_q1, ++rho_q2) {
+            // compute Re[rho_q rho_q^*]
+            double re = (real(*rho_q1) * real(*rho_q2) + imag(*rho_q1) * imag(*rho_q2));
+            // accumulate results for this wavenumber
+            acc(re / norm_);
         }
-        ++q; ++q_next; ++rho_q1; ++rho_q2;
+        // transform accumulator to array (mean, error_of_mean, count)
+        // and write to output iterator
+        *result++ = {{
+            mean(acc)
+          , count(acc) > 1 ? error_of_mean(acc) : 0
+          , static_cast<double>(count(acc))
+        }};
     }
 
     // store simulation step as time stamp

halmd/observables/utility/wavevector.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011-2013  Felix Höfling
+ * Copyright © 2011-2013 Felix Höfling
  *
  * This file is part of HALMD.
  *
@@ -34,9 +34,6 @@ namespace utility {
 /**
  * construct set of wavevector shells compatible with the reciprocal
  * lattice of the periodic simulation box and a list of wavenumbers
- *
- * The result in value() is a sorted container of key/value pairs
- * (wavenumber, wavevector).
  */
 
 template <int dimension>
@@ -44,8 +41,9 @@ class wavevector
 {
 public:
     typedef fixed_vector<double, dimension> vector_type;
-    typedef std::vector<std::pair<double, vector_type> > map_type;
     typedef std::vector<double> wavenumber_array_type;
+    typedef std::vector<vector_type> wavevector_array_type;
+    typedef std::vector<std::pair<std::size_t, std::size_t>> shell_array_type;
 
     static void luaopen(lua_State* L);
 
@@ -70,7 +68,7 @@ public:
     }
 
     //! returns list of wavevectors
-    map_type const& value() const
+    wavevector_array_type const& value() const
     {
         return wavevector_;
     }
@@ -79,6 +77,17 @@ public:
     wavenumber_array_type const& wavenumber() const
     {
         return wavenumber_;
+    }
+
+    /*
+     * returns list of wavevector shells
+     *
+     * The values are index ranges on the wavevector array, the order of shells
+     * coincides with the wavenumber grid.
+     */
+    shell_array_type const& shell() const
+    {
+        return shell_;
     }
 
 protected:
@@ -90,11 +99,10 @@ protected:
     double tolerance_;
     /** maximum number of wavevectors per wavenumber */
     double max_count_;
-    /**
-     * list of wavevectors grouped by their magnitude,
-     * the keys equal wavenumber_ (or a subset of)
-     */
-    map_type wavevector_;
+    // list of wavevectors grouped by their magnitude in ascending order
+    wavevector_array_type wavevector_;
+    // list of wavevector shells
+    shell_array_type shell_;
     /** module logger */
     std::shared_ptr<logger> logger_;
 };

lua/halmd/observables/utility/wavevector.lua.in
@@ -20,10 +20,8 @@
 local module = require("halmd.utility.module")
 
 -- grab C++ wrappers
-local wavevector_wrapper = {
-    [2] = libhalmd.observables.utility.wavevector_2_
-  , [3] = libhalmd.observables.utility.wavevector_3_
-}
+local wavevector = assert(libhalmd.observables.utility.wavevector)
+
 -- grab standard library
 local assert = assert
 
@@ -70,20 +68,28 @@ local assert = assert
 -- :param number args.tolerance:  relative tolerance on wavevector magnitude
 -- :param integer args.max_count: maximum number of wavevectors per wavenumber shell
 --
+-- .. method:: wavenumber()
+--
+--    Returns data slot that yiels the wavenumber grid.
+--
+-- .. method:: value()
+--
+--    Returns data slot that yiels the list of wavevectors grouped by their
+--    magnitude in ascending order.
+--
 local M = module(function(args)
     local box = assert(args.box)
     local wavenumber = assert(args.wavenumber)
     local tolerance = assert(args.tolerance)
     local max_count = assert(args.max_count)
 
-    local wavevector = assert(wavevector_wrapper[box.dimension])
     return wavevector(wavenumber, box.length, tolerance, max_count)
 end)
 
 ---
 -- .. function:: add_options(parser)
 --
---    Add module options to command line parser.
+--    Add module options to command line parser: ``wavenumbers``, ``tolerance``, ``max-count``.
 --
 --    :param parser: instance of :class:`halmd.utility.program_options.argument_parser`
 --

test/unit/observables/ssf.cpp
@@ -167,19 +167,14 @@ void lattice<modules_type>::test()
     BOOST_CHECK(result.size() == wavenumber.size());
 
     // compare with expected result
-    typedef typename wavevector_type::map_type::const_iterator iterator_type;
-    iterator_type q_begin = wavevector->value().begin();
-    iterator_type q_end = q_begin;
-    for (unsigned i = 0; i < result.size(); ++i) {
-        // find range with wavevectors of magnitude q
-        double q = wavevector->wavenumber()[i];
-        q_begin = q_end;
-        for ( ; q_end != wavevector->value().end(); ++q_end) {
-            if (q_end->first > q)
-                break;
-        }
-        unsigned nq = q_end - q_begin;
+    auto shell = wavevector->shell().begin();
+    for (unsigned i = 0; i < result.size(); ++i, ++shell) {
+        // range with wavevectors of magnitude q
+        unsigned nq = shell->second - shell->first;
         BOOST_CHECK(nq > 0);
+
+        auto q_begin = wavevector->value().begin() + shell->first;
+        auto q_end = q_begin + nq;
 
         // determine (h,k,l) of each wavevector and its contribution to the
         // structure factor
@@ -195,12 +190,12 @@ void lattice<modules_type>::test()
         // and zero otherwise. Thus, S_q = ncell * nunit_cell = npart
         // for matching wavevectors.
         double S_q = 0;
-        for (iterator_type q_it = q_begin; q_it != q_end; ++q_it) {
+        for (auto q_it = q_begin; q_it != q_end; ++q_it) {
             // compute elementwise product (h,k,l) × ncell
             // since fractions cannot be represented by double
             typedef fixed_vector<unsigned, dimension> index_type; // caveat: we assume q[i] ≥ 0
             index_type hkl_ncell = static_cast<index_type>(
-                round(element_prod(q_it->second, box->length() / (2 * M_PI)))
+                round(element_prod(*q_it, box->length() / (2 * M_PI)))
             );
             // check whether all entries are multiples of ncell
             // and if yes, count number of odd multiples

commit 92b2baaf213ca55444c79557c96bfc34e9e057ec

observables/…/wavevector: fix bug with multimap keys
The comparison predicate less_tol() used to sort wavevectors by their
magnitude did not induce a strict weak ordering. The tolerance was
simply chosen too large since the stored keys are taken from the
wavenumber grid provided (and not from the magnitude of the wavevectors
found). The issue was introduced in commit 1d73ef8b.

The issue has been detected by a failing assertion in a test run:
http://my.cdash.org/testDetails.php?test=11507122&build=493412

halmd/observables/utility/wavevector.cpp
@@ -61,9 +61,23 @@ wavevector<dimension>::wavevector(
     LOG("tolerance on magnitude: " << tolerance_);
     LOG("maximum shell size: " << max_count_);
 
-    // construct wavevectors and store as key/value pairs (wavenumber, wavevector),
-    // wavenumbers are equivalent if they differ by less than tolerance_
-    auto less_tol = [=] (double x, double y) { return x * (1 + tolerance_) < y; }; // comparison functor
+    // construct wavevectors and store as key/value pairs (wavenumber, wavevector).
+    //
+    // The use of floating-point numbers as keys could result in rounding
+    // issues (which is unlikely since they are not subject to arithmetic
+    // operations), hence we consider keys equivalent if they differ by less
+    // than the floating point precision.
+    // x ~ y <=> !less_tol(x, y) && !less_tol(y, x) <=> |x – y| ≤ ε max(|x|, |y|)
+    // assuming x, y ≥ 0:
+    // !(x ~ y) <=> y – x > ε y or x – y > ε x <=> x < y(1 – ε) or y < x(1 – ε)
+    auto less_tol = [=](double x, double y) { // comparison functor
+        return x < y * (1 - numeric_limits<double>::epsilon());
+    };
+    // Note that the comparison predicate passed to std::multimap must define a
+    // strict weak ordering, i.e. the equivalence relation must be transitive:
+    // a ~ b, b ~ c ⇒ a ~ c. This is only the case for less_tol() if the
+    // supplied keys are separated by more than ε.
+    // (Counter example for ε = 0.1: 1 ~ 1.1, 1.1 ~ 1.2, but not 1 ~ 1.2).
     multimap<double, vector_type, decltype(less_tol)> wavevector_map(less_tol);
     algorithm::host::pick_lattice_points_from_shell(
         first, last