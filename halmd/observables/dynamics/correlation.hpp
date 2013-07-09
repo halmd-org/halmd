058d16599fadcd386f69d2169df19ce534ce9ed6
Felix Höfling <hoefling@mf.mpg.de>
2013-07-09 17:08:57 +0200

    correlation: revise module for future support of tensor TCFs
    A tensor-valued TCF depends on extra parameters, e.g., the intermediate
    scattering function S(k, t) depends on the wavenumber k and is thus
    considered a rank-1 tensor. Such TCF functors will define:
    
        enum { result_rank = 1 };
        unsigned int const* result_shape() const;
    
    The challenge was to extend the dynamics::correlator interface in such a
    way (using SFINAE) that simple TCFs do not need these additional
    definitions.

--- halmd/observables/dynamics/correlation.hpp
+++ halmd/observables/dynamics/correlation.hpp
@@ -21,16 +21,15 @@
 #ifndef HALMD_OBSERVABLES_DYNAMICS_CORRELATION_HPP
 #define HALMD_OBSERVABLES_DYNAMICS_CORRELATION_HPP
 
+#include <boost/array.hpp>
+#include <boost/multi_array.hpp>
+#include <memory>
+
 #include <halmd/io/logger.hpp>
 #include <halmd/numeric/accumulator.hpp>
 #include <halmd/observables/samples/blocking_scheme.hpp>
 #include <halmd/utility/lua/lua.hpp>
 #include <halmd/utility/profiler.hpp>
-
-#include <boost/multi_array.hpp>
-
-#include <functional>
-#include <memory>
 
 namespace halmd {
 namespace observables {
@@ -53,19 +52,66 @@ public:
     virtual void compute(unsigned int level) = 0;
 };
 
+namespace detail {
+
+/**
+ * determine result_rank of generic TCF functor or provide a default
+ */
+template <typename T>
+constexpr auto get_rank_impl(int) -> decltype(T::result_rank)
+{
+    return T::result_rank;
+}
+
+template <typename T>
+constexpr int get_rank_impl(long)
+{
+    return 0;
+}
+
+template <typename T>
+constexpr int get_rank()
+{
+    return get_rank_impl<T>(0); // 0 is of type 'int' which takes precedence over 'long'
+}
+
+/**
+ * call result_shape() of generic TCF functor or provide a default
+ */
+template <typename T>
+auto get_shape_impl(T const& obj, int) -> decltype(obj.result_shape())
+{
+    return obj.result_shape();
+}
+
+template <typename T>
+unsigned int const* get_shape_impl(T const& obj, long)
+{
+    return nullptr;
+}
+
+template <typename T>
+unsigned int const* get_shape(T const& obj)
+{
+    return get_shape_impl(obj, 0); // 0 is of type 'int' which takes precedence over 'long'
+}
+
+} // namespace detail
+
 template <typename tcf_type>
 class correlation
   : public correlation_base
 {
+    enum { rank_ = 2 + detail::get_rank<tcf_type>() };
+
 public:
     typedef typename tcf_type::sample_type sample_type;
-    typedef typename tcf_type::result_type result_type;
     typedef typename tcf_type::accumulator_type accumulator_type;
+
     typedef observables::samples::blocking_scheme<sample_type> block_sample_type;
-    typedef boost::multi_array<accumulator_type, 2> block_result_type;
-    typedef boost::multi_array<typename accumulator_type::value_type, 2> block_mean_type;
-    typedef boost::multi_array<typename accumulator_type::value_type, 2> block_error_type;
-    typedef boost::multi_array<typename accumulator_type::size_type, 2> block_count_type;
+    typedef boost::multi_array<accumulator_type, rank_> block_result_type;
+    typedef boost::multi_array<typename accumulator_type::value_type, rank_> block_mean_type;
+    typedef boost::multi_array<typename accumulator_type::size_type, rank_> block_count_type;
     typedef logger logger_type;
 
     static void luaopen(lua_State* L);
@@ -73,23 +119,32 @@ public:
     correlation(
         std::shared_ptr<tcf_type> tcf
       , std::shared_ptr<block_sample_type> block_sample
-      , std::shared_ptr<logger_type> logger = std::make_shared<logger_type>()
+      , std::shared_ptr<logger_type> logger = std::make_shared<logger_type>("dynamics.correlation")
     );
 
     virtual void compute(unsigned int level);
-    block_mean_type const& get_mean();
-    block_error_type const& get_error();
-    block_count_type const& get_count();
+
+    block_result_type const& result() const
+    {
+        return result_;
+    }
+
+    static std::function<block_mean_type const& ()>
+    get_mean(std::shared_ptr<correlation> self);
+
+    static std::function<block_mean_type const& ()>
+    get_error(std::shared_ptr<correlation> self);
+
+    static std::function<block_count_type const& ()>
+    get_count(std::shared_ptr<correlation> self);
 
 private:
     typedef correlation_base _Base;
-    typedef utility::profiler profiler_type;
-    typedef profiler_type::scoped_timer_type scoped_timer_type;
+    typedef utility::profiler::scoped_timer_type scoped_timer_type;
 
     struct runtime
     {
-        typedef utility::profiler::accumulator_type accumulator_type;
-        accumulator_type tcf;
+        utility::profiler::accumulator_type tcf;
     };
 
     /** block structure holding the input data */
@@ -98,12 +153,13 @@ private:
     std::shared_ptr<tcf_type> tcf_;
     /** module logger */
     std::shared_ptr<logger_type> logger_;
+
     /** block structures holding accumulated result values */
     block_result_type result_;
     /** mean values */
     block_mean_type mean_;
     /** standard error of mean */
-    block_error_type error_;
+    block_mean_type error_;
     /** accumulator count */
     block_count_type count_;
 
@@ -121,12 +177,20 @@ correlation<tcf_type>::correlation(
   : block_sample_(block_sample)
   , tcf_(tcf)
   , logger_(logger)
-  // memory allocation
-  , result_(boost::extents[block_sample->count()][block_sample->block_size()])
-  , mean_(boost::extents[block_sample->count()][block_sample->block_size()])
-  , error_(boost::extents[block_sample->count()][block_sample->block_size()])
-  , count_(boost::extents[block_sample->count()][block_sample->block_size()])
 {
+    // construct shape of total result array from shape of correlation function
+    boost::array<typename block_result_type::size_type, rank_> extents;
+    extents[0] = block_sample_->count();
+    extents[1] = block_sample_->block_size();
+    auto shape = detail::get_shape(*tcf_);
+    for (unsigned int i = 2; i < rank_; ++i) {
+        extents[i] = shape[i - 2];
+    }
+    // memory allocation
+    result_.resize(extents);
+    mean_.resize(extents);
+    error_.resize(extents);
+    count_.resize(extents);
 }
 
 template <typename tcf_type>
@@ -152,65 +216,44 @@ void correlation<tcf_type>::compute(unsigned int level)
 }
 
 template <typename tcf_type>
-typename correlation<tcf_type>::block_mean_type const&
-correlation<tcf_type>::get_mean()
+std::function<typename correlation<tcf_type>::block_mean_type const& ()>
+correlation<tcf_type>::get_mean(std::shared_ptr<correlation<tcf_type>> self)
 {
-    for (std::size_t i = 0; i < result_.shape()[0]; ++i) {
-        for (std::size_t j = 0; j < result_.shape()[1]; ++j) {
-            mean_[i][j] = mean(result_[i][j]);
+    return [=]() -> block_mean_type const& {
+        auto in  = self->result_.origin();
+        auto out = self->mean_.origin();
+        for (unsigned int i = 0; i < self->mean_.num_elements(); ++i) {
+            *out++ = mean(*in++);
         }
-    }
-    return mean_;
-}
-
-template <typename tcf_type>
-typename correlation<tcf_type>::block_error_type const&
-correlation<tcf_type>::get_error()
-{
-    for (std::size_t i = 0; i < result_.shape()[0]; ++i) {
-        for (std::size_t j = 0; j < result_.shape()[1]; ++j) {
-            error_[i][j] = error_of_mean(result_[i][j]);
-        }
-    }
-    return error_;
-}
-
-template <typename tcf_type>
-typename correlation<tcf_type>::block_count_type const&
-correlation<tcf_type>::get_count()
-{
-    for (std::size_t i = 0; i < result_.shape()[0]; ++i) {
-        for (std::size_t j = 0; j < result_.shape()[1]; ++j) {
-            count_[i][j] = count(result_[i][j]);
-        }
-    }
-    return count_;
-}
-
-template <typename correlation_type>
-static std::function<typename correlation_type::block_mean_type const& ()>
-wrap_mean(std::shared_ptr<correlation_type> self)
-{
-    return [=]() -> typename correlation_type::block_mean_type const& {
-        return self->get_mean();
+        return self->mean_;
     };
 }
 
-template <typename correlation_type>
-static std::function<typename correlation_type::block_error_type const& ()>
-wrap_error(std::shared_ptr<correlation_type> self)
+template <typename tcf_type>
+std::function<typename correlation<tcf_type>::block_mean_type const& ()>
+correlation<tcf_type>::get_error(std::shared_ptr<correlation<tcf_type>> self)
 {
-    return [=]() -> typename correlation_type::block_error_type const& {
-        return self->get_error();
+    return [=]() -> block_mean_type const& {
+        auto in  = self->result_.origin();
+        auto out = self->error_.origin();
+        for (unsigned int i = 0; i < self->error_.num_elements(); ++i) {
+            *out++ = error_of_mean(*in++);
+        }
+        return self->error_;
     };
 }
 
-template <typename correlation_type>
-static std::function<typename correlation_type::block_count_type const& ()>
-wrap_count(std::shared_ptr<correlation_type> self)
+template <typename tcf_type>
+std::function<typename correlation<tcf_type>::block_count_type const& ()>
+correlation<tcf_type>::get_count(std::shared_ptr<correlation<tcf_type>> self)
 {
-    return [=]() -> typename correlation_type::block_count_type const& {
-        return self->get_count();
+    return [=]() -> block_count_type const& {
+        auto in  = self->result_.origin();
+        auto out = self->count_.origin();
+        for (unsigned int i = 0; i < self->count_.num_elements(); ++i) {
+            *out++ = count(*in++);
+        }
+        return self->count_;
     };
 }
 
@@ -225,9 +268,9 @@ void correlation<tcf_type>::luaopen(lua_State* L)
             namespace_("dynamics")
             [
                 class_<correlation, _Base>()
-                    .property("mean", &wrap_mean<correlation>)
-                    .property("error", &wrap_error<correlation>)
-                    .property("count", &wrap_count<correlation>)
+                    .property("mean", &correlation::get_mean)
+                    .property("error", &correlation::get_error)
+                    .property("count", &correlation::get_count)
                     .scope
                     [
                         class_<runtime>("runtime")
