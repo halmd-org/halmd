a014fa8d967c90638cb6c3e1c55520478a73d994
Felix Höfling <hoefling@mf.mpg.de>
2013-11-19 14:43:41 +0100

    numeric.blas: disable warning for CUDA 5.5 headers
    Clang ≥ 3.3 emits a warning for CUDA 5.5 headers:
    
        /usr/local/cuda-5.5/include/cuda_runtime.h:225:33: warning: function
        'cudaMallocHost' is not needed and will not be emitted
        [-Wunneeded-internal-declaration]

--- halmd/numeric/blas/detail/operators.hpp
+++ halmd/numeric/blas/detail/operators.hpp
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2008-2012  Peter Colberg and Felix Höfling
+ * Copyright © 2008-2013 Felix Höfling
+ * Copyright © 2008-2012 Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -34,8 +35,21 @@
 # include <cmath>
 # include <iostream>
 #endif
+
+// CUDA vector types for host compiler
 #ifdef HALMD_WITH_GPU
-# include <cuda_runtime.h> // CUDA vector types for host compiler
+/* Disable warning for CUDA 5.5 headers emitted by Clang:
+ *   /usr/local/cuda-5.5/include/cuda_runtime.h:225:33: warning: function
+ *   'cudaMallocHost' is not needed and will not be emitted [-Wunneeded-internal-declaration]
+ */
+# if (defined(__clang__) && __clang_major__ >= 3 && __clang_minor__ > 2)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wunneeded-internal-declaration"
+#  include <cuda_runtime.h>
+#  pragma GCC diagnostic pop
+# else
+#  include <cuda_runtime.h>
+# endif
 #endif
 
 #include <halmd/numeric/blas/detail/vector.hpp>
