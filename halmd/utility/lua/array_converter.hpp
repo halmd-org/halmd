halmd-org/halmd

commit 4b16bd3d27381545fb215178bd4ed589ad46821e

lua: fix FTBFS with Boost C++ 1.56
The move constructor of boost::cref has silently been dropped for
compatibility with std::cref:
https://svn.boost.org/trac/boost/ticket/10368

This breaks code such as "cref(v[i])" found in some Lua converters.
Rewrite the for-loops to avoid such constructions. Use std::cref
throughout.

halmd/utility/lua/array_converter.hpp
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2008-2010  Peter Colberg
+ * Copyright © 2014      Felix Höfling
+ * Copyright © 2008-2010 Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -60,9 +61,10 @@ struct default_converter<boost::array<T, N> >
     void to(lua_State* L, boost::array<T, N> const& array)
     {
         luaponte::object table = luaponte::newtable(L);
-        for (std::size_t i = 0; i < array.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : array) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(array[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }
@@ -107,9 +109,10 @@ struct default_converter<boost::multi_array<T, 1> >
     void to(lua_State* L, boost::multi_array<T, 1> const& array)
     {
         luaponte::object table = luaponte::newtable(L);
-        for (std::size_t i = 0; i < array.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : array) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(array[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/cache_converter.hpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright © 2014 Felix Höfling
  * Copyright © 2012 Peter Colberg
  *
  * This file is part of HALMD.
@@ -20,7 +21,7 @@
 #ifndef HALMD_UTILITY_LUA_CACHE_CONVERTER_HPP
 #define HALMD_UTILITY_LUA_CACHE_CONVERTER_HPP
 
-#include <boost/ref.hpp>
+#include <functional>
 #include <luaponte/luaponte.hpp>
 
 namespace halmd {
@@ -45,7 +46,7 @@ struct default_converter<halmd::cache<T> >
     //! convert from C++ to Lua
     void to(lua_State* L, halmd::cache<T> const& cache)
     {
-        object(L, boost::cref(*cache)).push(L);
+        object(L, std::cref(*cache)).push(L);
     }
 };
 

halmd/utility/lua/fixed_vector_converter.hpp
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2010  Peter Colberg
+ * Copyright © 2014 Felix Höfling
+ * Copyright © 2010 Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -63,9 +64,10 @@ struct default_converter<halmd::fixed_vector<T, N> >
     void to(lua_State* L, halmd::fixed_vector<T, N> const& v)
     {
         object table = newtable(L);
-        for (std::size_t i = 0; i < v.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(v[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/hdf5.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2010  Peter Colberg
+ * Copyright © 2014 Felix Höfling
+ * Copyright © 2010 Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -46,7 +47,7 @@ static luaponte::object read_attribute(
 {
     if (h5xx::exists_attribute(object, name)) {
         T value = h5xx::read_attribute<T>(object, name);
-        return luaponte::object(L, boost::cref(value));
+        return luaponte::object(L, std::cref(value));
     }
     return luaponte::object(); // == nil
 }

halmd/utility/lua/pair_converter.hpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright © 2014 Felix Höfling
  * Copyright © 2012 Peter Colberg
  *
  * This file is part of HALMD.
@@ -54,8 +55,8 @@ struct default_converter<std::pair<T, U> >
     void to(lua_State* L, std::pair<T, U> const& v)
     {
         object table = newtable(L);
-        table[1] = boost::cref(v.first);
-        table[2] = boost::cref(v.second);
+        table[1] = std::cref(v.first);
+        table[2] = std::cref(v.second);
         table.push(L);
     }
 };

halmd/utility/lua/raw_array_converter.hpp
@@ -78,9 +78,10 @@ struct default_converter<halmd::raw_array<T> >
     {
         LOG_TRACE("convert halmd::raw_array<" << demangled_name<T>() << "> of size " << v.size() << " to Lua table");
         object table = newtable(L);
-        for (std::size_t i = 0; i < v.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(v[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/ublas.hpp
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2010  Peter Colberg
+ * Copyright © 2014 Felix Höfling
+ * Copyright © 2010 Peter Colberg
  *
  * This file is part of HALMD.
  *
@@ -63,9 +64,10 @@ struct default_converter<boost::numeric::ublas::vector<T, A> >
     void to(lua_State* L, boost::numeric::ublas::vector<T, A> const& v)
     {
         object table = newtable(L);
-        for (std::size_t i = 0; i < v.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(v[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }
@@ -117,7 +119,7 @@ struct default_converter<boost::numeric::ublas::matrix<T, F, A> >
         for (std::size_t i = 0; i < m.size1(); ++i) {
             vector_type r = row(m, i);
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(r);
+            table[i + 1] = std::cref(r);
         }
         table.push(L);
     }
@@ -162,7 +164,7 @@ struct default_converter<boost::numeric::ublas::symmetric_matrix<T, F1, F2, A> >
         for (std::size_t i = 0; i < m.size1(); ++i) {
             vector_type r = row(m, i);
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(r);
+            table[i + 1] = std::cref(r);
         }
         table.push(L);
     }

halmd/utility/lua/vector_converter.hpp
@@ -67,9 +67,10 @@ struct default_converter<std::vector<T> >
     {
         LOG_TRACE("convert std::vector<" << demangled_name<T>() << "> of size " << v.size() << " to Lua table");
         object table = newtable(L);
-        for (std::size_t i = 0; i < v.size(); ++i) {
+        std::size_t i = 1;
+        for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = boost::cref(v[i]);
+            table[i++] = std::cref(x);
         }
         table.push(L);
     }

commit c246a6fe492988f9e80cf345d61a028a3598484b

lua converters: revert to boost::cref
Using std::cref causes a Lua error "Trying to use unregistered class".
The reason is that luaponte (luabind) is C++98 and binds only
boost::cref, see libs/luaponte/luaponte/detail/convert_to_lua.hpp.

The bug was introduced in commit:4b16bd3, which was not properly tested.
This fixes #335919.

halmd/utility/lua/array_converter.hpp
@@ -64,7 +64,7 @@ struct default_converter<boost::array<T, N> >
         std::size_t i = 1;
         for (auto const& x : array) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }
@@ -112,7 +112,7 @@ struct default_converter<boost::multi_array<T, 1> >
         std::size_t i = 1;
         for (auto const& x : array) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/cache_converter.hpp
@@ -21,7 +21,7 @@
 #ifndef HALMD_UTILITY_LUA_CACHE_CONVERTER_HPP
 #define HALMD_UTILITY_LUA_CACHE_CONVERTER_HPP
 
-#include <functional>
+#include <boost/ref.hpp>
 #include <luaponte/luaponte.hpp>
 
 namespace halmd {
@@ -46,7 +46,7 @@ struct default_converter<halmd::cache<T> >
     //! convert from C++ to Lua
     void to(lua_State* L, halmd::cache<T> const& cache)
     {
-        object(L, std::cref(*cache)).push(L);
+        object(L, boost::cref(*cache)).push(L);
     }
 };
 

halmd/utility/lua/fixed_vector_converter.hpp
@@ -67,7 +67,7 @@ struct default_converter<halmd::fixed_vector<T, N> >
         std::size_t i = 1;
         for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/hdf5.cpp
@@ -47,7 +47,7 @@ static luaponte::object read_attribute(
 {
     if (h5xx::exists_attribute(object, name)) {
         T value = h5xx::read_attribute<T>(object, name);
-        return luaponte::object(L, std::cref(value));
+        return luaponte::object(L, boost::cref(value));
     }
     return luaponte::object(); // == nil
 }

halmd/utility/lua/pair_converter.hpp
@@ -55,8 +55,8 @@ struct default_converter<std::pair<T, U> >
     void to(lua_State* L, std::pair<T, U> const& v)
     {
         object table = newtable(L);
-        table[1] = std::cref(v.first);
-        table[2] = std::cref(v.second);
+        table[1] = boost::cref(v.first);
+        table[2] = boost::cref(v.second);
         table.push(L);
     }
 };

halmd/utility/lua/raw_array_converter.hpp
@@ -81,7 +81,7 @@ struct default_converter<halmd::raw_array<T> >
         std::size_t i = 1;
         for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }

halmd/utility/lua/ublas.hpp
@@ -67,7 +67,7 @@ struct default_converter<boost::numeric::ublas::vector<T, A> >
         std::size_t i = 1;
         for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }
@@ -119,7 +119,7 @@ struct default_converter<boost::numeric::ublas::matrix<T, F, A> >
         for (std::size_t i = 0; i < m.size1(); ++i) {
             vector_type r = row(m, i);
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = std::cref(r);
+            table[i + 1] = boost::cref(r);
         }
         table.push(L);
     }
@@ -164,7 +164,7 @@ struct default_converter<boost::numeric::ublas::symmetric_matrix<T, F1, F2, A> >
         for (std::size_t i = 0; i < m.size1(); ++i) {
             vector_type r = row(m, i);
             // default_converter<T> only invoked with reference wrapper
-            table[i + 1] = std::cref(r);
+            table[i + 1] = boost::cref(r);
         }
         table.push(L);
     }

halmd/utility/lua/vector_converter.hpp
@@ -70,7 +70,7 @@ struct default_converter<std::vector<T> >
         std::size_t i = 1;
         for (auto const& x : v) {
             // default_converter<T> only invoked with reference wrapper
-            table[i++] = std::cref(x);
+            table[i++] = boost::cref(x);
         }
         table.push(L);
     }