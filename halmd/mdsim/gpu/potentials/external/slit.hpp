8553877fe6522ab4aa912b0809cc812924483d3d
Sutapa Roy <sutapa@is.mpg.de>
2014-09-16 13:43:37 +0200

    slit.hpp: ready for use on gpu

---
+++ halmd/mdsim/gpu/potentials/external/slit.hpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright © 2014 Sutapa Roy
+ * Copyright © 2014 Felix Höfling
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_MDSIM_GPU_POTENTIALS_EXTERNAL_SLIT_HPP
+#define HALMD_MDSIM_GPU_POTENTIALS_EXTERNAL_SLIT_HPP
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <cmath>
+#include <lua.hpp>
+#include <memory>
+
+#include <halmd/io/logger.hpp>
+#include <halmd/mdsim/gpu/potentials/external/slit_kernel.hpp>
+
+namespace halmd {
+namespace mdsim {
+namespace gpu {
+namespace potentials {
+namespace external {
+
+/**
+ * define slit potential and parameters
+ */
+template <int dimension, typename float_type>
+class slit
+{
+public:
+    typedef slit_kernel::slit<dimension> gpu_potential_type;
+
+    typedef fixed_vector<float_type, dimension> vector_type;
+    typedef boost::numeric::ublas::vector<float_type> scalar_container_type;
+    typedef boost::numeric::ublas::vector<vector_type> vector_container_type;
+    typedef boost::numeric::ublas::matrix<float_type> matrix_container_type;
+
+    slit(
+        scalar_container_type const& offset
+      , vector_container_type const& surface_normal
+      , matrix_container_type const& epsilon
+      , matrix_container_type const& sigma
+      , matrix_container_type const& wetting
+      , std::shared_ptr<halmd::logger> logger = std::make_shared<halmd::logger>()
+    );
+
+    /** bind textures before kernel invocation */
+    void bind_textures() const
+    {
+        slit_wrapper::param_geometry.bind(g_param_geometry_);
+        slit_wrapper::param_potential.bind(g_param_potential_);
+    }
+    
+    scalar_container_type const& offset() const
+    {
+        return offset_;
+    }
+    
+    vector_container_type const& surface_normal() const
+    {
+        return surface_normal_;
+    }
+
+    matrix_container_type const& epsilon() const
+    {
+        return epsilon_;
+    }
+    
+    matrix_container_type const& sigma() const
+    {
+        return sigma_;
+    }
+    
+    matrix_container_type const& wetting() const
+    {
+        return wetting_;
+    }
+    
+    unsigned int size() const
+    {
+        return offset_.size();
+    }
+
+
+    /**
+     * Bind class to Lua.
+     */
+    static void luaopen(lua_State* L);
+
+private:
+    /** wall positions in MD units */
+    scalar_container_type offset_;
+    /** wall normal vectors in MD units */
+    vector_container_type surface_normal_;
+    /** interaction strengths for wall potential in MD units */
+    matrix_container_type epsilon_;
+    /** interaction ranges for wall potential in MD units */
+    matrix_container_type sigma_;
+    /** wetting parameters for wall potential in MD units */
+    matrix_container_type wetting_;
+
+    /** potential parameters at CUDA device */
+    cuda::vector<float4> g_param_geometry_;
+    cuda::vector<float4> g_param_potential_;
+
+    /** module logger */
+    std::shared_ptr<logger> logger_;
+};
+
+} // namespace external
+} // namespace potentials
+} // namespace gpu
+} // namespace mdsim
+} // namespace halmd
+
+#endif /* ! HALMD_MDSIM_GPU_POTENTIALS_EXTERNAL_SLIT_HPP */
