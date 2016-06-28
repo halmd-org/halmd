96bb6b5d3877fb0de55deb397ba2363d2516eeac
Felix Höfling <f.hoefling@fu-berlin.de>
2016-06-28 13:58:44 +0200

    release HALMD under the terms of the LGPL-3+ license
    All contributors have agreed to this change of the license, see
    http://www.hostedredmine.com/issues/562004
    
    This closes #562004.

--- halmd/main.cpp
+++ halmd/main.cpp
@@ -5,17 +5,18 @@
  * This file is part of HALMD.
  *
  * HALMD is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include <halmd/config.hpp>
@@ -91,7 +92,7 @@ int main(int argc, char **argv)
                  << "Copyright © " PROGRAM_COPYRIGHT << endl << endl
                  << "This is free software. "
                     "You may redistribute copies of it under the terms of" << endl
-                 << "the GNU General Public License version 3 or later "
+                 << "the GNU Lesser General Public License version 3 or later "
                     "<http://www.gnu.org/licenses/gpl.html>." << endl
                  << "There is NO WARRANTY, to the extent permitted by law." << endl;
             return EXIT_SUCCESS;
