ee973c5b62ae18ed9626ef3bc3db78fd3202d2f2
Arthur Straube <arthur.straube@fu-berlin.de>
2016-12-21 16:05:29 +0100

    patch h5md for VMD: handle case that species is not given
    If the field "species" is not present in the H5MD file, the script
    aborted due to "s" being None.

--- examples/plotting/patch_h5md_for_VMD.py
+++ examples/plotting/patch_h5md_for_VMD.py
@@ -52,11 +52,14 @@ if not "particles" in f:
 s = None
 for p in f["particles"].itervalues():
     # make species time-independent
+    species = None
     if "species" in p.keys():
         species = p["species/value"][0]
         del p["species"]
         p["species"] = species
-        s = numpy.append(s, species) if s != None else species
+    else:
+        species = 0
+    s = numpy.append(s, species) if s != None else species
 
     # add image data and fold back particle positions to periodic box
     if "image" not in p.keys() and "position" in p.keys():
