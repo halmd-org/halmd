8c9bdac957f59bcd989a82c5b685ff31304a1fa8
Felix Höfling <f.hoefling@fu-berlin.de>
2017-08-26 12:38:59 +0200

    hilbert: edit profiling messages

--- halmd/mdsim/gpu/sorts/hilbert.cpp
+++ halmd/mdsim/gpu/sorts/hilbert.cpp
@@ -70,7 +70,7 @@ hilbert<dimension, float_type>::hilbert(
 template <int dimension, typename float_type>
 void hilbert<dimension, float_type>::order()
 {
-    LOG_TRACE("order particles after Hilbert space-filling curve");
+    LOG_TRACE("order particles along Hilbert space-filling curve");
     {
         scoped_timer_type timer(runtime_.order);
         cuda::vector<unsigned int> g_index(particle_->nparticle());
