3405ae55c7c07593a58b8bfc6993da2a65d1643c
Jake Atwell <jatwell@physik.fu-berlin.de>
2017-09-07 11:55:44 +0200

    gpu/orientations: Add mrg32k3a as RNG

--- halmd/mdsim/gpu/orientations/uniform.cpp
+++ halmd/mdsim/gpu/orientations/uniform.cpp
@@ -95,12 +95,14 @@ HALMD_LUA_API int luaopen_libhalmd_mdsim_gpu_orientations_uniform(lua_State* L)
 {
     uniform<3, float, halmd::random::gpu::rand48>::luaopen(L);
     uniform<3, float, halmd::random::gpu::mrg32k3a>::luaopen(L);
+    uniform<2, float, halmd::random::gpu::mrg32k3a>::luaopen(L);
     return 0;
 }
 
 // explicit instantiation
 template class uniform<3, float, halmd::random::gpu::rand48>;
 template class uniform<3, float, halmd::random::gpu::mrg32k3a>;
+template class uniform<2, float, halmd::random::gpu::mrg32k3a>;
 
 } // namespace mdsim
 } // namespace gpu
