e0cc2207c82fdae1ef3870ef767c3889895cead5
Jake Atwell <jatwell@physik.fu-berlin.de>
2018-09-04 14:41:26 +0200

    type_traits: Add new pseudo vector type
    Add new type for pseudo_vector- and coalesced_pseudo_vector types.

--- halmd/mdsim/type_traits.hpp
+++ halmd/mdsim/type_traits.hpp
@@ -48,11 +48,13 @@ template <> struct basic_vector_type<1, float> { typedef float type; typedef typ
 template <> struct basic_vector_type<2, float> { typedef float2 type; typedef type* ptr_type; typedef type const* const_ptr_type; };
 template <> struct basic_vector_type<3, float> { typedef float3 type; typedef type* ptr_type; typedef type const* const_ptr_type; };
 template <> struct basic_vector_type<4, float> { typedef float4 type; typedef type* ptr_type; typedef type const* const_ptr_type; };
+template <> struct basic_vector_type<6, float> { typedef fixed_vector<float, 6> type; typedef type* ptr_type; typedef type const* const_ptr_type; };
 
 template <> struct basic_vector_type<1, dsfloat> { typedef dsfloat type; typedef dsfloat_ptr<float> ptr_type; typedef dsfloat_const_ptr<float> const_ptr_type; };
 template <> struct basic_vector_type<2, dsfloat> { typedef fixed_vector<dsfloat, 2> type; typedef dsfloat_ptr<float2> ptr_type; typedef dsfloat_const_ptr<float2> const_ptr_type; };
 template <> struct basic_vector_type<3, dsfloat> { typedef fixed_vector<dsfloat, 3> type; typedef dsfloat_ptr<float3> ptr_type; typedef dsfloat_const_ptr<float3> const_ptr_type; };
 template <> struct basic_vector_type<4, dsfloat> { typedef fixed_vector<dsfloat, 4> type; typedef dsfloat_ptr<float4> ptr_type; typedef dsfloat_const_ptr<float4> const_ptr_type; };
+template <> struct basic_vector_type<6, dsfloat> { typedef fixed_vector<dsfloat, 6> type; typedef dsfloat_ptr<type> ptr_type; typedef dsfloat_const_ptr<type> const_ptr_type; };
 
 // template <> struct basic_vector_type<1, double> { typedef double type; typedef type const* const_ptr_type; };
 // template <> struct basic_vector_type<2, double> { typedef double2 type; typedef type const* const_ptr_type; };
@@ -79,8 +81,9 @@ struct type_traits;
 template <typename value_type>
 struct type_traits<4, value_type>
 {
-    typedef typename basic_vector_type<4, value_type>::type coalesced_vector_type;
     typedef typename basic_vector_type<4, value_type>::type vector_type;
+    typedef typename basic_vector_type<4, value_type>::type coalesced_vector_type;
+    typedef typename basic_vector_type<6, value_type>::type coalesced_pseudo_vector_type;
     typedef typename basic_vector_type<4, value_type>::ptr_type ptr_type;
     typedef typename basic_vector_type<4, value_type>::const_ptr_type const_ptr_type;
 };
@@ -88,8 +91,9 @@ struct type_traits<4, value_type>
 template <typename value_type>
 struct type_traits<3, value_type>
 {
-    typedef typename basic_vector_type<4, value_type>::type coalesced_vector_type;
     typedef typename basic_vector_type<3, value_type>::type vector_type;
+    typedef typename basic_vector_type<4, value_type>::type coalesced_vector_type;
+    typedef typename basic_vector_type<4, value_type>::type coalesced_pseudo_vector_type;
     typedef typename basic_vector_type<4, value_type>::ptr_type ptr_type;
     typedef typename basic_vector_type<4, value_type>::const_ptr_type const_ptr_type;
 };
@@ -97,8 +101,9 @@ struct type_traits<3, value_type>
 template <typename value_type>
 struct type_traits<2, value_type>
 {
-    typedef typename basic_vector_type<2, value_type>::type coalesced_vector_type;
     typedef typename basic_vector_type<2, value_type>::type vector_type;
+    typedef typename basic_vector_type<2, value_type>::type coalesced_vector_type;
+    typedef value_type coalesced_pseudo_vector_type;
     typedef typename basic_vector_type<2, value_type>::ptr_type ptr_type;
     typedef typename basic_vector_type<2, value_type>::const_ptr_type const_ptr_type;
 };
@@ -106,8 +111,9 @@ struct type_traits<2, value_type>
 template <typename value_type>
 struct type_traits<1, value_type>
 {
-    typedef value_type coalesced_vector_type;
     typedef value_type vector_type;
+    typedef value_type coalesced_vector_type;
+    typedef value_type coalesced_pseudo_vector_type;
     typedef typename basic_vector_type<1, value_type>::ptr_type ptr_type;
     typedef typename basic_vector_type<1, value_type>::const_ptr_type const_ptr_type;
 };
@@ -122,29 +128,61 @@ struct type_traits<1, value_type>
  * template parameters are space dimension and value type
  */
 
+// struct specialization is necessary because there is no analytical formula for pseudovetor dimensionality
 template <int dimension, typename value_type>
-struct type_traits
-{
-    typedef fixed_vector<value_type, dimension> vector_type;
-    // diagonal elements are the first 'dimension' elements
-    // followed by the off-diagonals T_12, T_13, ..., T_23, ...
-    typedef fixed_vector<value_type, (dimension + 1) * dimension / 2> stress_tensor_type;
-
-#ifdef HALMD_WITH_GPU
-    typedef detail::gpu::type_traits<dimension, value_type> gpu;
-#endif
-};
+struct type_traits;
 
 template <typename value_type>
 struct type_traits<1, value_type>
 {
     typedef value_type vector_type;
-    // diagonal elements are the first 'dimension' elements
-    // followed by the off-diagonals T_12, T_13, ..., T_23, ...
     typedef value_type stress_tensor_type;
 
 #ifdef HALMD_WITH_GPU
     typedef detail::gpu::type_traits<1, value_type> gpu;
+#endif
+};
+
+template <typename value_type>
+struct type_traits<2, value_type>
+{
+    typedef fixed_vector<value_type, 2> vector_type;
+    // diagonal elements are the first 'dimension' elements
+    // followed by the off-diagonals T_12, T_13, ..., T_23, ...
+    typedef fixed_vector<value_type, 3> stress_tensor_type;
+    typedef value_type pseudo_vector_type;
+
+#ifdef HALMD_WITH_GPU
+    typedef detail::gpu::type_traits<2, value_type> gpu;
+#endif
+};
+
+template <typename value_type>
+struct type_traits<3, value_type>
+{
+    typedef fixed_vector<value_type, 3> vector_type;
+    // diagonal elements are the first 'dimension' elements
+    // followed by the off-diagonals T_12, T_13, ..., T_23, ...
+    typedef fixed_vector<value_type, 6> stress_tensor_type;
+    typedef fixed_vector<value_type, 3> pseudo_vector_type;
+
+
+#ifdef HALMD_WITH_GPU
+    typedef detail::gpu::type_traits<3, value_type> gpu;
+#endif
+};
+
+template <typename value_type>
+struct type_traits<4, value_type>
+{
+    typedef fixed_vector<value_type, 4> vector_type;
+    // diagonal elements are the first 'dimension' elements
+    // followed by the off-diagonals T_12, T_13, ..., T_23, ...
+    typedef fixed_vector<value_type, 10> stress_tensor_type;
+    typedef fixed_vector<value_type, 6> pseudo_vector_type;
+
+#ifdef HALMD_WITH_GPU
+    typedef detail::gpu::type_traits<4, value_type> gpu;
 #endif
 };
 
