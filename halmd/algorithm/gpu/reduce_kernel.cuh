133b3e31dddce13aadc6505efd793f5ac1c83cae
Jaslo Ziska <jaslo.ziska@fu-berlin.de>
2021-01-25 16:01:52 +0100

    Implement new reduce algorithm in `reduce_kernel.cuh`
    This commit changes the reduce function in `reduce_kernel.cuh` to be similar
    to the device reduce function implemented in the last commit.

--- halmd/algorithm/gpu/reduce_kernel.cuh
+++ halmd/algorithm/gpu/reduce_kernel.cuh
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2008-2012 Peter Colberg
+ * Copyright © 2021      Jaslo Ziska
  *
  * This file is part of HALMD.
  *
@@ -23,44 +24,59 @@
 
 #include <boost/utility/enable_if.hpp>
 
+#include <halmd/algorithm/gpu/bits/shfl.cuh>
 #include <halmd/algorithm/gpu/reduce_kernel.hpp>
 #include <halmd/utility/gpu/thread.cuh>
 
 namespace halmd {
 namespace detail {
 
-template <unsigned int threads, typename accumulator_type>
-inline __device__ typename boost::enable_if_c<threads == 1, void>::type
-reduce(accumulator_type& acc, accumulator_type s_acc[])
+using halmd::algorithm::gpu::bits::shfl_down;
+
+/**
+ * Reduce accumulators of warp
+ *
+ * @param acc accumulator
+ */
+template <typename accumulator_type>
+__device__ void reduce_warp(accumulator_type& acc)
 {
-    if (TID < threads) {
-        acc(s_acc[TID + threads]);
+    int delta = WARP_SIZE / 2;
+    acc(shfl_down(FULL_MASK, acc, delta));
+
+    if (WTID < WARP_SIZE / 2) {
+        while (delta > 1) {
+            delta >>= 1; // <=> delta /= 2;
+            acc(shfl_down(0xFFFF, acc, delta));
+        }
     }
 }
 
-template <unsigned int threads, typename accumulator_type>
-inline __device__ typename boost::disable_if_c<threads == 1, void>::type
-reduce(accumulator_type& acc, accumulator_type s_acc[])
+/**
+ * Reduce accumulators of warp for less than 32 threads
+ *
+ * @param acc accumulator
+ * @param size number of threads to be reduced
+ */
+template <typename accumulator_type>
+__device__ void reduce_warp(accumulator_type& acc, unsigned int size)
 {
-    if (TID < threads) {
-        acc(s_acc[TID + threads]);
-        s_acc[TID] = acc;
+    assert(size > 1 && size <= warpSize);
+
+    int delta = 1 << (31 - __clz(size - 1)); // round down to previous power of 2 (powers of two are rounded down too)
+
+    accumulator_type tmp = shfl_down(FULL_MASK, acc, delta);
+    if (WTID + delta < size) { // only apply transformation if the warps are included in size
+        acc(tmp);
     }
 
-    //
-    // FIXME __syncthreads only if threads >= warpSize
-    //
-    // On Fermi devices (even with PTX < 2.0), this requires acc and s_acc
-    // to be declared as volatile, which in turn requires the methods of
-    // the accumulator and any used data type (fixed_vector, dsfloat) to
-    // be declared volatile, in the case of member methods, and/or accept
-    // function arguments with the volatile qualifier.
-    //
-    // Refer to Fermi Compatibility Guide, 1.3.3 Kernels.
-    //
-    __syncthreads();
-
-    reduce<threads / 2>(acc, s_acc);
+    if (WTID < (warpSize >> 1)) { // exit upper half-warp
+        // now that delta is a power of two reduce normally
+        while (delta > 1) {
+            delta >>= 1; // <=> delta /= 2;
+            acc(shfl_down(0xFFFF, acc, delta));
+        }
+    }
 }
 
 /**
@@ -74,16 +90,28 @@ inline __device__ void reduce(accumulator_type& acc)
     // We need to avoid default initialization of the shared memory
     // array, since this increases execution time of the kernel.
     // Use a char array, and cast to type of reduction functor.
-    __shared__ char s_storage[threads * sizeof(accumulator_type)];
-
+    // The size of the shared memory array is 1024 / WARP_SIZE because, at the time of writing this function,
+    // the maximum number of threads per block is 1024 and the warp size is 32 so the length of the shared memory
+    // would never be exceeded.
+    assert(WDIM <= 1024 / WARP_SIZE);
+    __shared__ char s_storage[sizeof(accumulator_type) * 1024 / WARP_SIZE];
     accumulator_type* const s_acc = reinterpret_cast<accumulator_type*>(s_storage);
 
-    // reduced value for this thread
-    s_acc[TID] = acc;
+    // reduce each warp individually
+    reduce_warp(acc);
+
+    // write the result from the first thread of each warp (first lane) to shared memory
+    if (WTID == 0) {
+        s_acc[WID] = acc;
+    }
+
     __syncthreads();
 
-    // compute reduced value for all threads in block
-    reduce<threads / 2>(acc, s_acc);
+    // reduce the results from all warps in the first warp
+    if (WID == 0) {
+        acc = s_acc[WTID];
+        reduce_warp(acc, WDIM);
+    }
 }
 
 /**
