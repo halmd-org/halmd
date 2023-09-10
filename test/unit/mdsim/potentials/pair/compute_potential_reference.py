2b252a14501bce644f4b52795900187e9449d02f
Felix Höfling <f.hoefling@fu-berlin.de>
2023-09-10 22:23:02 +0200

    potential tests: adjust auxiliary Python script for reference values
    The Python script, exemplarily, used the Morse potential, which has been
    modified in the previous commit.

--- test/unit/mdsim/potentials/pair/compute_potential_reference.py
+++ test/unit/mdsim/potentials/pair/compute_potential_reference.py
@@ -5,17 +5,18 @@ import mpmath as mp
 mp.dps=18
 
 # set potential parameters
-eps    = mp.mpf(.5)
+eps    = mp.mpf(.25)
 sigma  = mp.mpf(2)
-rmin   = mp.mpf(.75)
+rmin   = mp.mpf(1.5)
+B      = mp.mpf(1.41)
 cutoff = mp.mpf(5) * sigma
 
 # define truncated and shifted Morse potential U(r)
-potential = lambda r : eps * ((1 - mp.exp(-r/sigma + rmin))**2 - 1)
+potential = lambda r : eps / (2 * B * B - 1) * (mp.exp(-2 * (r - rmin) / sigma / B) - 2 * B * B * mp.exp(-(r - rmin) / sigma / B))
 potential_trunc = lambda r : potential(r) - potential(cutoff)
 
 # fval = -U'(r) / r
-fval = lambda r : -2 * eps * (1 - mp.exp(-r/sigma + rmin)) * (mp.exp(-r/sigma + rmin) / sigma) / r
+fval = lambda r : 2 * eps / (2 * B * B - 1) * (mp.exp(-2 * (r - rmin) / sigma / B) / B - B * mp.exp(-(r - rmin) / sigma / B)) / sigma / r
 
 # output is suitable to be pasted into the .cpp file of the potential unit test
 for r in (.25, .5, .75, 1., 2., 5., 10.):
