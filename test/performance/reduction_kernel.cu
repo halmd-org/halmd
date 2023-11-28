4529034f4b63c3587dd8675a9c6a45052937334c
Felix Höfling <f.hoefling@fu-berlin.de>
2023-11-28 13:19:57 +0100

    test performance/reduction: test more transforms
    For vector types, the components are reduced separately using
    complex_sum_ and ternary_sum_.
    
    Currently, the test cases related to these tranforms fail.

--- test/performance/reduction_kernel.cu
+++ test/performance/reduction_kernel.cu
@@ -26,15 +26,53 @@
 #include <test/performance/reduction_kernel.hpp>
 
 #include <cuda_wrapper/cuda_wrapper.hpp>
+#include <type_traits>
 
 using namespace halmd::algorithm::gpu;
+using halmd::dsfloat;
+using halmd::fixed_vector;
 
 template <typename T>
-__global__ void reduce_sum(T const* input, T* output)
+__global__
+typename std::enable_if<std::is_arithmetic<T>::value, void>::type
+reduce_sum(T const* input, T* output)
 {
     for (int i = 0; i < NREDUCES; ++i) {
         T val = input[TID + i * NTHREADS];
         reduce<sum_>(val);
+
+        if (TID == 0) {
+            output[i] = val;
+        }
+    }
+}
+
+// In case of fixed_vector, we could just rely on the operator+ and use the above version.
+// Instead, the following specialisations test the multi-variable transforms
+// 'complex_sum' and 'ternary_sum'.
+template <typename T>
+__global__
+typename std::enable_if<std::is_same<T, fixed_vector<typename T::value_type, 2>>::value, void>::type
+reduce_sum(T const* input, T* output)
+{
+    for (int i = 0; i < NREDUCES; ++i) {
+        T val = input[TID + i * NTHREADS];
+        reduce<complex_sum_>(val[0], val[1]);
+
+        if (TID == 0) {
+            output[i] = val;
+        }
+    }
+}
+
+template <typename T>
+__global__
+typename std::enable_if<std::is_same<T, fixed_vector<typename T::value_type, 3>>::value, void>::type
+reduce_sum(T const* input, T* output)
+{
+    for (int i = 0; i < NREDUCES; ++i) {
+        T val = input[TID + i * NTHREADS];
+        reduce<ternary_sum_>(val[0], val[1], val[2]);
 
         if (TID == 0) {
             output[i] = val;
@@ -49,9 +87,6 @@ reduce_kernel<T> reduce_kernel<T>::kernel = {
 };
 
 // explicit template instantiations
-using halmd::dsfloat;
-using halmd::fixed_vector;
-
 template class reduce_kernel<int>;
 template class reduce_kernel<fixed_vector<int, 2>>;
 #ifdef USE_GPU_SINGLE_PRECISION
