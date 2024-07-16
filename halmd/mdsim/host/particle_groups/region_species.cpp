0f9c8ee2b3cab620d0b1bee5f4be6082b7b900e6
Felix Höfling <f.hoefling@fu-berlin.de>
2024-07-16 15:21:47 +0200

    improve logging output
      - particle groups: emit warning only once
    
      - thermodynamic observables: log creation of module and (possibly
        non-default) reference volume, e.g., if the group is based on a
        region in space

--- halmd/mdsim/host/particle_groups/region_species.cpp
+++ halmd/mdsim/host/particle_groups/region_species.cpp
@@ -117,7 +117,7 @@ region_species<dimension, float_type, geometry_type>::ordered() // ID order
     if (selection_cache != ordered_cache_) {
 //        auto const& id = read_cache(particle_->id());
 
-        LOG_WARNING("sorting selection of particle indices not yet implemented");
+        LOG_WARNING_ONCE("sorting selection of particle indices not yet implemented");
 //        LOG_DEBUG("sorting selection of particle indices");
         scoped_timer_type timer(runtime_.sort_selection);
 
