cb88e51f8fceb8e634f67457d568d9b049e36651
Giorgia Marcelli <giorgia.marcelli@fu-berlin.de>
2025-05-28 16:43:51 +0200

    completed script for 4 ctest cases

--- test/lua/mdsim/velocities/rescale.lua
+++ test/lua/mdsim/velocities/rescale.lua
@@ -18,27 +18,71 @@
 -- <http://www.gnu.org/licenses/>.
 --
 
+
 local log = halmd.io.log
 local mdsim = halmd.mdsim
 local observables = halmd.observables
 
--- Shared setup function
+local function compute_and_assert_energy(particle, args)
+    local mass = args.mass or 1
+    local stiffness = args.stiffness
+    local tolerance = 1e-6
+    local dimension = #particle.data.velocity[1]
+    local nparticle = #particle.data.velocity
+    local expected = args.target_energy 
+
+    --local failed = 0
+
+    local velocity = particle.data["velocity"] --these provide nan values 
+    local position = particle.data["position"]
+
+    for i = 1, nparticle do
+        local v = velocity[i] --these provide nan values 
+        local r = position[i]
+        local v_str = ""
+    
+        local v2, r2 = 0, 0
+        for d = 1, dimension do
+            v2 = v2 + v[d]^2
+            r2 = r2 + r[d]^2
+        end
+
+        -- Check for invalid values
+        if v2 ~= v2 or r2 ~= r2 then
+            log.error(("NaN detected in particle %d: v² = %.6f, r² = %.6f"):format(i, v2, r2))
+            failed = failed + 1
+        else
+            local kinetic = 0.5 * mass * v2
+            local potential = 0.5 * stiffness * r2
+            local energy = kinetic + potential
+
+            if energy ~= energy then
+                log.error(("NaN energy for particle %d: kinetic = %.6f, potential = %.6f"):format(i, kinetic, potential))
+                failed = failed + 1
+            end
+            assert(math.abs(energy - expected) <= tolerance * expected,("particle %d: energy %.6f != expected %.6f"):format(i, energy, expected))
+            
+        end
+    end
+
+end
+
+
+-- Shared test runner
 local function run_rescale_test(args)
     local dimension = args.dimension
     local np = args.particles
     local L = args.box_length
 
-    local box_length = {}
-    for i = 1, dimension do box_length[i] = L end
-    local box = mdsim.box({length = box_length})
+    local length = {}
+    for i = 1, dimension do length[i] = L end
+    local box = mdsim.box({length = length})
 
-    -- Particle system
     local particle = mdsim.particle({dimension = dimension, particles = np, species = 1})
     mdsim.positions.lattice({box = box, particle = particle}):set()
 
     local group = mdsim.particle_groups.all({particle = particle})
 
-    -- Velocity assignment
     if not args.zero_velocities then
         mdsim.velocities.boltzmann({
             particle = particle,
@@ -47,78 +91,124 @@ local function run_rescale_test(args)
         }):set()
     end
 
-    -- Optional harmonic potential
     if args.stiffness > 0 then
+        local stiffness = {}
+        local offset = {}
+        for i = 1, dimension do
+            stiffness[i] = args.stiffness
+            offset[i] = 0
+        end
         local potential = mdsim.potentials.external.harmonic({
-            stiffness = {args.stiffness},
-            offset = {0}
+            stiffness = stiffness,
+            offset = offset
         })
         mdsim.forces.external({box = box, particle = particle, potential = potential})
     end
 
-    -- Thermodynamics and rescaling
-    local thermo = observables.thermodynamics({box = box, group = group})
+    local thermo = observables.thermodynamics({group = group, box = box})
+
+    -- sample initial state
+   -- observables.sampler:sample()
+
+    local target_energy = args.target_energy
+    local e_kin = thermo:kinetic_energy()
+    local e_pot = thermo:potential_energy()
+
+    if target_energy <= e_pot then
+        error(("Invalid target energy: %.6f is less than potential energy %.6f"):format(target_energy, e_pot))
+    end
+
+    log.message(("target energy:    %.6f"):format(target_energy))
+    log.message(("computed total:   %.6f"):format(e_kin + e_pot))
+    log.message(("kinetic energy:   %.6f"):format(e_kin))
+    log.message(("potential energy: %.6f"):format(e_pot))
 
     mdsim.velocities.rescale({
         particle = particle,
-        energy = args.target_energy
+        energy = target_energy
     }):set()
 
-    -- Logging
-    log.message(("Target energy: %.3f"):format(args.target_energy))
-    log.message(("Kinetic energy: %.6f"):format(thermo.kinetic_energy()))
-    log.message(("Potential energy: %.6f"):format(thermo.potential_energy()))
-    log.message(("Total energy: %.6f"):format(thermo.energy()))
+    log.message(("target energy:    %.6f"):format(target_energy))
+    log.message(("computed total energy after rescaling:   %.6f"):format(thermo:internal_energy()))
+    log.message(("kinetic energy after rescaling:   %.6f"):format(thermo:kinetic_energy()))
+    log.message(("potential energy after rescaling: %.6f"):format(thermo:potential_energy()))
+
+    compute_and_assert_energy(particle, args)
+
 end
 
 -- Test cases
 test = {}
 
--- 1. E=1, Boltzmann, stiffness=1
-test["Etot=1"] = function(args)
-    args.target_energy = 1
+
+test["energy_target"] = function(args)
+    args.target_energy = 50
     args.zero_velocities = false
     args.stiffness = 1
     run_rescale_test(args)
 end
 
--- 2. E=2, Boltzmann, stiffness=1
-test["Etot=2"] = function(args)
-    args.target_energy = 2
-    args.zero_velocities = false
-    args.stiffness = 1
-    run_rescale_test(args)
-end
-
--- 3. E=1, Boltzmann, stiffness=0
-test["K=0"] = function(args)
+test["no_potential_energy"] = function(args)
     args.target_energy = 1
     args.zero_velocities = false
     args.stiffness = 0
     run_rescale_test(args)
 end
 
--- 4. E=1, Zero velocities, stiffness=1
-test["v=0"] = function(args)
-    args.target_energy = 1
+test["zero_initial_velocity"] = function(args)
+    args.target_energy = 50
     args.zero_velocities = true
     args.stiffness = 1
     run_rescale_test(args)
 end
 
--- Argument parsing
-function define_args(parser)
-    parser:add_argument("run_test", {type = "string", help = "Select case1, case2, case3, or case4"})
-    parser:add_argument("particles", {type = "number", default = 10000, help = "Number of particles"})
-    parser:add_argument("dimension", {type = "number", default = 3, help = "Simulation dimension"})
-    parser:add_argument("box-length", {type = "number", default = 10, help = "Box side length"})
-    parser:add_argument("temperature", {type = "number", default = 1.0, help = "Temperature for Boltzmann velocities"})
+test["adding_mass"] = function(args)
+    args.target_energy = 50
+    args.zero_velocities = false
+    args.stiffness = 1
+    args.mass = 1
+    run_rescale_test(args)
 end
 
--- Run selected case
+
+-- Argument parser
+function define_args(parser)
+    parser:add_argument("run_test", {
+        type = "string",
+        help = "select test: energy_target, no_potential_energy, zero_initial_velocity, adding_mass"
+    })
+    parser:add_argument("target_energy", {
+        type = "number", default = 10,
+        help = "target total energy to rescale velocities"
+    })
+    parser:add_argument("zero_velocities", {
+        type = "boolean", default = false,
+        help = "start with zero initial velocities"
+    })
+    parser:add_argument("stiffness", {
+        type = "number", default = 1,
+        help = "harmonic stiffness for potential energy"
+    })
+    parser:add_argument("mass", {type = "number", default = 1, help = "mass of particles"})
+    parser:add_argument("particles", {type = "number", default = 100, help = "number of particles"})
+    parser:add_argument("dimension", {type = "number", default = 3, help = "system dimensionality"})
+    parser:add_argument("box-length", {type = "number", default = 10, help = "box's length"})
+    parser:add_argument("temperature", {type = "number", default = 1.0, help = "temperature (if Boltzmann velocities)"})
+end
+
+-- Entry point
 function main(args)
-    assert(test[args.run_test], "Unknown test: " .. args.run_test)
-    log.message(("Running test: %s"):format(args.run_test))
-    test[args.run_test](args)
-    log.message(("Finished test: %s"):format(args.run_test))
+        -- run selected test case or, by default, all tests
+    local test_case = args.run_test
+    local cases = test_case and { test_case } or {"energy_target", "zero_initial_velocity", "no_potential_energy"}
+    -- assert(test[args.run_test], "Unknown test case: " .. args.run_test)
+    for i,case in ipairs(cases) do
+        log.message(("Running test case '%s' ..."):format(case))
+        assert(test[case], ("test case '%s' is not registered"):format(case))
+
+        -- call test function
+        test[case](args)
+        log.message(("Test case '%s' finished."):format(case))
+        log.message("")
+    end
 end
