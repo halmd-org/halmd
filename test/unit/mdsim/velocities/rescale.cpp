59dd8a3f9a2230cb388f0df75604ab81cce5567f
Felix Höfling <f.hoefling@fu-berlin.de>
2025-06-06 16:40:22 +0200

    velocities::rescale: simplify and improve unit test
     - remove dependency on thermodynamics, box, etc.,
       instead, we compute the mean energies via functionality in particle_group
    
     - assign artificial values to potential energy
    
     - test obtained energy for each particle separately
    
     - check exception throwing

--- test/unit/mdsim/velocities/rescale.cpp
+++ test/unit/mdsim/velocities/rescale.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2025 Giorgia Marcelli
- * Copyright © 2011-2017 Felix Höfling
+ * Copyright © 2011-2025 Felix Höfling
  * Copyright © 2011-2012 Peter Colberg
  *
  * This file is part of HALMD.
@@ -26,21 +26,16 @@
 #define BOOST_TEST_MODULE rescale
 #include <boost/test/unit_test.hpp>
 
-#include <halmd/mdsim/box.hpp>
 #include <halmd/mdsim/host/particle.hpp>
 #include <halmd/mdsim/host/particle_groups/all.hpp>
 #include <halmd/mdsim/host/velocities/boltzmann.hpp>
 #include <halmd/mdsim/host/velocities/rescale.hpp>
-#include <halmd/observables/host/thermodynamics.hpp>
 #include <halmd/random/host/random.hpp>
-#include <halmd/numeric/accumulator.hpp>
-#include <halmd/numeric/blas/blas.hpp>
 #ifdef HALMD_WITH_GPU
 # include <halmd/mdsim/gpu/particle.hpp>
 # include <halmd/mdsim/gpu/particle_groups/all.hpp>
 # include <halmd/mdsim/gpu/velocities/boltzmann.hpp>
 # include <halmd/mdsim/gpu/velocities/rescale.hpp>
-# include <halmd/observables/gpu/thermodynamics.hpp>
 # include <halmd/random/gpu/random.hpp>
 # include <halmd/utility/gpu/device.hpp>
 # include <test/tools/cuda.hpp>
@@ -55,19 +50,51 @@
 #include <limits>
 
 /**
+ * obtain particle data and verify that total energy equal target value
+ */
+template <typename particle_type>
+void check_target_energies(particle_type const& particle, double target_energy, double tolerance)
+{
+    // obtain particle data: velocities, masses, potential energies
+    std::vector<typename particle_type::velocity_type> velocity(particle.nparticle());
+    BOOST_CHECK(
+        get_velocity(particle, velocity.begin()) == velocity.end()
+    );
+
+    std::vector<typename particle_type::mass_type> mass(particle.nparticle());
+    BOOST_CHECK(
+        get_mass(particle, mass.begin()) == mass.end()
+    );
+
+    std::vector<typename particle_type::en_pot_type> potential_energy(particle.nparticle());
+    BOOST_CHECK(
+        particle.template get_data<typename particle_type::en_pot_type>(
+            "potential_energy", potential_energy.begin()
+        ) == potential_energy.end()
+    );
+
+    // check total energy for each particle
+    for (unsigned int i = 0; i < particle.nparticle(); ++i) {
+        auto const& v = velocity[i];
+        double en_pot = potential_energy[i];
+        double en_tot = en_pot + mass[i] * inner_prod(v, v) / 2;
+//        BOOST_TEST_MESSAGE("v[" << i << "] = " << v << ", en_pot = " << en_pot);
+        BOOST_CHECK_CLOSE_FRACTION(en_tot, target_energy, tolerance);
+    }
+}
+
+/**
  * test energy rescaling using the rescale velocity module
  **/
 
 template <typename modules_type>
 struct rescale_test
 {
-    typedef typename modules_type::box_type box_type;
     typedef typename modules_type::particle_type particle_type;
     typedef typename modules_type::particle_group_type particle_group_type;
     typedef typename modules_type::random_type random_type;
     typedef typename modules_type::boltzmann_type boltzmann_type;
     typedef typename modules_type::rescale_type rescale_type;
-    typedef typename modules_type::thermo_type thermo_type;
     typedef typename particle_type::vector_type vector_type;
     typedef typename vector_type::value_type float_type;
     static unsigned int const dimension = vector_type::static_size;
@@ -82,7 +109,6 @@ struct rescale_test
 
     std::shared_ptr<particle_type> particle;
     std::shared_ptr<boltzmann_type> boltzmann;
-    std::shared_ptr<thermo_type> thermo;
 
     void test();
     rescale_test();
@@ -93,37 +119,59 @@ void rescale_test<modules_type>::test()
 {
     particle_group_type group(particle);
 
-    // assign mass profile
+    // assign linear mass profile to particle array
     float_type scale_mass = float_type(0.2) / npart;
     set_mass(*particle, boost::make_transform_iterator(
         boost::make_counting_iterator(0)
-      , [=](int i) {
-            return float_type(0.9) + scale_mass * i;
-        }
+      , [=](int i) { return float_type(0.9) + scale_mass * i; }
+    ));
+
+    // assign potential energy profile to particle array, from 0 to target_energy
+    typedef typename particle_type::en_pot_type en_pot_type;
+    float_type scale_energy = target_energy / (npart - 1);
+    particle->template set_data<en_pot_type>(
+        "potential_energy"
+      , boost::make_transform_iterator(
+            boost::make_counting_iterator(0)
+          , [=](int i) {
+                // ensure that values do not exceed the target value, be aware of rounding errors
+                return std::min(scale_energy * i, static_cast<en_pot_type>(target_energy));
+            }
     ));
 
     // assign initial velocities from Maxwell-Boltzmann distribution
     BOOST_TEST_MESSAGE("generate Maxwell-Boltzmann distribution");
     boltzmann->set();
 
-    // output energies before rescaling
-    BOOST_TEST_MESSAGE("Energy before rescaling: total = " << thermo->en_tot()
-        << ", kinetic = " << thermo->en_kin()
-        << ", potential = " << thermo->en_pot());
+    // compute initial energies
+    double en_kin = get_mean_en_kin(*particle, group);
+    double en_pot = get_mean_en_pot(*particle, group);
+    double en_tot = en_kin + en_pot;
 
-    // apply rescale module
+    BOOST_TEST_MESSAGE("energy before rescaling: total = " << en_tot
+        << ", kinetic = " << en_kin << ", potential = " << en_pot);
+
+    // apply velocity rescaling
     rescale_type rescaler(particle, target_energy);
     rescaler.set();
 
-    // output energies after rescaling
-    double energy_after = thermo->en_tot();
-    BOOST_TEST_MESSAGE("Energy after rescaling: total = " << energy_after
-        << ", kinetic = " << thermo->en_kin()
-        << ", potential = " << thermo->en_pot());
+    // test energy of each particle separately
+    check_target_energies(*particle, target_energy, 10 * tolerance::value);
+
+    // compute mean energies after rescaling
+    en_kin = get_mean_en_kin(*particle, group);
+    en_pot = get_mean_en_pot(*particle, group);
+    en_tot = en_kin + en_pot;
+
+    BOOST_TEST_MESSAGE("energy after rescaling: total = " << en_tot
+        << ", kinetic = " << en_kin << ", potential = " << en_pot);
 
     // check that total energy is close to target value
-    float_type tolerance = 2 * std::numeric_limits<float_type>::epsilon();
-    BOOST_CHECK_CLOSE_FRACTION(energy_after, target_energy, tolerance); // tolerance::value
+    BOOST_CHECK_CLOSE_FRACTION(en_tot, target_energy, tolerance::value);
+
+    // lower target energy and trap exception
+    rescaler.set_target_energy(target_energy / 2);
+    BOOST_CHECK_THROW(rescaler.set(), std::runtime_error);
 }
 
 template <typename modules_type>
@@ -133,29 +181,13 @@ rescale_test<modules_type>::rescale_test()
 
     npart = gpu ? 3000 : 30;
     temp = 2.2;
-    density = 0.3;
-    target_energy = 1.3;
-
-    double volume = npart / density;
-    double box_length = std::pow(volume, 1. / dimension);
-    boost::numeric::ublas::diagonal_matrix<typename box_type::matrix_type::value_type> edges(dimension);
-    for (unsigned int i = 0; i < dimension; ++i)
-        edges(i, i) = box_length;
+    target_energy = float(1.3);     // avoid possible round-off issues with lower precision
 
     // construct test modules, keep their dependencies only locally
     particle = std::make_shared<particle_type>(npart, 1);
 
     auto random = std::make_shared<random_type>();
     boltzmann = std::make_shared<boltzmann_type>(particle, random, temp);
-
-    auto group = std::make_shared<particle_group_type>(particle);
-    auto box = std::make_shared<box_type>(edges);
-    auto logger = std::make_shared<halmd::logger>();
-    thermo = std::make_shared<thermo_type>(
-        particle, group, box
-      , [=]() { return volume; }
-      , logger
-    );
 }
 
 // tolerance helper
@@ -165,20 +197,19 @@ struct host_tolerance
     static constexpr double value = 2 * std::numeric_limits<float_type>::epsilon();
 };
 
-template struct host_tolerance<double>;
+template<typename float_type>
+constexpr double host_tolerance<float_type>::value;
 
 // module traits
 template <int dimension, typename float_type>
 struct host_modules
 {
-    typedef halmd::mdsim::box<dimension> box_type;
     typedef halmd::mdsim::host::particle<dimension, float_type> particle_type;
     typedef halmd::mdsim::host::particle_groups::all<particle_type> particle_group_type;
 
     typedef halmd::random::host::random random_type;
     typedef halmd::mdsim::host::velocities::boltzmann<dimension, float_type> boltzmann_type;
     typedef halmd::mdsim::host::velocities::rescale<dimension, float_type> rescale_type;
-    typedef halmd::observables::host::thermodynamics<dimension, float_type> thermo_type;
 
     static bool const gpu = false;
     typedef host_tolerance<float_type> tolerance;
@@ -212,25 +243,24 @@ struct gpu_tolerance
     static double const value;
 };
 
-// dsfloat has effectively 43 bits, single float merely 24,
-// multiply by number of particles to get a sharp upper bound
+// dsfloat has effectively 43 bits, single float merely 24.
+// For an unknown reason, rescale<dsfloat> seems to be not much more precise than using single precision,
+// so let's mulitply by a huge, empirical factor
 template<>
-double const gpu_tolerance<halmd::dsfloat>::value = 10000 * std::numeric_limits<float>::epsilon() / (1U << (43 - 24));
+double const gpu_tolerance<halmd::dsfloat>::value = (1U << 17) * std::numeric_limits<float>::epsilon() / (1U << (43 - 24));
 
 template<>
-double const gpu_tolerance<float>::value = std::numeric_limits<float>::epsilon() / 4;    // yields 0.5 ulp, the 4 is empirical
+double const gpu_tolerance<float>::value = std::numeric_limits<float>::epsilon() / 2;
 
 template <int dimension, typename float_type>
 struct gpu_modules
 {
-    typedef halmd::mdsim::box<dimension> box_type;
     typedef halmd::mdsim::gpu::particle<dimension, float_type> particle_type;
     typedef halmd::mdsim::gpu::particle_groups::all<particle_type> particle_group_type;
 
     typedef halmd::random::gpu::random<halmd::random::gpu::rand48> random_type;
     typedef halmd::mdsim::gpu::velocities::boltzmann<dimension, float_type, halmd::random::gpu::rand48> boltzmann_type;
     typedef halmd::mdsim::gpu::velocities::rescale<dimension, float_type> rescale_type;
-    typedef halmd::observables::gpu::thermodynamics<dimension, float_type> thermo_type;
 
     static bool const gpu = true;
     typedef gpu_tolerance<float_type> tolerance;
